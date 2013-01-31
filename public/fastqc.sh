793740b970929a0d97075cdc7fa5b2556cc56e46
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-01-31 14:02:05 +0100

    Added fastqc.sh sample

--- public/fastqc.sh
+++ public/fastqc.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-#PARAMETER TEST_PARAMETER1
-#PARAMETER TEST_PARAMETER2
+#PARAMETER INPUT
+#PARAMETER OUTPUT_DIR
 
-TEST_PARAMETER1=123
-TEST_PARAMETER2=test_parameter
+INPUT=/srv/GT/analysis/masaomi/sushi/work/20120905-I8_970_20081205_R1.fastq.gz
+OUTPUT_DIR=/srv/GT/analysis/masaomi/sushi/work/fastqc_out
+mkdir -p $OUTPUT_DIR
+fastqc -t 4 -o $OUTPUT_DIR $INPUT
 
-INPUT=/srv/GT/analysis/masaomi/sushi/20120921-hir_hay1_800_R1.fastq.gz
-OUTPUT=/srv/GT/analysis/masaomi/sushi/SushiFabric/public/fastqc_out.zip
-EXDIR=/srv/GT/analysis/masaomi/sushi/SushiFabric/public
-fastqc_wrapper $INPUT $OUTPUT
-unzip -o $OUTPUT -d $EXDIR
