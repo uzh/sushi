956daa2e60533e99173092a636bf9464b23cb207
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-01-31 14:43:06 +0100

    Added sample data for fastqc.sh

--- public/fastqc.sh
+++ public/fastqc.sh
@@ -2,7 +2,7 @@
 #PARAMETER INPUT
 #PARAMETER OUTPUT_DIR
 
-INPUT=/srv/GT/analysis/masaomi/sushi/work/20120905-I8_970_20081205_R1.fastq.gz
+INPUT=/srv/GT/analysis/masaomi/sushi/work/SushiFabric/20120905-I8_970_20081205_R1_sample.fastq.gz
 OUTPUT_DIR=/srv/GT/analysis/masaomi/sushi/work/fastqc_out
 mkdir -p $OUTPUT_DIR
 fastqc -t 4 -o $OUTPUT_DIR $INPUT
