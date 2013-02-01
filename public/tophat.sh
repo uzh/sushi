f05fcee6c604942381a77ba6c6c902cf6abc27fb
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-02-01 11:02:46 +0100

    Added tophat.sh job script sample

---
+++ public/tophat.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#PARAMETER INPUT
+#PARAMETER BOWTIE_INDEX
+#PARAMETER OUTPUT_DIR
+#PARAMETER THREAD 
+INPUT='/srv/GT/analysis/masaomi/sushi/work/SushiFabric/public/20120905-I8_970_20081205_R1_sample.fastq.gz /srv/GT/analysis/masaomi/sushi/work/SushiFabric/public/20120905-I8_970_20081205_R2_sample.fastq.gz'
+OUTPUT_DIR=/srv/GT/analysis/masaomi/sushi/work/tophat_out
+BOWTIE_INDEX=/srv/GT/analysis/masaomi/sushi/work/bowtie_index/flowering_genes
+THREAD=1
+
+echo tophat -p $THREAD -o $OUTPUT_DIR $BOWTIE_INDEX $INPUT
+tophat -p $THREAD -o $OUTPUT_DIR $BOWTIE_INDEX $INPUT
+
