6b862ef1336455f99b849639813c0d24b56d8130
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-02-06 14:46:37 +0100

    Added wfm_job_list that returns running job list

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,12 @@ SushiFabric::Application.routes.draw do
     end
   end
 
+  resources :job_monitoring do
+    collection do
+      post :index
+    end
+  end
+
   resources :data_list do
     collection do
       post :index
