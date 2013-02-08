69bc8bfbc82a70878aa8074e7c74b410e8741eea
Tanguy LE CARROUR <tanguy.lecarrour@fgcz.ethz.ch>
2013-02-08 13:54:42 +0100

    Update schema after the last migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130128091013) do
+ActiveRecord::Schema.define(:version => 20130206132500) do
 
   create_table "data_lists", :force => true do |t|
     t.integer  "data_set_id"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(:version => 20130128091013) do
   create_table "samples", :force => true do |t|
     t.string   "name"
     t.string   "path"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "resource_id", :default => 0, :null => false
   end
 
   create_table "users", :force => true do |t|
