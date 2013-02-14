8dfe89d02109b1efbb7cd130cc66f997f8a7be79
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-02-14 15:01:00 +0100

    Added JobLog model and result links in job_monitoring view

--- public/fastqc.sh
+++ public/fastqc.sh
@@ -3,8 +3,10 @@
 #PARAMETER OUTPUT_DIR
 #PARAMETER THREAD
 
+
 INPUT=/srv/GT/analysis/masaomi/sushi/work/SushiFabric/public/20120905-I8_970_20081205_R1_sample.fastq.gz
 OUTPUT_DIR=/srv/GT/analysis/masaomi/sushi/work/fastqc_out
+RESULT_LINK=file:///srv/GT/analysis/masaomi/sushi/work/fastqc_out/ 
 THREAD=4
 
 mkdir -p $OUTPUT_DIR
