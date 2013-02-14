e23bd94a16afb31c06aaecce435ee63c9dc747b6
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-02-14 15:10:00 +0100

    Replace href html to link_to method in job_monitoring view

--- app/controllers/job_monitoring_controller.rb
+++ app/controllers/job_monitoring_controller.rb
@@ -4,8 +4,7 @@ class JobMonitoringController < ApplicationController
     @job_list = @job_list.split(/\n/).map{|job| job.split(/,/)}
     @results  = {}
     @job_list.each do |job|
-      result = JobLog.find_by_job_id(job[0].to_i) and link=result.result_link.to_s
-      if job[1] =~ /success/
+      if result = JobLog.find_by_job_id(job[0].to_i) and link=result.result_link.to_s and job[1] =~ /success/
         @results[job[0]]=link
       else
         @results[job[0]]=''
