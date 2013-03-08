89177f1b715e7063800fbb6c73e4b8e91f428498
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-03-08 11:06:33 +0100

    Added data_set/delete to delete Sample DataList DataSet, but it does not consider the linkage between them

--- app/controllers/data_set_controller.rb
+++ app/controllers/data_set_controller.rb
@@ -5,6 +5,23 @@ class DataSetController < ApplicationController
     @samples = Sample.all
   end
   def edit
+    @data_sets = DataSet.all
+    @data_lists = DataList.all
+    @samples = Sample.all
+    @params = params
+  end
+  def add
+    data_list = DataList.new
+    data_list.data_set_id = params[:data_set_id].to_i
+    data_list.sample_id = params[:sample_id].to_i
+    data_list.save
+    @data_sets = DataSet.all
+    @data_lists = DataList.all
+    @samples = Sample.all
+    @params = params
+    render 'data_set/edit'
+  end
+  def delete
     if sample_ids = params[:sample_ids]
       sample_ids.each do |id|
         Sample.find(id.to_i).destroy
@@ -24,26 +41,6 @@ class DataSetController < ApplicationController
     @data_lists = DataList.all
     @samples = Sample.all
     @params = params
-  end
-  def add
-    data_list = DataList.new
-    data_list.data_set_id = params[:data_set_id].to_i
-    data_list.sample_id = params[:sample_id].to_i
-    data_list.save
-    @data_sets = DataSet.all
-    @data_lists = DataList.all
-    @samples = Sample.all
-    @params = params
-    render 'data_set/edit'
-  end
-  def delete
-    data_list = DataList.find_by_data_set_id_and_sample_id(params[:data_set_id].to_i,params[:sample_id].to_i)
-    DataList.delete(data_list.id)
-    @data_sets = DataSet.all
-    @data_lists = DataList.all
-    @samples = Sample.all
-    @params = params
-    render 'data_set/edit'
   end
   def add_or_delete
     params[:method] = params[:method].strip
