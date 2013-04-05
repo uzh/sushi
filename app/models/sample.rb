65d8d28862fca62062b09e4849bd5b40f8e7e034
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-04-05 14:30:46 +0200

    Modified data_set and sample models

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,2 +1,15 @@
 class Sample < ActiveRecord::Base
+  attr_accessible :key_value
+  belongs_to :data_set
+
+  def to_hash
+    eval(self.key_value)
+  end
+  def saved?
+    if Sample.find_by_key_value(self.key_value)
+      true
+    else
+      false
+    end
+  end
 end
