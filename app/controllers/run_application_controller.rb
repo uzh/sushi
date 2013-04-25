fa7108be15d61c4ae058c6ab2c356f12f803510c
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-04-25 11:16:36 +0200

    Added run_application/set_parameter, confirmation

--- app/controllers/run_application_controller.rb
+++ app/controllers/run_application_controller.rb
@@ -1,10 +1,29 @@
 class RunApplicationController < ApplicationController
   def index
-    @job_scripts = Dir['lib/*.rb'].sort.select{|script| script !~ /sushiApp\.rb/}.to_a.map{|script| script.gsub(/\.rb/,'')}
+    @sushi_apps = Dir['lib/*.rb'].sort.select{|script| script !~ /sushiApp\.rb/}.to_a.map{|script| File.basename(script).gsub(/\.rb/,'')}
     if project_number = session[:project] and project = Project.find_by_number(project_number.to_i)
       @data_sets = project.data_sets
     end
   end
   def set_parameters
+    @params = params
+    class_name = params[:sushi_app][:class]
+    @sushi_app = eval(class_name).new
+    data_set_id = params[:data_set][:id]
+    @data_set = DataSet.find(data_set_id.to_i)
+  end
+  def confirmation
+    @params = params
+    class_name = params[:sushi_app][:class]
+    @sushi_app = eval(class_name).new
+    data_set_id = params[:data_set][:id]
+    @data_set = DataSet.find(data_set_id.to_i)
+    params[:parameters].each do |key, value|
+      @sushi_app.params[key].value = if @sushi_app.params[key].type != String
+                                       eval(value)
+                                     else
+                                       value
+                                     end
+    end
   end
 end
