046ddba35632bc3a979e401e59b95c8c6dddaaa6
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-05-21 11:59:19 +0200

    Updated sushiApp.rb and view and controller

--- lib/sushiApp.rb
+++ lib/sushiApp.rb
@@ -1,42 +1,48 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-Version = '20130425-114011'
+Version = '20130521-115649'
 
 require 'csv'
 require 'fileutils'
 require 'active_record'
 
-SUSHI_APP_DIR='/srv/GT/analysis/masaomi/sushi/work'
+SUSHI_APP_DIR='/srv/GT/analysis/masaomi/sushi/work_party'
 SUSHI_DB_TYPE='sqlite3'
-WORKSPACE_DIR='/srv/GT/analysis/masaomi/sushi/sushi_launch_box/gstore/sushi'
+WORKSPACE_DIR='/srv/GT/analysis/masaomi/sushi/work_lunch/gstore/sushi'
 #WORKSPACE_DIR='/srv/gstore/projects'
 
-class SushiApp
-  class Param
-    attr_accessor :value
-    attr_reader :type
-    def initialize(type, default)
-      @value = default
-      @type = type
-    end
+
+
+class Object
+  attr_accessor :data_type
+  def data_type
+    @data_type ||= self.class
   end
+end
+class SushiApp
   attr_reader :params
+  attr_reader :job_ids
   attr_accessor :dataset_tsv_file
   attr_accessor :parameterset_tsv_file
   attr_accessor :dataset_sushi_id
-  attr_reader :job_ids
+  attr_accessor :project
   def initialize
     @workspace = WORKSPACE_DIR ## will be on g-store; writable by sushi
-    @project = "p999"
-    @params = {}
-    @params['process_mode'] = Param.new(String, 'SAMPLE')
+    @project = ''
+    @params = {
+      'cores' => 1,
+      'ram' => nil,
+      'node' => nil,
+      'process_mode' => 'SAMPLE'
+    }
+    @job_ids = []
   end
   def save_params_as_tsv
     file_name = 'parameters.tsv'
     file_path = File.join(@result_dir_extended, file_name)
     CSV.open(file_path, 'w', :col_sep=>"\t") do |out|
       out << @output_params.keys
-      out << @output_params.values.map{|param| param.value}
+      out << @output_params.values
     end
   end
   def set_input_dataset
@@ -69,7 +75,9 @@ class SushiApp
     end
   end
   def check_application_paramters
-    @output_params = @params.clone
+    if (@required_params - @params.keys).empty?
+      @output_params = @params.clone
+    end
   end
   def output_parameters
     @output_params.keys.each do |key|
@@ -82,8 +90,8 @@ class SushiApp
       parameterset_tsv = CSV.readlines(@parameterset_tsv_file, :headers=>true, :col_sep=>"\t")
       parameterset_tsv.each do |row|
         row.headers.each do |header|
-          @params[header] ||= Param.new(String, row[header])
-          @params[header].value = if @params[header].type == String
+          @params[header] ||= row[header]
+          @params[header] = if @params[header].data_type == String
                                     row[header]
                                   else
                                     eval(row[header])
@@ -112,7 +120,7 @@ cd $SCRATCH_DIR
   end
   def job_footer
     @out.print "#### JOB IS DONE WE PUT THINGS IN PLACE AND CLEAN AUP\n"
-    @output_files.map{|header| output[header]}.each do |file|
+    @output_files.map{|header| next_dataset[header]}.each do |file|
       # in actual case, to save under /srv/gstore/
       if WORKSPACE_DIR =~ /srv\/gstore/
         @out.print "gstore-request copy ", File.basename(file), " ", File.join(@workspace, file), "\n"
@@ -129,14 +137,24 @@ rm -rf $SCRATCH_DIR
     @out.print "#### NOW THE ACTUAL JOBS STARTS\n"
     @out.print commands, "\n\n"
   end
-  def output
+  def next_dataset
     # this should be overwritten in a subclass
   end
   def commands
     # this should be overwritten in a subclass
   end
   def submit(job_script)
-    job_id = `wfm_monitoring #{job_script}`
+    gsub_options = []
+    gsub_options << "-c #{@params['cores']}" if @params['cores']
+    gsub_options << "-n #{@params['node']}" if @params['node']
+    gsub_options << "-r #{@params['ram']}" if @params['ram']
+    gsub_options << "-s #{@params['scratch']}" if @params['scratch']
+    job_id = `wfm_monitoring #{job_script} #{gsub_options.join(' ')}`
+    job_id = job_id.to_i
+    unless job_id.to_i > 1
+      raise 'failed in job submitting'
+    end
+    job_id
   end
   def save_input_dataset_as_tsv
     file_name = 'input_dataset.tsv'
@@ -160,11 +178,11 @@ rm -rf $SCRATCH_DIR
       end
     end
   end
+  def preprocess
+    # this should be overwritten in a subclass
+  end
   def run
-    set_input_dataset
-    exit unless check_required_columns
-    check_application_paramters
-    set_user_parameters
+    test_run
 
     ## the user presses RUN
     prepare_result_dir
@@ -172,8 +190,7 @@ rm -rf $SCRATCH_DIR
     ## sushi writes creates the job scripts and builds the result data set that is to be generated
     @result_dataset = []
     @job_scripts = []
-    @job_ids = []
-    if @params['process_mode'].value == 'SAMPLE'
+    if @params['process_mode'] == 'SAMPLE'
       @dataset_hash.each do |row|
         @dataset = row
         ## WRITE THE JOB SCRIPT
@@ -184,27 +201,166 @@ rm -rf $SCRATCH_DIR
         job_footer
         @out.close
         @job_scripts << @job_script
-        @result_dataset << output
+        @result_dataset << next_dataset
       end
       @job_scripts.each do |job_script|
-        #print "Submit job #{File.basename(job_script)} job_id=#{submit(job_script)}"
-        @job_ids << submit(job_script).chomp
+        job_id = submit(job_script)
+        @job_ids << job_id
+        print "Submit job #{File.basename(job_script)} job_id=#{job_id}"
+        open(File.join(@result_dir_extended, 'get_log.sh'), 'w') do |out|
+          out.print "#!/bin/sh\n\n"
+          out.print 'CURDIR=`dirname $0`', "\n"
+          out.print "wfm_get_log #{job_id} with_err > $CURDIR/log.dat\n"
+        end
       end
-    elsif @params['process_mode'].value == 'DATASET'
+    elsif @params['process_mode'] == 'DATASET'
       # This should be implemented in a subclass (Application class)
     else 
       #stop
+      warn "the process mode (#{@params['process_mode']}) is not defined"
+      raise "stop job submitting"
     end
 
-		#puts
-    #print 'job scripts: '
-    #p @job_scripts
-    #print 'result dataset: '
-    #p @result_dataset
+		puts
+    print 'job scripts: '
+    p @job_scripts
+    print 'result dataset: '
+    p @result_dataset
 
     save_params_as_tsv
     save_input_dataset_as_tsv
     save_next_dataset_as_tsv
+  end
+  def test_run
+    preprocess
+    set_input_dataset
+    set_user_parameters
+
+    failures = 0
+    print 'check project name: '
+    if @project.empty?
+      puts "\e[31mFAILURE\e[0m: project number is required but not found. you should set it in usecase."
+      puts "\tex.)"
+      puts "\tapp = #{self.class}.new"
+      puts "\tapp.project = 'p1001'"
+      failures += 1
+    else
+      puts "\e[32mPASSED\e[0m:\n\t@project=#{@project}"
+    end
+
+    print 'check application name: '
+    if @name.to_s.empty?
+      puts "\e[31mFAILURE\e[0m: application name is required but not found. you should set it in application class."
+      puts "\tex.)"
+      puts "\tclass #{self.class}"
+      puts "\t def initialize"
+      puts "\t  @name = '#{self.class}'"
+      puts "\t end"
+      puts "\tend"
+      failures += 1
+    else
+      puts "\e[32mPASSED\e[0m:\n\t@name=#{@name}"
+    end
+
+    print 'check analysis_category: '
+    if @analysis_category.to_s.empty?
+      puts "\e[31mFAILURE\e[0m: analysis_category is required but not found. you should set it in application class."
+      puts "\tex.)"
+      puts "\tclass #{self.class}"
+      puts "\t def initialize"
+      puts "\t  @analysis_category = 'Mapping'"
+      puts "\t end"
+      puts "\tend"
+      failures += 1
+    else
+      puts "\e[32mPASSED\e[0m:\n\t@analysis_category=#{@analysis_category}"
+    end
+
+    print 'check dataset: '
+    if !@dataset_hash or @dataset_hash.empty?
+      puts "\e[31mFAILURE\e[0m: dataset is not found. you should set it by using #{self.class}#dataset_sushi_id or #{self.class}#dataset_tsv_file properties"
+      puts "\tex.)"
+      ptus "\tusecase = #{self.class}.new"
+      puts "\tusecase.dataset_tsv_file = \"dataset.tsv\""
+      failures += 1
+    else
+      puts "\e[32mPASSED\e[0m:\n\t@dataset_hash.length = #{@dataset_hash.length}"
+    end
+
+    print 'check required columns: '
+    unless check_required_columns
+      puts "\e[31mFAILURE\e[0m: required_column(s) is not found in dataset"
+      failures += 1
+    else
+      puts "\e[32mPASSED\e[0m:"
+    end
+    puts "\trequired columns: #{@required_columns}"
+    puts "\tdataset  columns: #{@dataset_hash.map{|row| row.keys}.flatten.uniq}"
+
+    print 'check required parameters: '
+    unless check_application_paramters
+      puts "\e[31mFAILURE\e[0m: required_param(s) is not set yet. you should set it in usecase"
+      puts "\tmissing params: #{@required_params-@params.keys}"
+      puts "\tex.)"
+      puts "\tusecase = #{self.class}.new"
+      puts "\tusecase.params['#{(@required_params-@params.keys)[0]}'] = parameter"
+      puts
+      failures += 1
+    else
+      puts "\e[32mPASSED\e[0m:"
+    end
+    puts "\tparameters: #{@params.keys}"
+    puts "\trequired  : #{@required_params}"
+
+    print 'check next dataset: '
+    @dataset={}
+    @result_dir='.'
+    unless self.next_dataset
+      puts "\e[31mFAILURE\e[0m: next dataset is not set yet. you should overwrite SushiApp#next_dataset method in #{self.class}"
+      puts "\tnote: the return value should be Hash (key: column title, value: value in a tsv table)"
+      failures += 1
+    else
+      puts "\e[32mPASSED\e[0m:"
+    end
+
+    print 'check output files: '
+    if !@output_files or @output_files.empty?
+      puts "\e[31mWARNING\e[0m: no output files. you will not get any output files after the job running. you can set @output_files (array) in #{self.class}"
+      puts "\tnote: usually it should be define in initialize method"
+      puts "\t      the elements of @output_files should be chosen from #{self.class}#next_dataset.keys"
+      puts "\t      #{self.class}#next_dataset.keys: #{self.next_dataset.keys}"
+    else
+      puts "\e[32mPASSED\e[0m:"
+    end
+
+    print 'check commands: '
+    unless commands
+      puts "\e[31mFAILURE\e[0m: any commands is not defined yet. you should overwrite SushiApp#commands method in #{self.class}"
+      puts "\tnote: the return value should be String (this will be in the main body of submitted job script)"
+      failures += 1
+    else
+      puts "\e[32mPASSED\e[0m:"
+    end
+
+    print 'check workflow manager: '
+    begin
+      hello = `wfm_hello`
+    rescue
+    end
+    unless hello =~ /hello/
+      puts "\e[31mFAILURE\e[0m: workflow_manager does not reply. check if workflow_manager is working"
+      failures += 1
+    else
+      puts "\e[32mPASSED\e[0m:"
+    end
+
+    if failures > 0
+      puts
+      puts "\e[31mFailures (#{failures})\e[0m: All failures should be solved"
+      raise "test run fails"
+    else
+      puts "All checks \e[32mPASSED\e[0m"
+    end
   end
 end
 
