df466e6ec18d6187b0e898e6dc45b31b420228ab
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-07-04 17:22:22 +0200

    Added job model and show data set link in job monitoring view

--- app/controllers/run_application_controller.rb
+++ app/controllers/run_application_controller.rb
@@ -97,5 +97,12 @@ class RunApplicationController < ApplicationController
     end
     @sushi_app.dataset_sushi_id = data_set_id.to_i
     @sushi_app.run
+    @sushi_app.job_ids.each do |job_id|
+      new_job = Job.new
+      new_job.submit_job_id = job_id.to_i
+      new_job.input_dataset_id = data_set_id.to_i
+      new_job.next_dataset_id = @sushi_app.next_dataset_id
+      new_job.save
+    end
   end
 end
