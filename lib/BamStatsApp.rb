372489cf3a79cfab704590d2852966d335c85489
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-07-12 17:17:25 +0200

    Added BamStatsApp.rb and fixed TophatApp.rb and TophatWrappedApp.rb for BamStatsApp.rb

---
+++ lib/BamStatsApp.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushiApp'
+
+class BamStatsApp <  SushiApp
+  def initialize
+    super
+    @name = 'BAM Stat'
+    @params['process_mode'] = 'DATASET'
+    @analysis_category = 'QC'
+    @required_columns = ['Name','BAM [File]','BAI [File]', 'Species']
+    @required_params = ['name', 'paired']
+    @params['paired'] = false
+    @params['name'] = 'BAM Statistics'
+    @params['strandMode'] = ''
+    @params['build'] = {'select'=>''}
+    Dir["/srv/GT/reference/*/*/*"].sort.select{|build| File.directory?(build)}.each do |dir|
+      @params['build'][dir.gsub(/\/srv\/GT\/reference\//,'')] = File.basename(dir)
+    end
+    @params['featureFile'] = 'genes.gtf'
+  end
+  def next_dataset
+    {'Name'=>@params['name'],
+     'Report [File]'=>File.join(@result_dir, @params['name']),
+    }
+  end
+  def commands
+    command = "/usr/local/ngseq/bin/R --vanilla --slave<<  EOT\n"
+    command<<  "source('/usr/local/ngseq/sushi_scripts/init.R')\n"
+    command << "config = list()\n"
+    config = @params
+    config.keys.each do |key|
+      command << "config[['#{key}']] = '#{config[key]}'\n" 
+    end
+    command << "config[['dataRoot']] = '#{@gstore_dir}'\n"
+    command << "output = list()\n"
+    output = next_dataset
+    output.keys.each do |key|
+      command << "output[['#{key}']] = '#{output[key]}'\n" 
+    end
+    command<<  "inputDatasetFile = '#{@input_dataset_tsv_path}'\n"
+    command<<  "bamStatsApp(input=inputDatasetFile, output=output, config=config)\n"
+    command<<  "EOT\n"
+    command
+  end
+end
+
+if __FILE__ == $0
+  usecase = BamStatsApp.new
+
+  usecase.project = "p1001"
+  usecase.user = "masa"
+
+  # set user parameter
+  # for GUI sushi
+  #usecase.params['process_mode'].value = 'SAMPLE'
+  #usecase.params['build'] = 'TAIR10'
+  #usecase.params['paired'] = true
+  #usecase.params['cores'] = 2
+  #usecase.params['node'] = 'fgcz-c-048'
+
+  # also possible to load a parameterset csv file
+  # mainly for CUI sushi
+  usecase.parameterset_tsv_file = 'tophat_parameterset.tsv'
+  #usecase.params['name'] = 'name'
+
+  # set input dataset
+  # mainly for CUI sushi
+  usecase.dataset_tsv_file = 'tophat_dataset.tsv'
+
+  # also possible to load a input dataset from Sushi DB
+  #usecase.dataset_sushi_id = 1
+
+  # run (submit to workflow_manager)
+  usecase.run
+  #usecase.test_run
+
+end
+
