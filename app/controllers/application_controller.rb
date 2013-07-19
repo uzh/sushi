5f350796c99fd7c1b4f8718a11b04def74644809
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-07-19 14:22:05 +0200

    Added ApplicationController#runnable_application for refactoring to show app buttons in both data_set and run_application views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,4 +39,26 @@ class ApplicationController < ActionController::Base
     end
     session[:bf_user_id]
   end
+  def runnable_application(data_set_headers)
+    non_sushi_apps = ['sushiApp.rb', 'sushiToolBox.rb', 'SushiWrap.rb', 'optparse_ex.rb']
+    sushi_apps = Dir['lib/*.rb'].select{|script| !non_sushi_apps.include?(File.basename(script))}.to_a.map{|script| File.basename(script)}
+    sushi_apps.concat Dir['lib/*.sh'].map{|script| File.basename(script)}
+
+    # filter application with data_set
+    sushi_apps = sushi_apps.sort.select do |script|
+      class_name = ''
+      if script =~ /\.rb/
+        class_name = script.gsub(/\.rb/,'')
+        require class_name
+      elsif script =~ /\.sh/
+        class_name = script.gsub(/\.sh/,'')
+        sushi_wrap = SushiWrap.new("lib/#{script}")
+        sushi_wrap.define_class
+      end
+      sushi_app = eval(class_name).new
+      required_columns = sushi_app.required_columns
+      (required_columns - data_set_headers).empty?
+    end
+    sushi_apps
+  end
 end
