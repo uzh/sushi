a885202d4272607e75439016fae8bdbab1c4d89e
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-07-26 16:15:26 +0200

    Added EdgeRApp.rb

---
+++ lib/EdgeRApp.rb
@@ -0,0 +1,89 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+Version = '20130719-094810'
+
+require 'sushiApp'
+
+class EdgeRApp < SushiApp
+  def initialize
+    super
+    @name = 'EdgeR'
+    @analysis_category = 'Differential Expression'
+    @required_columns = ['Name','Count', 'Species']
+    @required_params = ['grouping', 'sampleGroup', 'refGroup']
+    # optional params
+    @params['cores'] = '1'
+    @params['ram'] = '2'
+    @params['scratch'] = '10'
+    @params['featureFile'] = 'genes.gtf'
+    @params['featureLevel'] = 'gene'
+    @params['grouping'] = '' ### TODO: this should be filled by a column selector that allows to select a column with the tag 'Factor'
+    @params['sampleGroup'] = '' ## TODO: this should be a value from the selected column
+    @params['refGroup'] = '' ## TODO: this should be a value from the selected column
+    @params['normMethod'] = 'logMean'
+    @params['runGO'] = 'false'
+    @params['specialOptions'] = ''
+  end
+  def next_dataset
+    @comparison = "#{@params['sampleGroup']}--over--#{@params['refGroup']}"
+    report_file = File.join(@result_dir, @comparison)
+    report_link = File.join(report_file, '00index.html')
+    {'Name'=>@comparison,
+     'Report [File]'=>report_file,
+     'Html [Link]'=>report_link,
+    }
+  end
+  def commands
+    command = "/usr/local/ngseq/bin/R --vanilla --slave << EOT\n"
+    command << "source('/usr/local/ngseq/sushi_scripts/init.R')\n"
+    command << "config = list()\n"
+    config = @params
+    config.keys.each do |key|
+      command << "config[['#{key}']] = '#{config[key]}'\n" 
+    end
+    command << "config[['dataRoot']] = '#{@gstore_dir}'\n"
+    command << "output = list()\n"
+    output = next_dataset
+    output.keys.each do |key|
+      command << "output[['#{key}']] = '#{output[key]}'\n" 
+    end
+    command<<  "inputDatasetFile = '#{@input_dataset_tsv_path}'\n"
+    command << "edgeRApp(input=inputDatasetFile, output=output, config=config)\n"
+    command << "EOT"
+    command
+  end
+end
+
+if __FILE__ == $0
+  usecase = EdgeRApp.new
+
+  usecase.project = "p1001"
+  usecase.user = 'masamasa'
+
+  # set user parameter
+  # for GUI sushi
+  #usecase.params['process_mode'].value = 'SAMPLE'
+  usecase.params['build'] = 'mm10'
+  usecase.params['paired'] = true
+  usecase.params['strandMode'] = 'both'
+  usecase.params['cores'] = 8
+  usecase.params['node'] = 'fgcz-c-048'
+
+  # also possible to load a parameterset csv file
+  # mainly for CUI sushi
+  #usecase.parameterset_tsv_file = 'tophat_parameterset.tsv'
+  #usecase.parameterset_tsv_file = 'test.tsv'
+
+  # set input dataset
+  # mainly for CUI sushi
+  #usecase.dataset_tsv_file = 'tophat_dataset.tsv'
+
+  # also possible to load a input dataset from Sushi DB
+  usecase.dataset_sushi_id = 3
+
+  # run (submit to workflow_manager)
+  usecase.run
+  #usecase.test_run
+
+end
+
