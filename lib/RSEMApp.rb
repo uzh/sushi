a7e838c4f2b0a1e3125c4b9a59e54316c7c48419
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-08-16 11:49:29 +0200

    Changed init.R path, /usr/local/ngseq/opt/sushi_scripts/init.R

--- lib/RSEMApp.rb
+++ lib/RSEMApp.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-Version = '20130726-113511'
+Version = '20130815-144535'
 
 require 'sushiApp'
 
@@ -44,7 +44,7 @@ class RSEMApp < SushiApp
   end
   def commands
     command = "/usr/local/ngseq/bin/R --vanilla --slave << EOT\n"
-    command << "source('/usr/local/ngseq/sushi_scripts/init.R')\n"
+    command << "source('/usr/local/ngseq/opt/sushi_scripts/init.R')\n"
     command << "config = list()\n"
     config = @params
     config.keys.each do |key|
--- lib/TophatWrappedApp.rb
+++ lib/TophatWrappedApp.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-Version = '20130719-094810'
+Version = '20130815-144448'
 
 require 'sushiApp'
 
@@ -44,7 +44,7 @@ class TophatWrappedApp < SushiApp
   end
   def commands
     command = "/usr/local/ngseq/bin/R --vanilla --slave << EOT\n"
-    command << "source('/usr/local/ngseq/sushi_scripts/init.R')\n"
+    command << "source('/usr/local/ngseq/opt/sushi_scripts/init.R')\n"
     command << "config = list()\n"
     config = @params
     config.keys.each do |key|
