285b79e426ecb74f8937571de7952f16c070b516
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-08-16 15:59:02 +0200

    Added NcPROApp.rb (but still got an error from R script)

---
+++ lib/NcPROApp.rb
@@ -0,0 +1,93 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushiApp'
+
+class NcPROApp < SushiApp
+  def initialize
+    super
+    @name = 'ncPRO_Report'
+    @params['process_mode'] = 'DATASET'
+    @analysis_category = 'QC'
+    @required_columns = ['Name','Read1','Species']
+    @required_params = ['name', 'paired']
+
+    @params['cores'] = '4'
+    @params['ram'] = '16'
+    @params['scratch'] = '100'
+    @params['build'] = {'select'=>''}
+    Dir["/srv/GT/reference/*/*/*"].sort.select{|build| File.directory?(build)}.each do |dir|
+      @params['build'][dir.gsub(/\/srv\/GT\/reference\//,'')] = File.basename(dir)
+    end
+    @params['paired'] = false
+    @params['name'] = 'ncPRO_Result'
+  end
+  def preprocess
+    if @params['paired']
+      @required_columns<<  'Read2'
+    end
+  end
+  def next_dataset
+    report_file = File.join(@result_dir, @params['name'])
+    report_link = File.join(report_file, 'ncpro/report.html')
+    {'Name'=>@params['name'],
+     'Report [File]'=>report_file,
+     'Html [Link]'=>report_link,
+    }
+  end
+  def commands
+    command = "/usr/local/ngseq/bin/R --vanilla --slave<<  EOT\n"
+    command <<  "source('/usr/local/ngseq/sushi_scripts/init.R')\n"
+    command << "config = list()\n"
+    config = @params
+    config.keys.each do |key|
+      command << "config[['#{key}']] = '#{config[key]}'\n" 
+    end
+    command << "config[['dataRoot']] = '#{@gstore_dir}'\n"
+    command << "output = list()\n"
+    output = next_dataset
+    output.keys.each do |key|
+      command << "output[['#{key}']] = '#{output[key]}'\n" 
+    end
+    command<<  "inputDatasetFile = '#{@input_dataset_tsv_path}'\n"
+    command<<  "ncproApp(input=inputDatasetFile, output=output, config=config)\n"
+    command<<  "EOT\n"
+    command
+  end
+end
+
+if __FILE__ == $0
+  usecase = NcPROApp.new
+
+  usecase.project = "p1001"
+  usecase.user = "masa"
+
+  # set user parameter
+  # for GUI sushi
+  #usecase.params['process_mode'].value = 'SAMPLE'
+  #usecase.params['build'] = 'TAIR10'
+  #usecase.params['paired'] = true
+  #usecase.params['cores'] = 2
+  #usecase.params['node'] = 'fgcz-c-048'
+
+  # also possible to load a parameterset csv file
+  # mainly for CUI sushi
+  usecase.parameterset_tsv_file = 'tophat_parameterset.tsv'
+  #usecase.params['name'] = 'name'
+
+  # set input dataset
+  # mainly for CUI sushi
+  usecase.dataset_tsv_file = 'tophat_dataset.tsv'
+
+  # also possible to load a input dataset from Sushi DB
+  #usecase.dataset_sushi_id = 1
+
+  # run (submit to workflow_manager)
+  #usecase.run
+  usecase.test_run
+
+end
+
+
+
+
