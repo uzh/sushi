211c07e66570153f8632afceb69e778d74617d48
Hubert Rehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2013-08-23 15:58:47 +0200

    modified featureLevel option for RSEMApp; added CountQCApp

---
+++ lib/CountQCApp.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushiApp'
+
+class CountQCApp < SushiApp
+  def initialize
+    super
+    @name = 'CountQC'
+    @analysis_category = 'QC'
+    @required_columns = ['Name','Count', 'Species', 'Build']
+    @required_params = []
+    # optional params
+    @params['cores'] = '1'
+    @params['ram'] = '2'
+    @params['scratch'] = '10'
+    @params['name'] = 'Count_QC'
+    @params['featureFile'] = 'genes.gtf'
+    @params['featureLevel'] = ['gene', 'isoform']
+    @params['normMethod'] = 'logMean'
+    @params['expressionName'] = ''
+    @params['runGO'] = 'false'
+    @params['specialOptions'] = ''
+  end
+  def next_dataset
+    report_file = File.join(@result_dir, @params['name'])
+    report_link = File.join(report_file, '00index.html')
+    {'Name'=>@params['name'],
+     'Report [File]'=>report_file,
+     'Html [Link]'=>report_link,
+    }
+  end
+  def commands
+    command = "/usr/local/ngseq/bin/R --vanilla --slave << EOT\n"
+    command << "source('/usr/local/ngseq/opt/sushi_scripts/init.R')\n"
+    command << "config = list()\n"
+    config = @params
+    config.keys.each do |key|
+      command << "config[['#{key}']] = '#{config[key]}'\n" 
+    end
+    command << "config[['dataRoot']] = '#{@gstore_dir}'\n"
+    command << "output = list()\n"
+    output = next_dataset
+    output.keys.each do |key|
+      command << "output[['#{key}']] = '#{output[key]}'\n" 
+    end
+    command<<  "inputDatasetFile = '#{@input_dataset_tsv_path}'\n"
+    command << "countQCApp(input=inputDatasetFile, output=output, config=config)\n"
+    command << "EOT"
+    command
+  end
+end
+
+if __FILE__ == $0
+   usecase = CountQCApp.new
+
+  usecase.project = "p1001"
+  usecase.user = 'masamasa'
+
+  # set user parameter
+  # for GUI sushi
+  #usecase.params['process_mode'].value = 'SAMPLE'
+  usecase.params['build'] = 'mm10'
+  usecase.params['paired'] = true
+  usecase.params['strandMode'] = 'both'
+  usecase.params['cores'] = 8
+  usecase.params['node'] = 'fgcz-c-048'
+
+  # also possible to load a parameterset csv file
+  # mainly for CUI sushi
+  #usecase.parameterset_tsv_file = 'tophat_parameterset.tsv'
+  #usecase.parameterset_tsv_file = 'test.tsv'
+
+  # set input dataset
+  # mainly for CUI sushi
+  #usecase.dataset_tsv_file = 'tophat_dataset.tsv'
+
+  # also possible to load a input dataset from Sushi DB
+  usecase.dataset_sushi_id = 3
+
+  # run (submit to workflow_manager)
+  usecase.run
+  #usecase.test_run
+
+end
+
