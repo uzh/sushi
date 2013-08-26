d26b239ef5495787b7041ad631f8d47f73e07e15
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-08-26 14:18:58 +0200

    Fixed public path in job_monitoring.rb

--- app/controllers/job_monitoring_controller.rb
+++ app/controllers/job_monitoring_controller.rb
@@ -1,6 +1,7 @@
 class JobMonitoringController < ApplicationController
   def index
-    @job_list = `public/wfm_job_list -d #{WORKFLOW_MANAGER} -p #{session[:project]}`
+    public_dir = File.expand_path('../../../public', __FILE__)
+    @job_list = `#{public_dir}/wfm_job_list -d #{WORKFLOW_MANAGER} -p #{session[:project]}`
     @job_list = @job_list.split(/\n/).map{|job| job.split(/,/)}
     @total = @job_list.length
 
@@ -26,11 +27,13 @@ class JobMonitoringController < ApplicationController
     end
   end
   def print_log
-    text = `public/wfm_get_log #{params[:job_id]} :with_err #{WORKFLOW_MANAGER}`
+    public_dir = File.expand_path('../../../public', __FILE__)
+    text = `#{public_dir}/wfm_get_log #{params[:job_id]} :with_err #{WORKFLOW_MANAGER}`
     render :text => text.gsub(/\n/,'<br />')
   end
   def print_script
-    text = `public/wfm_get_script #{params[:job_id]} #{WORKFLOW_MANAGER}`
+    public_dir = File.expand_path('../../../public', __FILE__)
+    text = `#{public_dir}/wfm_get_script #{params[:job_id]} #{WORKFLOW_MANAGER}`
     render :text => text.gsub(/\n/,'<br />')
   end
 end
