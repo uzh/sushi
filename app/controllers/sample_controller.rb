83addcf5723cfb07ca715c8f3e0de14a7f6cefd4
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-08-30 09:35:21 +0200

    Fixed sample column name edit function

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -6,19 +6,21 @@ class SampleController < ApplicationController
     # assuming values are not different, 
     # in other words, this should be done after editing values
     new_headers = params[:sample_headers]
-    #@current_headers = Hash[*@data_set.samples.first.to_hash.keys.map{|header| [header.split.first, header]}.flatten]
-    @data_set.samples.each_with_index do |sample, i|
-      new_sample = {}
-      sample.to_hash.each do |header, value|
-        header_without_tag = header.split.first
-        new_header = new_headers[header_without_tag]
-        new_sample[new_header] = value
+    current_headers = Hash[*@data_set.samples.first.to_hash.keys.map{|header| [header.split.first, header]}.flatten]
+    if new_headers and new_headers!=current_headers
+      @data_set.samples.each_with_index do |sample, i|
+        new_sample = {}
+        sample.to_hash.each do |header, value|
+          header_without_tag = header.split.first
+          new_header = new_headers[header_without_tag]
+          new_sample[new_header] = value
+        end
+        @data_set.samples[i].key_value = new_sample.to_s
+        @data_set.samples[i].save
       end
-      @data_set.samples[i].key_value = new_sample.to_s
-      @data_set.samples[i].save
+      @data_set.md5 = @data_set.md5hexdigest
+      @data_set.save
     end
-    @data_set.md5 = @data_set.md5hexdigest
-    @data_set.save
 
     # update values
 =begin
