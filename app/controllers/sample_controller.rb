e7f13af9ad88df5680d096cb622552707644a5b2
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-08-30 08:56:07 +0200

    Added sample/edit view but no function implemented yet

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -2,24 +2,22 @@ class SampleController < ApplicationController
   def show
     @data_set = DataSet.find_by_id(params[:id])
 
-    # check real data
-    @file_exist = {}
-    @sample_path = []
-    @data_set.samples.each do |sample|
-      sample.to_hash.each do |header, file|
-        if header =~ /\[File\]/
-          file_path = File.join(GSTORE_DIR, file)
-          @sample_path << File.dirname(file)
-          @file_exist[file] = File.exist?(file_path)
-        else
-          @file_exist[file] = true
+    # update values
+=begin
+    params.select{|key| key=~/sample_\d+/}.sort_by{|key| key.match(/sample_(\d+)/)[1].to_i}.each do |sample_no|
+      if sample = params[sample_no] and i = sample_no.split(/_/)[1].to_i
+        sample = eval(sample)
+        new_sample = {}
+        @data_set.samples[i].to_hash.keys.each do |header|
+          if new_value = sample[saheader.split.first]
+            new_sample[header] = new_value
+          end
         end
       end
     end
-    @sample_path.uniq!
-
-    if @file_exist.values.inject{|a,b| a and b}
-      @sushi_apps = runnable_application(@data_set.headers)
-    end
+=end
+  end
+  def edit
+    @data_set = DataSet.find_by_id(params[:id])
   end
 end
