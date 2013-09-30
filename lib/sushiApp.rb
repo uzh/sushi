f8893be3ccc131a1d3ba44b5eb7bc5635f0bb9cd
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2013-09-30 13:28:28 +0200

    Deleted get_log_script process from SushiApp

--- lib/sushiApp.rb
+++ lib/sushiApp.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-Version = '20130930-113815'
+Version = '20130930-132802'
 
 require 'csv'
 require 'fileutils'
@@ -328,10 +328,8 @@ rm -rf #{@scratch_dir} || exit 1
                     else 
                       File.join(@scratch_result_dir, @analysis_category + '_' + sample_name) + '.sh'
                     end
-      @get_log_script = File.join(@scratch_result_dir, "get_log_#{sample_name}.sh")
       make_job_script
       @job_scripts << @job_script
-      @get_log_scripts << @get_log_script
       @result_dataset << next_dataset
     end
   end
@@ -341,10 +339,8 @@ rm -rf #{@scratch_dir} || exit 1
                   else 
                     File.join(@scratch_result_dir, @analysis_category + '_' + 'job_script.sh')
                   end
-    @get_log_script = File.join(@scratch_result_dir, "get_log.sh")
     make_job_script
     @job_scripts << @job_script
-    @get_log_scripts << @get_log_script
     @result_dataset << next_dataset
   end
   def run
@@ -361,7 +357,6 @@ rm -rf #{@scratch_dir} || exit 1
     ## sushi writes creates the job scripts and builds the result data set that is to be generated
     @result_dataset = []
     @job_scripts = []
-    @get_log_scripts = []
     if @params['process_mode'] == 'SAMPLE'
       sample_mode
     elsif @params['process_mode'] == 'DATASET'
@@ -377,12 +372,6 @@ rm -rf #{@scratch_dir} || exit 1
       job_id = submit(job_script)
       @job_ids << job_id
       print "Submit job #{File.basename(job_script)} job_id=#{job_id}"
-      get_log_script = @get_log_scripts[i]
-      open(get_log_script, 'w') do |out|
-        out.print "#!/bin/sh\n\n"
-        log_file = File.basename(job_script.gsub(/\.sh/,'.log'))
-        out.print "wfm_get_log #{job_id} with_err #{WORKFLOW_MANAGER} > #{log_file}\n"
-      end
     end
 
 		puts
