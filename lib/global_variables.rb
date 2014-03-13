119e39fce19c569af479ca847cdbb7b2f8f53d0f
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2014-03-13 11:58:27 +0100

    Changed builder_selector and fixed to show build and version in conformation view (Ticket#2693)

--- lib/global_variables.rb
+++ lib/global_variables.rb
@@ -4,6 +4,24 @@
 module GlobalVariables
   SUSHI = 'Supercalifragilisticexpialidocious!!'
 
+
+  def builder_selector(base_dir, shown_pattern=nil, value_pattern=nil)
+    selector = {}
+    Dir[base_dir].sort.select{|dir| File.directory?(dir)}.each do |dir|
+      key = if shown_pattern
+              dir.gsub(shown_pattern.keys.first, shown_pattern)
+            else
+              dir
+            end
+      value = if value_pattern
+                dir.gsub(value_pattern.keys.first, value_pattern)
+              else
+                File.basename(dir)
+              end
+      selector[key] = value
+    end
+    selector
+  end
   def ref_selector
     selector = {'select'=>''}
 
@@ -12,7 +30,8 @@ module GlobalVariables
     builds = builder_selector(base_pattern, shown_replace_pattern)
 
     base_pattern = "/srv/GT/reference/*/*/*/Annotation/Version*"
-    versions = builder_selector(base_pattern, shown_replace_pattern)
+    value_replace_pattern = {/\/srv\/GT\/reference\/.+?\/.+?\//=>''}
+    versions = builder_selector(base_pattern, shown_replace_pattern, value_replace_pattern)
 
     builds.keys.each do |build_key|
       if versions.keys.find{|version_key| version_key=~/#{build_key}/}
