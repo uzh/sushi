a5d2e7a253ac422c159f6a8823e7e12c5dfeaa82
Hubert Rehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2014-03-17 13:03:59 +0100

    refactored tophat

--- lib/TophatApp.rb
+++ lib/TophatApp.rb
@@ -3,6 +3,9 @@
 Version = '20131128-084636'
 
 require 'sushi_fabric'
+require_relative 'global_variables'
+require_relative 'optparse_ex'
+include GlobalVariables
 
 class TophatApp < SushiFabric::SushiApp
   def initialize
@@ -15,10 +18,7 @@ class TophatApp < SushiFabric::SushiApp
     @params['cores'] = '8'
     @params['ram'] = '16'
     @params['scratch'] = '100'
-    @params['build'] = {'select'=>''}
-    Dir["/srv/GT/reference/*/*/*"].sort.select{|build| File.directory?(build)}.each do |dir|
-      @params['build'][dir.gsub(/\/srv\/GT\/reference\//,'')] = File.basename(dir)
-    end
+    @params['build'] = ref_selector
     @params['paired'] = false
     @params['strandMode'] = ['both', 'sense', 'antisense']
     @params['featureFile'] = 'genes.gtf'
@@ -34,12 +34,17 @@ class TophatApp < SushiFabric::SushiApp
       @required_columns << 'Read2'
     end
   end
+  def set_default_parameters
+    @params['paired'] = dataset_has_column?('Read2')
+  end
   def next_dataset
     {'Name'=>@dataset['Name'], 
      'BAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}.bam"), 
      'BAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}.bam.bai"),
      'Species'=>@dataset['Species'],
-     'Build'=>@params['build'],
+     'build'=>@params['build'],
+     'paired'=>@params['paired'],
+     'featureFile=@params['featureFile']
     }
   end
   def commands
@@ -68,35 +73,6 @@ class TophatApp < SushiFabric::SushiApp
 end
 
 if __FILE__ == $0
-  usecase = TophatApp.new
-
-  usecase.project = "p1001"
-  usecase.user = 'masamasa'
-
-  # set user parameter
-  # for GUI sushi
-  #usecase.params['process_mode'].value = 'SAMPLE'
-  usecase.params['build'] = 'mm10'
-  usecase.params['paired'] = true
-  usecase.params['strandMode'] = 'both'
-  usecase.params['cores'] = 8
-  usecase.params['node'] = 'fgcz-c-048'
-
-  # also possible to load a parameterset csv file
-  # mainly for CUI sushi
-  #usecase.parameterset_tsv_file = 'tophat_parameterset.tsv'
-  #usecase.parameterset_tsv_file = 'test.tsv'
-
-  # set input dataset
-  # mainly for CUI sushi
-  #usecase.dataset_tsv_file = 'tophat_dataset.tsv'
-
-  # also possible to load a input dataset from Sushi DB
-  usecase.dataset_sushi_id = 3
-
-  # run (submit to workflow_manager)
-  usecase.run
-  #usecase.test_run
-
+  run TophatApp
 end
 
