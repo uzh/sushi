274fc4d2db43e25a072c1b80c1a41afa57b3a483
Hubert Rehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2014-03-18 12:39:49 +0100

    refactored rsem and ncpro

--- lib/RSEMApp.rb
+++ lib/RSEMApp.rb
@@ -3,6 +3,8 @@
 Version = '20131128-084622'
 
 require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
 
 class RSEMApp < SushiFabric::SushiApp
   def initialize
@@ -20,10 +22,7 @@ EOS
     @params['cores'] = '8'
     @params['ram'] = '16'
     @params['scratch'] = '100'
-    @params['build'] = {'select'=>''}
-    Dir["/srv/GT/reference/*/*/*"].sort.select{|build| File.directory?(build)}.each do |dir|
-      @params['build'][dir.gsub(/\/srv\/GT\/reference\//,'')] = File.basename(dir)
-    end
+    @params['build'] = ref_selector
     @params['paired'] = false
     @params['strandMode'] = ['both', 'sense', 'antisense']
     @params['featureFile'] = 'genes.gtf'
@@ -40,24 +39,35 @@ EOS
       @required_columns << 'Read2'
     end
   end
+  def set_default_parameters
+    @params['paired'] = dataset_has_column?('Read2')
+  end
   def next_dataset
     if @params['keepBam']
       {'Name'=>@dataset['Name'], 
        'Count [File]'=>File.join(@result_dir, "#{@dataset['Name']}.txt"),
        'BAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}.bam"), 
        'BAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}.bam.bai"),
-       'Build'=>@params['build'],
        'Species'=>@dataset['Species'],
-       'Feature Level'=>'isoform'
-      }
+       'build'=>@params['build'],
+       'featureLevel'=>'isoform',
+       'featureFile'=>@params['featureFile'],
+       'strandMode'=>@params['strandMode'],
+       'paired'=>@params['paired'],
+       'Read Count'=>@dataset['Read Count']
+      }.merge factor_dataset
     else 
       {'Name'=>@dataset['Name'],
        'Count [File]'=>File.join(@result_dir, "#{@dataset['Name']}.txt"),
-       'Build'=>@params['build'],
        'Species'=>@dataset['Species'],
-       'Feature Level'=>'isoform'
-      }
-   end
+       'build'=>@params['build'],
+       'featureLevel'=>'isoform',
+       'featureFile'=>@params['featureFile'],
+       'strandMode'=>@params['strandMode'],
+       'paired'=>@params['paired'],
+       'Read Count'=>@dataset['Read Count']
+      }.merge factor_dataset 
+    end
   end
   def commands
     command = "/usr/local/ngseq/bin/R --vanilla --slave << EOT\n"
@@ -85,35 +95,7 @@ EOS
 end
 
 if __FILE__ == $0
-  usecase = RSEMApp.new
-
-  usecase.project = "p1001"
-  usecase.user = 'masamasa'
-
-  # set user parameter
-  # for GUI sushi
-  #usecase.params['process_mode'].value = 'SAMPLE'
-  usecase.params['build'] = 'mm10'
-  usecase.params['paired'] = true
-  usecase.params['strandMode'] = 'both'
-  usecase.params['cores'] = 8
-  usecase.params['node'] = 'fgcz-c-048'
-
-  # also possible to load a parameterset csv file
-  # mainly for CUI sushi
-  #usecase.parameterset_tsv_file = 'tophat_parameterset.tsv'
-  #usecase.parameterset_tsv_file = 'test.tsv'
-
-  # set input dataset
-  # mainly for CUI sushi
-  #usecase.dataset_tsv_file = 'tophat_dataset.tsv'
-
-  # also possible to load a input dataset from Sushi DB
-  usecase.dataset_sushi_id = 1
-
-  # run (submit to workflow_manager)
-  #usecase.run
-  usecase.test_run
+  run RSEMApp
 
 end
 
