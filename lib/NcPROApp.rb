a94fea6386eca64d2b795cfba9fe4b49ae45f0aa
Hubert Rehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2014-03-18 12:39:49 +0100

    refactored rsem and ncpro

--- lib/NcPROApp.rb
+++ lib/NcPROApp.rb
@@ -2,6 +2,8 @@
 # encoding: utf-8
 
 require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
 
 class NcPROApp < SushiFabric::SushiApp
   def initialize
@@ -14,20 +16,18 @@ Annotation and Profiling of ncRNAs in smallRNA-seq<br/>
 Uses ncPRO-seq for a complete analysis of small-RNA-seq. ncPRO-seq considers performs quality assessment and quantiation.
 It considers various species of small RNA.<br />
 <a href='https://ncpro.curie.fr/index.html'>https://ncpro.curie.fr/index.html/</a>
+ncPRO can only process single-end stranded RNA.
 EOS
 
     @required_columns = ['Name','Read1','Species']
-    @required_params = ['name', 'cores', 'ram', 'scratch', 'paired']
+    @required_params = ['name', 'cores', 'ram', 'scratch']
 
     @params['cores'] = '8'
     @params['ram'] = '16'
     @params['scratch'] = '100'
-    @params['build'] = {'select'=>''}
-    Dir["/srv/GT/reference/*/*/*"].sort.select{|build| File.directory?(build)}.each do |dir|
-      @params['build'][dir.gsub(/\/srv\/GT\/reference\//,'')] = File.basename(dir)
-    end
-    @params['paired'] = false
-    @params['paired', 'description'] = 'whether the reads are paired end; must be false since ncPRO-seq does not support paired-end'
+    @params['build'] = ref_selector
+    #@params['paired'] = false
+    #@params['paired', 'description'] = 'whether the reads are paired end; must be false since ncPRO-seq does not support paired-end'
     @params['name'] = 'ncPRO_Result'
   end
   def preprocess
@@ -39,6 +39,8 @@ EOS
     report_file = File.join(@result_dir, @params['name'])
     report_link = File.join(report_file, 'ncpro/report.html')
     {'Name'=>@params['name'],
+     'Species'=>@dataset['Species'],
+     'build'=>@params['build'],
      'Report [File]'=>report_file,
      'Html [Link]'=>report_link,
     }
@@ -65,34 +67,7 @@ EOS
 end
 
 if __FILE__ == $0
-  usecase = NcPROApp.new
-
-  usecase.project = "p1001"
-  usecase.user = "masa"
-
-  # set user parameter
-  # for GUI sushi
-  #usecase.params['process_mode'].value = 'SAMPLE'
-  #usecase.params['build'] = 'TAIR10'
-  #usecase.params['paired'] = true
-  #usecase.params['cores'] = 2
-  #usecase.params['node'] = 'fgcz-c-048'
-
-  # also possible to load a parameterset csv file
-  # mainly for CUI sushi
-  usecase.parameterset_tsv_file = 'tophat_parameterset.tsv'
-  #usecase.params['name'] = 'name'
-
-  # set input dataset
-  # mainly for CUI sushi
-  usecase.dataset_tsv_file = 'tophat_dataset.tsv'
-
-  # also possible to load a input dataset from Sushi DB
-  #usecase.dataset_sushi_id = 1
-
-  # run (submit to workflow_manager)
-  #usecase.run
-  usecase.test_run
+  run NcPROApp
 
 end
 
