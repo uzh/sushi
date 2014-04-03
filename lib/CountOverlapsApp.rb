c99972640f64357ef56a3b2b33d581dbbf899246
Hubert Rehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2014-04-03 13:48:38 +0200

    fix counting problems

--- lib/CountOverlapsApp.rb
+++ lib/CountOverlapsApp.rb
@@ -6,10 +6,10 @@ require 'sushi_fabric'
 require_relative 'global_variables'
 include GlobalVariables
 
-class HTSeqApp < SushiFabric::SushiApp
+class CountOverlapsApp < SushiFabric::SushiApp
   def initialize
     super
-    @name = 'HTSeq'
+    @name = 'CountOverlaps'
     @analysis_category = 'Count'
     @required_columns = ['Name','BAM','BAI', 'build']
     @required_params = ['build','paired', 'strandMode']
@@ -69,42 +69,13 @@ class HTSeqApp < SushiFabric::SushiApp
     output.keys.each do |key|
       command << "output[['#{key}']] = '#{output[key]}'\n" 
     end
-    command << "countHTSeq(input=input, output=output, config=config)\n"
+    command << "countBamHits(input=input, output=output, config=config)\n"
     command << "EOT"
     command
   end
 end
 
 if __FILE__ == $0
-  usecase = HTSeqApp.new
-
-  usecase.project = "p1001"
-  usecase.user = 'masamasa'
-
-  # set user parameter
-  # for GUI sushi
-  #usecase.params['process_mode'].value = 'SAMPLE'
-  usecase.params['build'] = 'mm10'
-  usecase.params['paired'] = true
-  usecase.params['strandMode'] = 'both'
-  usecase.params['cores'] = 8
-  usecase.params['node'] = 'fgcz-c-048'
-
-  # also possible to load a parameterset csv file
-  # mainly for CUI sushi
-  #usecase.parameterset_tsv_file = 'tophat_parameterset.tsv'
-  #usecase.parameterset_tsv_file = 'test.tsv'
-
-  # set input dataset
-  # mainly for CUI sushi
-  #usecase.dataset_tsv_file = 'tophat_dataset.tsv'
-
-  # also possible to load a input dataset from Sushi DB
-  usecase.dataset_sushi_id = 3
-
-  # run (submit to workflow_manager)
-  usecase.run
-  #usecase.test_run
 
 end
 
