ab276a913d8e52d8959750be6bd915c473b678c3
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2014-04-04 14:03:21 +0200

    Added wc command option to WordCount.sh

--- lib/WordCount.sh
+++ lib/WordCount.sh
@@ -2,10 +2,11 @@
 
 #@name = 'Word_Count'
 #@analysis_category = 'Stats'
+#@params['count_option'] = ['-c', '-l', '-m', '-w']
 #@required_columns = ['Name', 'Read1']
-#@required_params = []
-#@next_dataset = ['Name', 'Name', 'Stats[File]', 'Name.stats']
+#@required_params = ['count_option']
+#@next_dataset = ['Name'=>'Name', 'Stats[File]'=>'Name.stats']
 
-gunzip -c $GSTORE_DIR/$Read1 |wc > $Name.stats
+gunzip -c $GSTORE_DIR/$Read1 |wc $count_option > $Name.stats
 
 
