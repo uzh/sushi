861753be2178b8fe2f17e4b88c056f52a31c29aa
Lennart Opitz <lopitz@fgcz-s-034.fgcz-net.unizh.ch>
2014-08-28 11:57:43 +0200

    new version of qualimap don't store huge coverage file anymore

--- lib/DnaBamStatsApp.rb
+++ lib/DnaBamStatsApp.rb
@@ -46,7 +46,7 @@ PICARD_DIR=#{GlobalVariables::PICARD_DIR}
 $SAMTOOLS view -ub #{File.join(@gstore_dir, @dataset['BAM'])} | $SAMSTAT -f bam -n #{@dataset['Name']}.samstat
 ###qualimap
 unset DISPLAY ; $QUALIMAP bamqc -bam  #{File.join(@gstore_dir, @dataset['BAM'])} -c -nt #{@params['cores']} --java-mem-size=10G -outdir #{@dataset['Name']}
-rm #{@dataset['Name']}/coverage.txt
+#rm #{@dataset['Name']}/coverage.txt
 echo Display=$DISPLAY
 ###picard 
 /usr/bin/java -Xmx10g -jar $PICARD_DIR/CollectGcBiasMetrics.jar OUTPUT=#{@dataset['Name']}.gc.dat SUMMARY_OUTPUT=#{@dataset['Name']}.gc.sum.dat INPUT=#{File.join(@gstore_dir, @dataset['BAM'])} CHART_OUTPUT=#{@dataset['Name']}.picard.pdf ASSUME_SORTED=#{@params['sortedBam']} REFERENCE_SEQUENCE=$REF VALIDATION_STRINGENCY=LENIENT
