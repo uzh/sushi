2d83fc791aceb86719898e6555e9d4787c87fe7a
Lennart Opitz <lopitz@fgcz-s-034.fgcz-net.unizh.ch>
2014-09-01 11:21:53 +0200

    implementing select option in teqc and fastqscreen app

--- lib/TeqcApp.rb
+++ lib/TeqcApp.rb
@@ -16,8 +16,14 @@ class TeqcApp <  SushiFabric::SushiApp
     @params['scratch'] = '100'
     @params['paired'] = false
     @params['name'] = 'TEQC_Result'
-    @params['designFile'] = '/srv/GT/databases/targetEnrichment_designs/SureSelect_HumanAllExon_V5_UTR/S04380219_Covered.bed'
-    @params['designFile', 'description'] = 'set full path to designFile according to the used kit'
+    @params['designFile'] = {'select'=>''}
+#'/srv/GT/databases/targetEnrichment_designs/SureSelect_HumanAllExon_V5_UTR/S04380219_Covered.bed'
+#    @params['designFile', 'description'] = 'set full path to designFile according to the used kit'
+    Dir["/srv/GT/databases/targetEnrichment_designs/*"].sort.select{|design| File.directory?(design)}.each do |dir|
+      @params['designFile'][File.basename(dir)] = File.basename(dir)
+    end
+
+
     @params['cmdOptions'] = ""
     @params['mail'] = ""
   end
