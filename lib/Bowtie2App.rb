241b193d47e387a53ff4a3eece3fa746c849b200
Giancarlo Russo <grusso@fgcz-s-034.fgcz-net.unizh.ch>
2014-09-02 10:23:22 +0200

    Var and ALL Paths

--- lib/Bowtie2App.rb
+++ lib/Bowtie2App.rb
@@ -23,7 +23,7 @@ EOS
     @params['ram'] = '16'
     @params['scratch'] = '100'
     @params['build'] = ref_selector
-    @params['build', 'description'] = 'the genome build and annotation to use as reference. If human variant calling is the main goal, please use hg_19_kariotypic.'
+    @params['build', 'description'] = 'the genome build and annotation to use as reference. If human variant calling is the main goal, please use hg_19_karyotypic.'
     @params['paired'] = false
     @params['paired', 'description'] = 'whether the reads are paired end; if false then only Read1 is considered even if Read2 is available.'
     @params['cmdOptions'] = ''
