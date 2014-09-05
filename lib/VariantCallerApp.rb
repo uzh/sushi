519436a147cd4dd6e113d8bcf93911f1513e4446
Giancarlo Russo <grusso@fgcz-s-024>
2014-09-05 12:17:39 +0200

    GR

--- lib/VariantCallerApp.rb
+++ lib/VariantCallerApp.rb
@@ -220,7 +220,7 @@ fi
 if [ $ANN == "true" ]; then 
 snpEffDir=/srv/GT/reference/"#{@params['build']}"/Genes/snpEff
 mkdir -p $snpEffDir
-sed s/"DIRECTORY_FOR_DATA"/"\/srv\/GT\/reference\/$BUILD\/Genes\/snpEff"/g $SNPEFF_DIR/snpEff.config > $snpEffDir/snpEff.config
+sed s/"DIRECTORY_FOR_DATA"/"\/srv\/GT\/reference\/#{@params\['build'\]}\/Genes\/snpEff"/g $SNPEFF_DIR/snpEff.config > $snpEffDir/snpEff.config
 
    ## CHECK IF DATABASE EXISTS AND CREATE IT IF NOT ###
     while [ ! -f "$snpEffDir/*/snpEffectPredictor.bin"  ]
