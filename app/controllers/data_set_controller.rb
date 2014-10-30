8535bde5ab5903414fcd4f16c47ece0ee3410771
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2014-10-30 15:57:50 +0100

    Added wfm_delete_command (WorkflowManager-0.1.2)

--- app/controllers/data_set_controller.rb
+++ app/controllers/data_set_controller.rb
@@ -296,11 +296,7 @@ class DataSetController < ApplicationController
 
       # delete data in gstore
       if @sample_path.first
-        @command = if @fgcz
-                     "g-req remove #{File.join(SushiFabric::GSTORE_DIR, @sample_path.first)}"
-                   else
-                     "rm -rf #{File.join(SushiFabric::GSTORE_DIR, @sample_path.first)}"
-                   end
+        @command = "wfm_delete_target -f #{File.join(SushiFabric::GSTORE_DIR, @sample_path.first)} -d #{SushiFabric::WORKFLOW_MANAGER}"
         if @option[:delete] == 'also_gstore'
           @command_log = `#{@command}`
           if request = @command_log.split and request_no = request[4]
