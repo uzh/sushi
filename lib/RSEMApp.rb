b10776aedb46819c97a10b6aba2a7f6e71884e7f
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2015-01-26 22:48:03 +0100

    Integrated global variables file, /usr/local/ngseq/opt/sushi_scripts/GLOBAL_VARIABLES.txt

--- lib/RSEMApp.rb
+++ lib/RSEMApp.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-Version = '20150105-102801'
+Version = '20150126-164832'
 
 require 'sushi_fabric'
 require_relative 'global_variables'
@@ -75,7 +75,7 @@ EOS
   end
   def commands
     command = "/usr/local/ngseq/bin/R --vanilla --slave << EOT\n"
-    command << "R_SCRIPT_DIR <<- '#{GlobalVariables::R_SCRIPT_DIR}'\n"
+    command << "R_SCRIPT_DIR <<- '#{R_SCRIPT_DIR}'\n"
     command<<  "source(file.path(R_SCRIPT_DIR, 'init.R'))\n"
     command << "config = list()\n"
     config = @params
