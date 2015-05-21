f31f9096ee72dda8878050d3530b131418e3781e
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2015-05-21 14:19:04 +0200

    Fixed sushi_application#refresh when sushi application is removed

--- app/controllers/sushi_application_controller.rb
+++ app/controllers/sushi_application_controller.rb
@@ -3,6 +3,8 @@ class SushiApplicationController < ApplicationController
     @sushi_apps = SushiApplication.all.sort_by{|app| app.analysis_category}
   end
   def refresh
+
+    # new check
     sushi_apps = all_sushi_applications
     lib_dir = File.expand_path('../../../lib', __FILE__)
     sushi_apps.select{|app| app =~ /\.rb$/}.each do |app|
@@ -23,6 +25,14 @@ class SushiApplicationController < ApplicationController
         sushi_app_entry.save
       end
     end
+
+    # delete check
+    delete_apps = SushiApplication.all.map{|app| app.class_name} - sushi_apps.map{|app| app.gsub(/\.rb/,'').gsub(/\.sh/,'')}
+    delete_apps.each do |class_name|
+      app = SushiApplication.find_by_class_name(class_name)
+      SushiApplication.delete(app)
+    end
+
     @sushi_apps = SushiApplication.all.sort_by{|app| app.analysis_category}
     render action: "index"
   end
