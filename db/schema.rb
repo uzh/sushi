310255fb1f8595166722845470344879de78d4f3
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2015-05-21 11:27:43 +0200

    Added SushiApplication model and saved sushi app list in DB, migrate is needed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150227095020) do
+ActiveRecord::Schema.define(:version => 20150521082651) do
 
   create_table "data_sets", :force => true do |t|
     t.integer  "project_id"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(:version => 20150227095020) do
     t.integer  "data_set_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "sushi_applications", :force => true do |t|
+    t.string   "class_name"
+    t.string   "analysis_category"
+    t.text     "required_columns"
+    t.text     "next_dataset_keys"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
   end
 
   create_table "users", :force => true do |t|
