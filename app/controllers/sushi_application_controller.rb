c261a0a7d8f97fdde4705da5dd734a3ff71b7ee9
Masa <masaomi.hatakeyama@fgcz.uzh.ch>
2015-05-21 14:09:12 +0200

    Fixed sushi_application#refresh when existing application is updated

--- app/controllers/sushi_application_controller.rb
+++ app/controllers/sushi_application_controller.rb
@@ -1,24 +1,21 @@
 class SushiApplicationController < ApplicationController
   def index
-    #@sushi_apps = all_sushi_applications.map{|app| eval(app.gsub(/\.rb/,'').gsub(/\.sh/,'')).new}
-    #@sushi_apps = all_sushi_applications.map{|app| ''}
-    #@sushi_apps = []
-    #@sushi_apps.each do |app|
-    #  app.instance_variable_set(:@dataset, {})
-    #end
-    #@sushi_apps = @sushi_apps.sort_by{|app| app.analysis_category}
-
     @sushi_apps = SushiApplication.all.sort_by{|app| app.analysis_category}
   end
   def refresh
     sushi_apps = all_sushi_applications
-    sushi_apps.each do |app|
-      class_name = app.gsub(/\.rb/,'').gsub(/\.sh/,'')
-      unless SushiApplication.find_by_class_name(class_name)
+    lib_dir = File.expand_path('../../../lib', __FILE__)
+    sushi_apps.select{|app| app =~ /\.rb$/}.each do |app|
+      class_name = app.gsub(/\.rb/,'')
+      updated_at = File.stat(File.join(lib_dir, app)).mtime
+      unless sushi_app = SushiApplication.find_by_class_name(class_name) and updated_at < sushi_app.updated_at
+        if sushi_app 
+          load File.join(lib_dir, app)
+        end
         sushi_app_instance = eval(class_name).new
         sushi_app_instance.instance_variable_set(:@dataset, {})
         sushi_app_instance.instance_variable_set(:@result_dir, '')
-        sushi_app_entry = SushiApplication.new
+        sushi_app_entry = (sushi_app || SushiApplication.new)
         sushi_app_entry.class_name = class_name
         sushi_app_entry.analysis_category = sushi_app_instance.analysis_category
         sushi_app_entry.required_columns = sushi_app_instance.required_columns
