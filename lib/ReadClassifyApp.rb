a403dbc8a6cdced43ccb445a019ce2f668901717
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2015-11-19 07:19:04 +0100

    Updated ReadClassifyApp.rb

--- lib/ReadClassifyApp.rb
+++ lib/ReadClassifyApp.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-Version = '20151118-093049'
+Version = '20151119-071823'
 
 require 'sushi_fabric'
 require_relative 'global_variables'
@@ -30,21 +30,33 @@ http://seselab.org/homeoroq/
     @params['ram'] = '16'
     @params['scratch'] = '100'
   end
+  def preprocess
+    @parent1_genome = if samp = @dataset_hash.first and ref_path = samp['refBuild1'] and dirs = ref_path.split('/') and spc = dirs.first and sub = spc.split('_')
+                spc[0] + sub.last[0,3]
+              else
+                'parent1_genome'
+              end
+    @parent2_genome = if samp = @dataset_hash.first and ref_path = samp['refBuild2'] and dirs = ref_path.split('/') and spc = dirs.first and sub = spc.split('_')
+                     spc[0] + sub.last[0,3]
+                   else
+                     'parent2_genome'
+                   end
+  end
   def next_dataset
     {'Name'=>@dataset['Name'], 
-     'Parent1OrigBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent1_genome_orig.bam"), 
-     'Parent1OrigBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent1_genome_orig.bam.bai"), 
-     'Parent1OtherBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent1_genome_other.bam"), 
-     'Parent1OtherBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent1_genome_other.bam.bai"), 
-     'Parent1CommonBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent1_genome_common.bam"), 
-     'Parent1CommonBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent1_genome_common.bam.bai"), 
+     'Parent1OrigBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent1_genome}_orig.bam"), 
+     'Parent1OrigBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent1_genome}_orig.bam.bai"), 
+     'Parent1OtherBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent1_genome}_other.bam"), 
+     'Parent1OtherBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent1_genome}_other.bam.bai"), 
+     'Parent1CommonBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent1_genome}_common.bam"), 
+     'Parent1CommonBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent1_genome}_common.bam.bai"), 
      'build1'=>@dataset['build1'],
-     'Parent2OrigBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent2_genome_orig.bam"), 
-     'Parent2OrigBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent2_genome_orig.bam.bai"), 
-     'Parent2OtherBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent2_genome_other.bam"), 
-     'Parent2OtherBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent2_genome_other.bam.bai"), 
-     'Parent2CommonBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent2_genome_common.bam"), 
-     'Parent2CommonBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_parent2_genome_common.bam.bai"), 
+     'Parent2OrigBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent2_genome}_orig.bam"), 
+     'Parent2OrigBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent2_genome}_orig.bam.bai"), 
+     'Parent2OtherBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent2_genome}_other.bam"), 
+     'Parent2OtherBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent2_genome}_other.bam.bai"), 
+     'Parent2CommonBAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent2_genome}_common.bam"), 
+     'Parent2CommonBAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}_#{@parent2_genome}_common.bam.bai"), 
      'build2'=>@dataset['build2'],
      'Species'=>@dataset['Species'],
     }.merge(extract_column("Factor")).merge(extract_column("B-Fabric"))
@@ -54,11 +66,13 @@ http://seselab.org/homeoroq/
     bam2 = File.join(@gstore_dir, @dataset['BAM2'])
     sam1 = File.join("tmp", File.basename(bam1).gsub(/.bam/, '_parent1.sam'))
     sam2 = File.join("tmp", File.basename(bam2).gsub(/.bam/, '_parent2.sam'))
-    out1_prefix = "#{@dataset['Name']}_parent1_genome"
-    out2_prefix = "#{@dataset['Name']}_parent2_genome"
+    out1_prefix = "#{@dataset['Name']}_#{@parent1_genome}"
+    out2_prefix = "#{@dataset['Name']}_#{@parent2_genome}"
     command = "mkdir tmp\n"
     command << "grep 'version' /usr/local/ngseq/stow/read_classify-2.1.0/bin/read_classify.py\n"
     command << "/usr/local/ngseq/bin/python --version\n"
+    command << "export PYTHONPATH=$PYTHONPATH:/usr/local/ngseq/lib/python2.7:/usr/local/ngseq/lib/python2.7/dist-packages\n"
+    command << "env|grep PYTHON\n"
     command << "samtools view -h #{bam1} > #{sam1}\n"
     command << "samtools view -h #{bam2} > #{sam2}\n"
     command << "/usr/local/ngseq/bin/python /usr/local/ngseq/stow/read_classify-2.1.0/bin/read_classify.py #{sam1} #{sam2} #{out1_prefix} #{out2_prefix}\n"
