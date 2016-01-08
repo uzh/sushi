0e8d6ce948d86d8cbe279ce33bc7608b6a12e679
Lennart Opitz <lopitz@fgcz.ethz.ch>
2016-01-08 14:37:39 +0100

    change memory settings ncpro

--- lib/NcPROApp.rb
+++ lib/NcPROApp.rb
@@ -24,7 +24,7 @@ EOS
     @required_params = ['name', 'cores', 'ram', 'scratch']
 
     @params['cores'] = '8'
-    @params['ram'] = '16'
+    @params['ram'] = '40'
     @params['scratch'] = '100'
     @params['refBuild'] = ref_selector
     #@params['paired'] = false
