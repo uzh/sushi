a689da2295be6b039c9afc970f0720fcd35d3d4b
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2016-04-14 08:47:18 +0200

    add a simplified tophat example for the documentation

---
+++ public/examples/TophatExample.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+Version = '20140317-142505'
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class TophatExample < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'TophatExample'
+    @analysis_category = 'Map'
+    @description =<<-EOS
+TopHat is a fast splice junction mapper for RNA-Seq reads. 
+It aligns RNA-Seq using bowtie2, and then analyzes the mapping results to identify splice junctions between exons.<br />
+<a href='https://ccb.jhu.edu/software/tophat/manual.shtml'>https://ccb.jhu.edu/software/tophat/manual.shtml/</a>
+    EOS
+    @required_columns = ['Name','Read1','Species']
+    @required_params = ['build','paired']
+    # optional params
+    @params['cores'] = '8'
+    @params['ram'] = '16'
+    @params['scratch'] = '100'
+    @params['library_type'] = ['fr-unstranded', 'fr-firststrand', 'fr-secondstrand']
+    @params['library_type', 'description'] = 'strand specificity of hte library'
+    @params['paired'] = false
+    @params['paired', 'description'] = 'either the reads are paired-end or single-end'
+    @params['bowtie2_index'] = ['GRCh38/BOWTIE2Index/genome', 'hg19/BOWTIE2Index/genome', 'GRCm38/BOWTIE2Index/genome', 'mm10/BOWTIE2Index/genome']
+    @params['bowtie2_index', 'description'] = 'bowtie2 index of reference build'
+    @params['tophat_options'] = ""
+    @params['tophat_options', 'description' = "other options that will be directly pasted to tophats command line"
+  end
+  def set_default_parameters
+    @params['paired'] = dataset_has_column?('Read2')
+  end
+  def preprocess
+    if @params['paired']
+      @required_columns << 'Read2'
+    end
+  end
+  def next_dataset
+    {'Name'=>@dataset['Name'], 
+     'BAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}.bam"), 
+     'BAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}.bam.bai"),
+     'build'=>@params['build'],
+     'Species'=>@dataset['Species']
+    }
+  end
+  def bowtie2_index
+    @bowtie2_index= File.join('/srv/GT/reference', @params['build'], 'BOWTIE2Index/genome')
+  end
+  def num_threads
+    if @params['cores'].to_i > 1
+      "--num-threads #{@params['cores']}"
+    else
+      ""
+    end 
+  end
+  def commands
+    cmd = "tophat -o . #{num_threads} --library_type #{library_type} #{@params['tophat_options']} #{bowtie2_index} #{@gstore_dir}/#{@dataset['Read1']}"
+    if @params['paired']
+      cmd << ",#{@gstore_dir}/#{@dataset['Read2']}"
+    end
+    cmd << "\n"
+    cmd << "mv accepted_hits.bam #{@dataset['Name']}.bam\n"
+    cmd << "samtools index #{@dataset['Name']}.bam\n"
+    cmd
+  end
+end
