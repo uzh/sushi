68e77bdf7664b8326b6fdfa3a313b816802b3a7e
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2016-04-22 14:22:11 +0200

    add example apps

---
+++ master/public/examples/FastqcExample1.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+Version = '20140317-142505'
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class FastqcExample < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'FastqcExample'
+    @analysis_category = 'Map'
+    @description =<<-EOS
+
+    EOS
+    @required_columns = ['Name','Read1']
+    @required_params = ['paired']
+    # optional params
+    @params['cores'] = '8'
+    @params['ram'] = '16'
+    @params['scratch'] = '100'
+    @params['library_type'] = ['fr-unstranded', 'fr-firststrand', 'fr-secondstrand']
+    @params['paired'] = false
+    @params['paired', 'description'] = 'whether the reads are paired-end or single-end'
+  end
+  def set_default_parameters
+    @params['paired'] = dataset_has_column?('Read2')
+  end
+  def preprocess
+    if @params['paired']
+      @required_columns << 'Read2'
+    end
+  end
+  def next_dataset
+    {'Name'=>@dataset['Name'], 
+     'Report [Link]'=>File.join(@result_dir, "#{@dataset['Name']}/fastqc_report.html"), 
+     'ReportDir [File]'=>File.join(@result_dir, "#{@dataset['Name']}")
+    }
+  end
+  def commands
+    cmd = "fastqc --extract -o . -t #{@params['cores']} #{@dataset['Read1']}"
+    cmd << "\n"
+    fastqcDir = "#{File.basename(@dataset['Read1'].to_s).gsub('.fastq.gz','_fastqc')
+    cmd << "mv #{fastqcDir} #{@dataset['Name']}"
+    cmd << "\n"
+    cmd
+  end
+end
