16045a5a6778cdd69b40e32feb2fb6882046b286
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2016-05-17 16:32:41 +0200

    add app TranscriptCoverage

---
+++ master/lib/TranscriptCoverageApp.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+Version = '20150226-110907'
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class TranscriptCoverageApp < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'TranscriptCoverage'
+    @analysis_category = 'Profiles'
+ @description =<<-EOS
+    Compute the base-by-base coverage from transript alignment files.
+EOS
+    @required_columns = ['Name','trBAM','trBAI', 'refBuild']
+    @required_params = ['refBuild','paired', 'strandMode']
+    # optional params
+    @params['cores'] = '2'
+    @params['ram'] = '30'
+    @params['scratch'] = '10'
+    @params['refBuild'] = ref_selector
+    @params['paired'] = false
+    @params['strandMode'] = ['both', 'sense', 'antisense']
+    @params['refFeatureFile'] = 'genes.gtf'
+    @params['minReadLength'] = 28
+    @params['maxReadLength'] = 32
+    @params['getCoverageByReadLength'] = true
+    @params['readStartOnlyCoverage'] = true
+    @params['specialOptions'] = ''
+    @params['mail'] = ""
+  end
+  def set_default_parameters
+    @params['refBuild'] = @dataset[0]['refBuild']
+    if dataset_has_column?('refFeatureFile')
+      @params['refFeatureFile'] = @dataset[0]['refFeatureFile']
+    end
+    if dataset_has_column?('paired')
+      @params['paired'] = @dataset[0]['paired']
+    end                               
+    if dataset_has_column?('strandMode')
+      @params['strandMode'] = @dataset[0]['strandMode']
+    end                               
+  end
+
+  def next_dataset
+    {'Name'=>@dataset['Name'], 
+     'Count [File]'=>File.join(@result_dir, "#{@dataset['Name']}.txt"),
+     'Profiles [File]'=>File.join(@result_dir, "#{@dataset['Name']}-profiles.RData"),
+     'Species'=>@dataset['Species'],
+     'refBuild'=>@params['refBuild'],
+     'refFeatureFile'=>@params['refFeatureFile'],
+     'strandMode'=>@params['strandMode'],
+     'paired'=>@params['paired'],
+     'Read Count'=>@dataset['Read Count']
+    }.merge(extract_column("Factor")).merge(extract_column("B-Fabric"))
+  end
+  def commands
+    run_RApp("EzAppTranscriptCoverage")
+  end
+end
+
+if __FILE__ == $0
+
+end
+
