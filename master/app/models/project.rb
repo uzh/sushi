ba84aef6241ea0958b061a516ebe21d9c6d9f909
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2016-08-22 06:25:34 +0200

    Added bfabric button

--- master/app/models/project.rb
+++ master/app/models/project.rb
@@ -12,4 +12,15 @@ class Project < ActiveRecord::Base
     end
     flag
   end
+  def register_bfabric(op = 'new')
+    base = "public/register_sushi_dataset_into_bfabric"
+    check = "public/check_dataset_bfabric"
+    if SushiFabric::Application.config.fgcz? and File.exist?(base) and File.exist?(check)
+      self.data_sets.each do |data_set|
+        unless data_set.data_set # if it is the top node dataset (== suppose raw dataset)
+          data_set.register_bfabric(op)
+        end
+      end
+    end
+  end
 end
