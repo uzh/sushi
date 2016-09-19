73c5d89e1f6deb08f072784c11a56c28723ddb05
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2016-09-19 11:10:22 +0200

    Changed the sushi application list view using Ajax to draw the table after showing the view

--- master/app/controllers/sushi_application_controller.rb
+++ master/app/controllers/sushi_application_controller.rb
@@ -7,4 +7,8 @@ class SushiApplicationController < ApplicationController
     @sushi_apps = SushiApplication.all.sort_by{|app| app.analysis_category}
     render action: "index"
   end
+  def refresh_table
+    refresh_sushi_application
+    @sushi_apps = SushiApplication.all.sort_by{|app| app.analysis_category}
+  end
 end
