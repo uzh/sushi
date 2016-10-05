461048409b2e4aabcc52d9c24c7b8892ae809c7e
Lennart Opitz <lopitz@fgcz.ethz.ch>
2016-10-05 21:54:39 +0200

    add DNA Haplotyper

---
+++ master/lib/GatkDnaHaplotyperApp.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class GatkDnaHaplotyperApp <  SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'GatkDnaHaplotyper'
+    @params['process_mode'] = 'SAMPLE'
+    @analysis_category = 'Variants'
+    @description =<<-EOS
+Haplotype calling for DNA-seq<br/>
+<a href='https://www.broadinstitute.org/gatk/guide/tooldocs/org_broadinstitute_gatk_tools_walkers_haplotypecaller_HaplotypeCaller.php'>HaplotypeCaller</a>
+    EOS
+    @required_columns = ['Name','BAM','BAI', 'refBuild']
+    @required_params = ['name']
+    @params['cores'] = '8'
+    @params['ram'] = '50'
+    @params['scratch'] = '100'
+    @params['name'] = 'GATK_DnaVariants'
+    @params['build'] = ref_selector
+    @params['targetFile'] = '/srv/GT/analysis/lopitz/GATK-tutorial_data/intervals/test.bed'
+    @params['getRealignedBam'] = true
+    @params['specialOptions'] = ''
+    @params['mail'] = ""
+  end
+  def set_default_parameters
+    @params['refBuild'] = @dataset[0]['refBuild']
+  end
+  def next_dataset
+    {'Name'=>@dataset['Name'],
+     'GVCF [File]'=>File.join(@result_dir, "#{@dataset['Name']}-HC_calls.g.vcf"),
+      if @params['getRealignedBam']{
+          'BAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}-realigned.bam"),
+          'BAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}-realigned.bam.bai"),
+     }
+     'Species'=>@dataset['Species'],
+     'refBuild'=>@params['refBuild']
+    }.merge(extract_column("Factor")).merge(extract_column("B-Fabric"))
+  end
+  def commands
+    run_RApp("EzAppGatkDnaHaplotyper")
+  end
+end
+
+if __FILE__ == $0
+
+end
