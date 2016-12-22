4bb75b53eca8d0b7de0d7e48016cf2526e11e1ca
Roman Briskine <roman.briskine@ieu.uzh.ch>
2016-12-22 14:49:37 +0100

    Add Kallisto and Salmon apps

---
+++ master/lib/KallistoApp.rb
@@ -0,0 +1,90 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class KallistoApp < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'Kallisto'
+    @analysis_category = 'Count'
+    @description =<<-EOS
+    <a href="https://pachterlab.github.io/kallisto/about">kallisto</a> is a program for quantifying abundances of transcripts from RNA-Seq data. It is based on the novel idea of pseudoalignment for rapidly determining the compatibility of reads with targets, without the need for alignment.
+EOS
+    @required_columns = ['Name','Read1','Species']
+    @required_params = ['paired', 'strandMode']
+    # optional params
+    @params['cores'] = '8'
+    @params['ram'] = '30'
+    @params['scratch'] = '100'
+    @params['refBuild'] = ref_selector
+    @params['paired'] = false
+    @params['strandMode'] = ['both', 'sense', 'antisense']
+    @params['refFeatureFile'] = 'genes.gtf'
+    @params['bootstrap-samples'] = 10
+    @params['bootstrap-samples', 'description'] = 'number of bootstrap samples'
+    @params['seed'] = 42
+    @params['seed', 'description'] = 'seed for the bootstrap sampling'
+    @params['fragment-length'] = 0
+    @params['fragment-length', 'description'] = 'estimated average fragment length (required for single-end reads but should be set to 0 for paired-end reads)'
+    @params['sd'] = 0
+    @params['sd', 'description'] = 'estimated fragment length standard deviation (required for single-end reads but should be set to 0 for paired-end reads)'
+    @params['bias'] = true
+    @params['bias', 'description'] = 'perform sequence based bias correction'
+    @params['pseudobam'] = false
+    @params['pseudobam', 'description'] = 'generate a bam file with pseudoalignments'
+    @params['trimAdapter'] = true
+    @params['trimLeft'] = 0
+    @params['trimRight'] = 0
+    @params['minTailQuality'] = 0
+    @params['minAvgQuality'] = 20
+    @params['mail'] = ""
+  end
+  def preprocess
+    if @params['paired']
+      @required_columns << 'Read2'
+    else
+      @required_params << 'fragment-length' << 'sd'
+    end
+  end
+  def set_default_parameters
+    @params['paired'] = dataset_has_column?('Read2')
+    if dataset_has_column?('strandMode')
+      @params['strandMode'] = @dataset[0]['strandMode']
+    end
+  end
+  def next_dataset
+    ds = {
+      'Name'=>@dataset['Name'], 
+      'Count [File]'=>File.join(@result_dir, "#{@dataset['Name']}.txt"),
+      'bootstrappedCount [File]'=>File.join(@result_dir, "#{@dataset['Name']}.h5"),
+      'runInfo [File]'=>File.join(@result_dir, "#{@dataset['Name']}.json")
+    }
+    if @params['pseudobam']
+      ds.merge!(
+       'BAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}.bam"), 
+       'BAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}.bam.bai")
+      )
+    end
+    ds.merge(
+      'Species'=>@dataset['Species'],
+      'refBuild'=>@params['refBuild'],
+      'featureLevel'=>'isoform',
+      'refFeatureFile'=>@params['refFeatureFile'],
+      'strandMode'=>@params['strandMode'],
+      'paired'=>@params['paired'],
+      'Read Count'=>@dataset['Read Count']
+    ).merge(extract_column("Factor")).merge(extract_column("B-Fabric"))
+  end
+  def commands
+    run_RApp("EzAppKallisto")
+  end
+end
+
+if __FILE__ == $0
+  run KallistoApp
+
+end
+
