b886aa1c8fd4934e5479f80703ea6a1be536e072
weihongqi <weihong.qi@fgcz.ethz.ch>
2017-03-18 14:26:00 +0100

    Add ReseqApp, delete ResequencingApp

--- master/lib/ReseqApp.rb
+++ master/lib/ReseqApp.rb
@@ -1,33 +1,30 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-Version = '20170313'
+Version = '20170318'
 
 require 'sushi_fabric'
 require_relative 'global_variables'
 include GlobalVariables
 
-class ResequencingApp < SushiFabric::SushiApp
+class ReseqApp < SushiFabric::SushiApp
   def initialize
     super
-    @name = 'Resequencing'
+    @name = 'Reseq'
     @analysis_category = 'Map'
     @description =<<-EOS
-Resequencing pipeline (mapping, variant calling and consensusing from SMRT Analysis
+Resequencing pipeline (read alignment, variant calling, consensus polishing) from SMRT Analysis
 <a href='https://github.com/PacificBiosciences/SMRT-Analysis/wiki'>https://github.com/PacificBiosciences/SMRT-Analysis/wiki</a>
 EOS
     
     @required_columns = ['Name','Reads']
-    @required_params = ['RefFile']
-    @params['RefFile'] = {'select'=>''}
-    @params['RefFile'][lambda] = "common/userdata.d/references/lambda"
-    @params['RefFile'][ecoli] = "common/userdata.d/references/ecoli"
-    @params['RefFile'][bsubtilis] = "common/userdata.d/references/B_subtilis_v1"
-    @params['RefFile'][yeast] = "common/userdata.d/references/s_cerevisiae"
-    @params['RefFile'][yhalotolerans] = "common/userdata.d/references/yhalotolerans"
-    # optional params
+    @required_params = ['refFile']
+    @params['refFile'] = {'select'=>''}
+    Dir["/misc/ngseq8/opt/smrtanalysis.2.3.0/install/smrtanalysis_2.3.0.140936/common/userdata.d/references/*"].sort.select{|dir| File.directory?(dir)}.each do |dir|
+      @params['refFile'][File.basename(dir)] = File.basename(dir)
+    end
     @params['cores'] = '8'
-    @params['ram'] = '50'
-    @params['scratch'] = '400'
+    @params['ram'] = '30'
+    @params['scratch'] = '100'
     @params['minSubReadLength'] = '50'
     @params['minSubReadLength', 'description'] = 'Minimum Subread Length. Subreads shorter than this value (in bps) are filtered out and excluded from pre-assembly.'
     @params['readScore'] = '75'
@@ -47,8 +44,8 @@ EOS
      'VCF [File]'=>File.join(@result_dir, "#{@dataset['Name']}.vcf.gz"),
      'VCFINDEX [File]'=>File.join(@result_dir, "#{@dataset['Name']}.vcf.gz.tbi"),
      'Consensus [File]'=>File.join(@result_dir, "#{@dataset['Name']}.polished.fasta"),	
-     'ResequencingOut [File]'=>File.join(@result_dir, "#{@dataset['Name']}"),  
-  }.merge(extract_column("Factor")).merge(extract_column("B-Fabric"))
+     'ResequencingOut [File]'=>File.join(@result_dir, "#{@dataset['Name']}"),   
+    }.merge(extract_column("Factor")).merge(extract_column("B-Fabric"))
   end
   def commands
     run_RApp("EzAppResequencing")
@@ -56,7 +53,7 @@ EOS
 end
 
 if __FILE__ == $0
-  run ResequencingApp
+  run ReseqApp
   #usecase = Bowtie2App.new
 
   #usecase.project = "p1001"
