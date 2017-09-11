de9c0acb17c2bfcc8a596cd29ff4db8b7cc9a8cd
Giancarlo Russo <grusso@fgcz-net.unizh.ch>
2017-09-11 17:03:05 +0200

    introducing link to index00.html (8)

--- master/lib/SubreadsApp.rb
+++ master/lib/SubreadsApp.rb
@@ -32,7 +32,7 @@ EOS
     @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
   end
   def next_dataset
-    report_link = File.join(@result_dir, @params['name'], "index00.html")
+    report_link = File.join(@result_dir, @params['name'], "index.html")
     {'Name'=>@dataset['Name'], 
      'Read1 [File]'=>File.join(@result_dir, "#{@dataset['Name']}.filtered_subreads.fastq.gz"),	
      'Static Report [Link]'=>report_link,
