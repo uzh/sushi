1e44db43f3d1fa9d5667b01421383f02152a45db
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2017-11-19 22:20:22 +0100

    Added samtools module in ReadClassifyApp

--- master/lib/ReadClassifyApp.rb
+++ master/lib/ReadClassifyApp.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-Version = '20170907-111728'
+Version = '20171119-221935'
 
 require 'sushi_fabric'
 require_relative 'global_variables'
@@ -31,6 +31,7 @@ http://seselab.org/homeoroq/
     @params['scratch'] = '100'
     @params['paired'] = true
     @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
+    @modules = ["Tools/samtools"]
   end
   def preprocess
     @parent1_genome = if samp = @dataset_hash.first and ref_path = samp['refBuild1'] and dirs = ref_path.split('/') and spc = dirs.first and sub = spc.split('_')
