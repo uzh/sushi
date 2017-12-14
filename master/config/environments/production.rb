7b56104f16b8261a8b891757440f6131ad1f3aa0
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2017-12-14 16:11:27 +0100

    Added config.course_users in config/environments/production.rb

--- master/config/environments/production.rb
+++ master/config/environments/production.rb
@@ -88,6 +88,20 @@ SushiFabric::Application.configure do
     config.sushi_app_dir = Dir.pwd
     config.module_source = "/usr/local/ngseq/etc/lmod_profile"
     config.course_mode = false
+    config.course_users = [
+                                [1000, 1000],
+                                [1001, 1001],
+                                [1002, 1002],
+                                [1003, 1003],
+                                [1004, 1004],
+                                [1005, 1005],
+                                [1006, 1006],
+                                [1007, 1007],
+                                [1008, 1008],
+                                [1009, 1009],
+                                [1010, 1010],
+                                ["----------", 1000]
+    ]
   end
 
 end
