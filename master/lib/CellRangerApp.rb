e492e7f8a466912badcb43d76483f21d22c9cb84
Lennart Opitz <lopitz@fgcz.ethz.ch>
2018-01-03 12:53:55 +0100

    add CellRangerApp

---
+++ master/lib/CellRangerApp.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class CellRangerApp <  SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'CellRangerCount'
+    @analysis_category = 'SingleCell'
+    @description =<<-EOS
+This wrapper runs <a href='https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/using/count',>cellranger count</a> in Single-library analysis mode.
+    EOS
+    @required_columns = ['Name','RawDataDir','Species']
+    @required_params = ['name']
+    @params['cores'] = '8'
+    @params['ram'] = '32'
+    @params['scratch'] = '100'
+    @params['name'] = 'CellRangerCount'
+    @params['reference'] = ''#ref_selector
+    @params['specialOptions'] = ''
+    @params['mail'] = ""
+    @modules = ["Dev/R"]
+    @inherit_tags = ["B-Fabric"]
+  end
+  def set_default_parameters
+    @params['Species'] = @dataset[0]['Species']
+  end
+  def next_dataset
+    report_dir = File.join(@result_dir, @params['name'])
+    dataset = {
+    'Name'=>@dataset['Name'],
+      'ResultDir [File]'=>report_dir,
+      'Species'=>@dataset['Species'],
+      'reference'=>@params['reference']
+    }.merge(extract_columns(@inherit_tags))
+    dataset
+  end
+  def commands
+    run_RApp("EzAppCellRanger")
+  end
+end
+
+if __FILE__ == $0
+
+end
