8765850103c53b6630cdf34606bcc37386f1085c
Ge Tan <gtan@me.com>
2018-01-29 13:08:00 +0100

    MACS2: update module loading

--- master/lib/MACS2App.rb
+++ master/lib/MACS2App.rb
@@ -32,7 +32,8 @@ Capturing the influence of genome complexity to evaluate the significance of enr
     @params['cmdOptions'] = '--nomodel --bw 200'
     @params['specialOptions'] = ''
     @params['mail'] = ''
-    @modules = ["Tools/UCSC", "Tools/BEDTools", "Dev/Python2", "Tools/MACS2", "Dev/R", "Tools/sambamba"]
+    @modules = ["Tools/UCSC", "Tools/BEDTools", "Dev/Python2", "Dev/R", "Tools/sambamba"]
+    # MACS2 is in Python2 bin
     @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
   end
   def set_default_parameters
