65ceaabf6531bf54928fd89a703658294117e1ae
Lennart Opitz <lopitz@fgcz.ethz.ch>
2018-05-14 15:36:03 +0200

    make r version selective in Teqc app

--- master/lib/TeqcApp.rb
+++ master/lib/TeqcApp.rb
@@ -31,9 +31,10 @@ Quality control for target capture experiments<br/>
     @params['covUniformityPlot'] = true
     @params['covTargetLengthPlot'] = true
     @params['duplicatesPlot'] = true
+    @params['Rversion'] = ["Dev/R/3.4.2", "Dev/R/3.5.0"]
     @params['cmdOptions'] = ""
     @params['mail'] = ""
-    @modules = ["Tools/samtools", "Dev/R", "Tools/sambamba"]
+    @modules = ["Tools/samtools", "Tools/sambamba"]
   end
  def set_default_parameters
    @params['refBuild'] = @dataset[0]['refBuild']
@@ -53,6 +54,7 @@ Quality control for target capture experiments<br/>
     }
   end
   def commands
+    command = "module load #{@params["Rversion"]}\n"
     run_RApp("EzAppTeqc")
   end
 end
