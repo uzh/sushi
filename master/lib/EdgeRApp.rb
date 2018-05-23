860fd2098224b0636fc9b1a0f3e9cf4f7f413686
Ge Tan <gtan@me.com>
2018-05-23 12:01:50 +0200

    Add Rversion to DESeq2 and EdgeR

--- master/lib/EdgeRApp.rb
+++ master/lib/EdgeRApp.rb
@@ -46,7 +46,7 @@ class EdgeRApp < SushiFabric::SushiApp
     @params['specialOptions'] = ''
     @params['expressionName'] = ''
     @params['mail'] = ""
-    @params['Rversion'] = ["Dev/R/3.4.2", "Dev/R/3.5.0"]
+    @params['Rversion'] = ["Dev/R/3.5.0", "Dev/R/3.4.2"]
     @modules = ["Tools/GFOLD", "Dev/PhantomJS"]
   end
    def preprocess
