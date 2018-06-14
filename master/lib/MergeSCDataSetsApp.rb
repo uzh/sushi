a063ce9d0d90f7fbfd6b2a5d91ea5c5bb083a2a9
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2018-06-14 13:57:46 +0200

    Added draft MergeSCDataSetsApp

---
+++ master/lib/MergeSCDataSetsApp.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class MergeSCDataSetsApp < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'MergeSCDS'
+    @description =<<-EOS
+Merging more than two DataSets generaged from same library for CellRangerApp<br /><br />
+Assuming that all other columns than file path are same between datasets.<br />
+<br />
+    EOS
+    @analysis_category = 'SingleCell'
+    @params['OrigDataSet'] = []
+    @params['TargetDataSet'] = []
+    @required_columns = ['Name', 'Read1', 'RawDataDir', 'Species']
+    @required_params = ['TargetDataSet']
+    @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
+  end
+  def next_dataset
+    {
+      'Name'=>@dataset['Name'],
+      'Read1 [File]'=>@dataset['Read1'],
+      'RawDataDir1 [File]'=>@dataset['RawDataDir'],
+      'Read2 [File]'=>@dataset['Read1'],
+      'RawDataDir2 [File]'=>@dataset['RawDataDir'],
+      'Species'=>@dataset['Species'],
+    }.merge(extract_columns(@inherit_tags))
+  end
+  def set_default_parameters
+    if data_set = DataSet.find_by_id(@dataset_sushi_id)
+      @params['OrigDataSet'] = {data_set.name => data_set.id}
+      @params['TargetDataSet'] = Hash[*data_set.project.data_sets.map{|d| [d.name, d.id]}.flatten].to_a.reverse
+    end
+  end
+  def preprocess
+    dataset_sushi_id = @params['TargetDataSet']
+    dataset = DataSet.find_by_id(dataset_sushi_id)
+    dataset_hash2 = {}
+    if dataset = DataSet.find_by_id(dataset_sushi_id.to_i)
+      dataset.samples.each do |sample|
+        name = sample.to_hash['Name']
+        dataset_hash2[name] = sample.to_hash
+      end
+    end
+    # here
+    # merge dataset_hash2 with @dataset_hash
+    dataset_hash1 = @dataset_hash.clone
+    dataset_hash1.each_with_index do |sample, i|
+      name = sample['Name']
+      @dataset_hash[i]['Read2 [File]'] = dataset_hash2[name]['Read1 [File]']
+      @dataset_hash[i]['RawDataDir2 [File]'] = dataset_hash2[name]['RawDataDir [File]']
+    end
+    @dataset_hash.sort_by!{|row| row['Name']}
+  end
+  def commands
+    coms = ""
+    coms << "echo '#{GlobalVariables::SUSHI}'\n"
+    coms << "echo '#{GlobalVariables::SUSHI}' > #{@dataset['Name']}_dummy.txt\n"
+    coms
+  end
+end
