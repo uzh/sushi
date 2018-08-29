60c5d4209020a6acc2fafc89c4e15d30fd3cee48
Giancarlo Russo <giancarlo.russo@fgcz.ethz.ch>
2018-08-29 14:11:36 +0200

    typo

--- master/lib/DESeq2App.rb
+++ master/lib/DESeq2App.rb
@@ -33,7 +33,8 @@ class DESeq2App < SushiFabric::SushiApp
     #@params['normMethod'] = 'logMean'
     @params['runGO'] = ['true', 'false']
     @params['grouping2'] = ''
-    @params['grouping2', 'description'] =  'specify the column name of your secondary factor (if there is one). Ensure the 
+    @params['grouping2', 'description'] =  'specify the column name of your secondary co-variate (factor or numeric, 
+    assuming there is one). Ensure the 
     column name is in the format "NAME [Factor]" or "NAME [Numeric]"'    @params['backgroundExpression'] = 10
     @params['backgroundExpression', "description"] = "counts to be added to shrink estimated log2 ratios"
     @params['transcriptTypes'] = ''
