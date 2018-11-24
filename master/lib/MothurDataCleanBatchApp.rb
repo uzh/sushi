ac93bbf8b3ecb9e1bb12f7cc88cd1509a9a71343
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2018-11-24 14:42:39 +0100

    complete mothur workflow in one app

--- master/lib/MothurDataCleanBatchApp.rb
+++ master/lib/MothurDataCleanBatchApp.rb
@@ -12,22 +12,26 @@ super
 @name = 'MothurDataCleanBatch'
 @analysis_category = 'Metagenomics'
 @description =<<-EOS
-OTU-based metagenomics analysis with Mothur.
+OTU-based metagenomics analysis with Mothur. Please make sure that the input files are from the same technology and adjust minLen and maxLen accordingly.
 <a href='https://mothur.org/wiki/MiSeq_SOP'>https://mothur.org/wiki</a>
   EOS
 @required_columns = ['Name', 'Read1']
-@required_params = ['cutOff', 'diffs']
+@required_params = ['cutOffTaxonomy', 'diffs','cutOffCluster']
 @params['cores'] = '1'
 @params['ram'] = '8'
 @params['scratch'] = '10'
-@params['cutOff'] = '80'
-@params['cutOff', 'description'] = 'Cut-off for taxonomy assignment in classify.seqs'
+@params['cutOffTaxonomy'] = '80'
+@params['cutOffTaxonomy', 'description'] = 'Cut-off for taxonomy assignment in classify.seqs'
 @params['diffs'] = '2'
-@params['diffs', 'description'] = 'Differences allowed in the pre.cluster step. Should be 1 every 100 bases.If the data are only Pacbio, it is ignored'
+@params['diffs', 'description'] = 'Differences allowed in the pre.cluster step. Should be 1 every 100 bases.'
 @params['minLen'] = '145'
 @params['minLen', 'description'] = 'Sequences shorter than this long are removed.'
 @params['maxLen'] = '330'
 @params['maxLen', 'description'] = 'Sequences longer than this are removed.'
+@params['cutOffCluster'] = '0.03'
+@params['cutOffCluster', 'description'] = 'Cut-off similarity to cluster OTUs'
+@params['referenceFasta'] = '/srv/GT/analysis/grusso/courses/metagenomicsCourse/references/bioPoolReference.16S.fasta'
+@params['referenceFasta', 'description'] = 'Full path to fasta file for the mock community (if available).'
 @params['mail'] = ""
 @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
 @modules = ["Dev/R"]
@@ -40,11 +44,24 @@ end
   def set_default_parameters
      @params['paired'] = dataset_has_column?('Read2')
   end
+  def preprocess
+    if @params['mockSample']
+      @required_columns << 'Mock'
+    end
+  end
+  def set_default_parameters
+     @params['mockSample'] = dataset_has_column?('Mock')
+  end
+  
 def next_dataset
-    {'Name'=>@dataset['Name'],
-     'CountTable [File]'=>File.join(@result_dir, "#{@dataset['Name']}.count.txt"),
+     {'Name'=>@dataset['Name'],
+     'ReadsCountTable [File]'=>File.join(@result_dir, "#{@dataset['Name']}.reads.count.txt"),
+     'ReadsTaxonomyFile [File]'=>File.join(@result_dir, "#{@dataset['Name']}.reads.to.tax.txt"),
      'PreClusteredFastaFile [File]'=>File.join(@result_dir, "#{@dataset['Name']}.preclustered.txt"),
-     'TaxonomyFile [File]'=>File.join(@result_dir, "#{@dataset['Name']}.taxonomy.txt"),
+     'OTUsCountTable [File]'=>File.join(@result_dir, "#{@dataset['Name']}.OTUs.count.txt"),
+     'OTUsToTaxonomyFile [File]'=>File.join(@result_dir, "#{@dataset['Name']}.OTUs.to.tax.txt"),
+     'ErrorFile [File]'=>File.join(@result_dir, "#{@dataset['Name']}.errorcount.txt"),
+     'stepConvergence [File]'=>File.join(@result_dir, "#{@dataset['Name']}.step.converge.txt"),
      }
 end
 def commands
