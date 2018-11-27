6833df451c4786da23fd1f3a848b4862ba8f1875
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2018-11-27 23:01:24 +0100

    output files

--- master/lib/MothurDataAnalysisReportApp.rb
+++ master/lib/MothurDataAnalysisReportApp.rb
@@ -27,7 +27,7 @@ def next_dataset
 @params['name'] = "MothurDataAnalysisReport"
 report_file = File.join(@result_dir, '00index_files')
 report_link = File.join(@result_dir, '00index.html')
-{'Name'=>@dataset['Name'],
+{'Name'=>@params['name'],
   'Report [File]'=>report_file,
   'Static Report [Link,File]'=>report_link,
 }
