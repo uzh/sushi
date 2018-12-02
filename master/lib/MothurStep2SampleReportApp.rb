5f9f8bc857b23dd3124e276a0561217501af976d
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2018-12-02 21:35:14 +0100

    added Mothur step 1 and 2 report apps

---
+++ master/lib/MothurStep2SampleReportApp.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+Version = '20181127-163024'
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class MothurStep2SampleReportApp < SushiFabric::SushiApp
+def initialize
+super
+@name = 'MothurStep2SampleReport'
+@analysis_category = 'Metagenomics'
+@description =<<-EOS
+Report of the data analysis performed with Mothur.
+EOS
+@params['process_mode'] = 'DATASET'
+@required_columns = ['Name','ChimeraPlot','PreClusteredAndChimeraSummary','stepConvergenceSummary','OTUsToTaxonomyFile','OTUsCountTable']
+@required_params = ['name']
+@params['cores'] = '1'
+@params['ram'] = '8'
+@params['scratch'] = '10'
+@params['mail'] = ""
+@params['name'] = "MothurStep2SampleReportApp"
+@inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
+@modules = ["Dev/R/3.5.1"]
+end
+def next_dataset
+report_file = File.join(@result_dir, '00index_files')
+report_link = File.join(@result_dir, '00index.html')
+{'Name'=>@params['name'],
+  'Report [File]'=>report_file,
+  'Static Report [Link,File]'=>report_link
+}
+end
+def commands
+run_RApp("EzAppMothurStep2SampleReport")
+end
+end
+
+if __FILE__ == $0
+end
