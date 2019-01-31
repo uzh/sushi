5a12a1cc1006595fd831b2c287fbc00487b76cd2
Ge Tan <gtan@me.com>
2019-01-31 22:16:05 +0100

    Add limma

---
+++ master/lib/LimmaApp.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+Version = '20180507-041245'
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class LimmaRApp < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'Limma'
+    @params['process_mode'] = 'DATASET'
+    @analysis_category = 'Differential_Expression'
+    @description =<<-EOS
+    Empirical analysis of digital gene expression data in R<br/>
+<a href='https://bioconductor.org/packages/release/bioc/html/limma.html'>limma</a><br/>
+    EOS
+    @required_columns = ['Name','Count', 'Species', 'refBuild', 'featureLevel', 'refFeatureFile']
+    @required_params = ['grouping', 'sampleGroup', 'refGroup']
+    # optional params
+    @params['cores'] = '1'
+    @params['ram'] = '2'
+    @params['scratch'] = '10'
+    @params['refBuild'] = ref_selector
+    @params['refFeatureFile'] = 'genes.gtf'
+    @params['featureLevel'] = ['gene', 'isoform']
+    @params['modelMethod'] = ['limma-trend', 'voom']
+    @params['modelMethod', 'description'] = 'Which model method to use'
+    @params['grouping'] = '' ### TODO: this should be filled by a column selector that allows to select a column with the tag 'Factor'
+    @params['sampleGroup'] = '' ## TODO: this should be a value from the selected column
+    @params['sampleGroup', 'description'] = 'sampleGroup should be different from refGroup'
+    @params['refGroup'] = '' ## TODO: this should be a value from the selected column
+    @params['refGroup', 'description'] = 'refGroup should be different from sampleGroup'
+    @params['onlyCompGroupsHeatmap'] = ['false', 'true']
+    @params['onlyCompGroupsHeatmap', 'description'] = 'Only show the samples from comparison groups in heatmap'
+    @params['runGO'] = ['true', 'false']
+    @params['grouping2'] = ''
+    @params['grouping2', 'description'] =  'specify the column name of your secondary co-variate  (factor or numeric, 
+    assuming there is one). Ensure the 
+    column name is in the format "NAME [Factor]" or "NAME [Numeric]"'
+    @params['backgroundExpression'] = 10
+    @params['backgroundExpression', "description"] = "counts to be added to shrink estimated log2 ratios"
+    @params['transcriptTypes'] = ''
+    @params['transcriptTypes', 'multi_selection'] = true
+    @params['transcriptTypes', 'selected'] = 0
+    @params['specialOptions'] = ''
+    @params['expressionName'] = ''
+    @params['mail'] = ""
+    @params['Rversion'] = ["Dev/R/3.5.1", "Dev/R/3.5.0", "Dev/R/3.4.2", "Dev/R/3.4.0", "Dev/R/3.3.0"]
+    @modules = ["Tools/GFOLD", "Dev/PhantomJS"]
+  end
+   def preprocess
+    @random_string = (1..12).map{[*('a'..'z')].sample}.join
+  end
+  def next_dataset
+    @comparison = "#{@params['sampleGroup']}--over--#{@params['refGroup']}"
+    @params['comparison'] = @comparison
+    @params['name'] = @comparison
+    report_file = File.join(@result_dir, "#{@params['comparison']}")
+    report_link = File.join(report_file, '00index.html')
+    {'Name'=>@comparison,
+     'Species'=>(dataset = @dataset.first and dataset['Species']),
+     'refBuild'=>@params['refBuild'],
+     'Static Report [Link]'=>report_link,
+     'Live Report [Link]'=>"#{SHINY_EXPLORE_DE}?data=#{report_file}/result-#{@comparison}-#{@random_string}-EzResult.RData",
+     'Report [File]'=>report_file,
+    }
+  end
+  def set_default_parameters
+    @params['refBuild'] = @dataset[0]['refBuild']
+    if dataset_has_column?('refFeatureFile')
+      @params['refFeatureFile'] = @dataset[0]['refFeatureFile']
+    end
+    @params['transcriptTypes'] = @dataset[0]['transcriptTypes'].to_s.split(',')
+  end
+  def commands
+    command = "module load #{@params["Rversion"]}\n"
+    command << run_RApp("EzAppLimma")
+  end
+end
+
+if __FILE__ == $0
+
+end
