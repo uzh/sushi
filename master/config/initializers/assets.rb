17004058eafb0ccb67a5203410eb5cad0adb64fc
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2019-02-12 14:47:57 +0100

    Updated some gems due to rails-html-sanitizer-1.0.4 dependencies error

--- master/config/initializers/assets.rb
+++ master/config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.precompile += %w( jquery.treeview.js )
 Rails.application.config.assets.precompile += %w( jquery.treeview.edit.js )
 Rails.application.config.assets.precompile += %w( jquery.treeview.async.js )
+Rails.application.config.assets.precompile += %w( chartkick.js )
