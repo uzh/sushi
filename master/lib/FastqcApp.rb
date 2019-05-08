0b995be35cf6859989cf06ffbdf22c8c78d0a464
Ge Tan <gtan@me.com>
2019-05-08 15:08:20 +0200

    Add picard module in fastqc

--- master/lib/FastqcApp.rb
+++ master/lib/FastqcApp.rb
@@ -28,7 +28,7 @@ EOS
     @params['name'] = 'FastQC_Result'
     @params['cmdOptions'] = ""
     @params['mail'] = ""
-    @modules = ["QC/FastQC", "Dev/R", "Tools/samtools", "Tools/sambamba"]
+    @modules = ["QC/FastQC", "Dev/R", "Tools/Picard", "Tools/samtools", "Tools/sambamba"]
   end
  def set_default_parameters
     @params['paired'] = dataset_has_column?('Read2')
