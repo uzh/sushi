1d231a0504be3bec7d3f859a4a194f576526a781
Ge Tan <gtan@me.com>
2019-05-13 10:27:16 +0200

    Add R versions to some apps

--- master/lib/SCReportMergingApp.rb
+++ master/lib/SCReportMergingApp.rb
@@ -47,6 +47,7 @@ class SCReportMergingApp < SushiFabric::SushiApp
     @params['all2allMarkers', 'description'] = 'Run all against all cluster comparisons?'
     @params['specialOptions'] = ''
     @params['mail'] = ""
+    @params['Rversion'] = ["Dev/R/3.6.0", "Dev/R/3.5.1", "Dev/R/3.5.0", "Dev/R/3.4.2", "Dev/R/3.4.0", "Dev/R/3.3.0"]
     @modules = ["Dev/R", "Dev/Python/3.6.8"]
   end
   def next_dataset
