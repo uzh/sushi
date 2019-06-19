9f9a25923396ba3d4dd528ff0952ca02af4dce87
Ge Tan <gtan@me.com>
2019-06-19 15:48:49 +0200

    Add CellRangerAggrApp

---
+++ master/lib/CellRangerAggrApp.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class CellRangerAggrApp <  SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'CellRangerAggr'
+    @params['process_mode'] = 'DATASET'
+    @analysis_category = 'SingleCell'
+    @description =<<-EOS
+This wrapper runs <a href='https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/using/count',>cellranger aggr</a> in multi-library analysis mode.
+EOS
+    @required_columns = ['Name','CountMatrix']
+    @required_params = ['name']
+    @params['cores'] = '1'
+    @params['ram'] = '4'
+    @params['scratch'] = '100'
+    @params['name'] = 'CellRangerAggr_Result'
+    @params['refBuild'] = ref_selector
+    @params['refFeatureFile'] = 'genes.gtf'
+    @params['featureLevel'] = 'gene'
+    @params['transcriptTypes'] = ''
+    @params['transcriptTypes', 'multi_selection'] = true
+    @params['transcriptTypes', 'selected'] = 0
+    @params['normalize'] = ['mapped', 'none']
+    @params['mail'] = ""
+    @modules = ["Dev/R"]
+  end
+  def set_default_parameters
+  end
+  def next_dataset
+    report_file = File.join(@result_dir, @params['name'])
+    {'Name'=>@params['name'],
+     'refBuild'=>@params['refBuild'],
+     'refFeatureFile'=>@params['refFeatureFile'],
+     'featureLevel'=>@params['featureLevel'],
+     'transcriptTypes'=>@params['transcriptTypes'],
+     'Report [Link]'=>File.join(report_file, 'web_summary.html')
+    }
+  end
+  def commands
+    run_RApp("EzAppCellRangerAggr")
+  end
+end
