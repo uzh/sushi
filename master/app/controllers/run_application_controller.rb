b41300c7ad8d992162d6051bff9f4d3dd53fad79
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2019-08-23 10:57:16 +0200

    Fixed: Tentatively turned-off using fgcz-c-047 for 2-3 months

--- master/app/controllers/run_application_controller.rb
+++ master/app/controllers/run_application_controller.rb
@@ -156,7 +156,7 @@ class RunApplicationController < ApplicationController
       end
 
       # 20190823 masa tentatively in 2-3 months for Gwyneth
-      @nodes.delete('fgcz-c-047: cpu 32,mem   1 TB,scr  28T')
+      @nodes.delete('fgcz-c-047: cores 8/16, ram 1009G, scr 900G')
       @nodes
     end
 		unless @factors
