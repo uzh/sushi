cef8625588f9a2a58523b533a76f2075d0c41ab1
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2019-10-15 23:06:57 +0200

    fixed required inputs

--- master/lib/DADA2Step1SampleApp.rb
+++ master/lib/DADA2Step1SampleApp.rb
@@ -17,7 +17,7 @@ Data preprocssing with DADA2. Please make sure that the input files are from the
   EOS
   @params['process_mode'] = 'DATASET'
 @required_columns = ['Name', 'Read1']
-@required_params = ['maxLen', 'technology','paired','concatenateReads']
+@required_params = ['maxLen', 'technology','paired']
 @params['cores'] = '2'
 @params['ram'] = '8'
 @params['scratch'] = '10'
