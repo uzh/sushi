73293fcf8287cc53ea11660467d421fa80977e35
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2019-11-08 15:19:04 +0100

    completed temporary group fix

--- master/lib/PhyloSeqAnalysisApp.rb
+++ master/lib/PhyloSeqAnalysisApp.rb
@@ -17,24 +17,26 @@ super
   EOS
 @params['process_mode'] = 'DATASET'
 @required_columns = ['Name', 'RObjectPhyloseq']
-@required_params = ['group','taxonomicRank']
+@required_params = ['taxonomicRank']
 @params['cores'] = '1'
 @params['ram'] = '8'
 @params['scratch'] = '10'
 @params['taxonomicRank'] = ['Phylum','Class','Order','Family','Genus']
 @params['taxonomicRank', 'description'] = 'Which rank should be displayed in the rank-dependent plots in the report?'
-@params['group'] = false
-@params['group', 'description'] = 'are there group informations in the dataset?'
 @params['rawCount'] = '5'
-@params['rawCount', 'description'] = 'OTUs with fewer than these counts in less than the fraction of samples 
-below will be removed.'
+@params['rawCount', 'description'] = 'OTUs with fewer than these counts in less than the fraction of samples below will be removed.'
 @params['sampleFraction'] = '0.3'
 @params['sampleFraction', 'description'] = 'Minimum fraction of samples for which the raw count threshold above applies.'
 @params['numTopRanks'] = '15'
 @params['numTopRanks', 'description'] = 'Number of top-ranked OTUs to be considered in the plots.'
-@params['pack'] = ''
-@params['sodiumPerc'] = ''
 @params['grouping'] = ''
+@params['grouping','description'] = 'This IS a selector. This decides from which of the lists below sampleGroup and refGroup must be chosen.'
+@params['groupingDetails'] = ''
+@params['groupingDetails','description'] = "This is NOT a selector. It simply shows the list of available values for the availalbe attributes  listed in grouping."
+@params['sampleGroup'] = ''
+@params['sampleGroup','description'] = 'This MUST be assigned from the values associated to the grouping variable'
+@params['refGroup'] = ''
+@params['refGroup','description'] = 'This MUST be assigned from the values associated to the grouping variable'
 @params['mail'] = ""
 @params['name'] = "PhyloSeqReport"
 @inherit_tags = ["B-Fabric", "Characteristic","group"]
@@ -46,21 +48,11 @@ def set_default_parameters
       require 'csv'
       fileName = File.join(SushiFabric::GSTORE_DIR, desMat)
       designMatrixTable = CSV.read(fileName,:headers => true, :col_sep => "\t")
-      ff = Hash[designMatrixTable.headers.collect{ |item| [item, ["Values available for this variable"] + designMatrixTable[item].uniq]}]
-      p ff
-      ff.each do |key, value|
-      @params[key] = value
-      @params[key, 'description'] = 'This is NOT a selector, just a list of available values. If #{key} is used as grouping variable, sampleGroup and refGroup MUST be chose from this list.'
-      end
+      ff = Hash[designMatrixTable.headers.collect{ |item| [item, designMatrixTable[item].uniq]}]
       @params['grouping']= ff.keys
-      @params['grouping','description']='This IS a selector. This decides from which of the lists above sampleGroup and refGroup must be chosen.'
-      @params['sampleGroup'] = ''
-      @params['sampleGroup','description'] = 'This MUST be chosen from the values in the dropdown list associated to the grouping variable'
-      @params['refGroup'] = ''
-      @params['refGroup','description'] = 'This MUST be a different choice from the values in the dropdown list associated to the SAME grouping variable'
+      @params['groupingDetails']  = ff.to_a
       end
 end
-
 def next_dataset
     report_file = File.join(@result_dir, @params['name'])
     report_link = File.join(report_file, '00index.html')
