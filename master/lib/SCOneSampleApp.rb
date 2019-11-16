d8c44f1df4bbfef041cbba96a2825bd5163f7b7f
Daymé González Rodríguez <daymegr@fgcz-net.unizh.ch>
2019-11-16 18:18:44 +0100

    new SC app for one sample

---
+++ master/lib/SCOneSampleApp.rb
@@ -0,0 +1,96 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+
+class SCOneSampleApp < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'SCOneSample'
+    @params['process_mode'] = 'SAMPLE'
+    @analysis_category = 'SingleCell'
+    @description =<<-EOS
+Single cell report<br/>
+    EOS
+    @required_columns = ['Name', 'Species', 'refBuild', 'CountMatrix', 'ResultDir']
+    @required_params = ['name']
+    # optional params
+    @params['cores'] = '8'
+    @params['ram'] = '30'
+    @params['scratch'] = '50'
+    @params['name'] = 'SCOneSample'
+    @params['refBuild'] = ref_selector
+    @params['paired'] = false
+    @params['strandMode'] = ['both', 'sense', 'antisense']
+    @params['refFeatureFile'] = 'genes.gtf'
+    @params['featureLevel'] = ['gene', 'isoform']
+    @params['transcriptTypes'] = ''
+    @params['transcriptTypes', 'multi_selection'] = true
+    @params['transcriptTypes', 'selected'] = 0
+    @params['scProtocol'] = ['10X', 'smart-Seq2']
+    @params['species'] = ['Human', 'Mouse', "other"]
+    @params['tissue'] = []
+    @params['tissue','multi_selection'] = true
+    tissue = {}
+    CSV.foreach("/srv/GT/databases/all_cell_markers.txt", headers: true, col_sep: "\t") do |e|
+      tissue[e["tissueType"]] = true
+    end
+    @params['tissue'] = tissue.keys.sort
+    @params['pcs'] = 20
+    @params['pcs', 'description'] = 'The maximal dimensions to use for reduction.'
+    @params['pcGenes'] = ''
+    @params['pcGenes', 'description'] = 'The genes used in supvervised clustering'
+    @params['resolution'] = 0.6
+    @params['resolution', 'description'] = 'Value of the resolution parameter, use a value above (below) 1.0 if you want to obtain a larger (smaller) number of communities.'
+    @params['all2allMarkers'] = false
+    @params['all2allMarkers', 'description'] = 'Run all against all cluster comparisons?'
+    @params['cellsPercentage'] = 0.05
+    @params['cellsPercentage', 'description'] = 'A gene will be kept if it is expressed in at least this percentage of cells'
+    @params['nmad'] = 3
+    @params['nmad', 'description'] = 'Median absolute deviation (MAD) from the median value of each metric across all cells'
+    @params['specialOptions'] = ''
+    @params['mail'] = ""
+    @params['Rversion'] = ["Dev/R/3.6.1"]
+    @modules = ["Dev/R", "Dev/Python/3.6.8"]
+  end
+  def preprocess
+    @random_string = (1..12).map{[*('a'..'z')].sample}.join
+  end
+  def next_dataset
+    report_file = File.join(@result_dir, "#{@dataset['Name']}_SCReport")
+    report_link = File.join(report_file, '00index.html')
+    {'Name'=>@dataset['Name'],
+     'Species'=>@dataset['Species'],
+     'refBuild'=>@params['refBuild'],
+     'refFeatureFile'=>@params['refFeatureFile'],
+     'Static Report [Link]'=>report_link,
+     'Live Report [Link]'=>"#{SHINY_EXPLORE_SC}?data=#{report_file}/SCReport-#{@random_string}.rds",
+     'Report [File]'=>report_file,
+     'ResultDir [Link]'=>@dataset['ResultDir'],
+    }
+  end
+  def set_default_parameters
+    @params['refBuild'] = @dataset[0]['refBuild']
+    if dataset_has_column?('refFeatureFile')
+      @params['refFeatureFile'] = @dataset[0]['refFeatureFile']
+    end
+    @params['transcriptTypes'] = @dataset[0]['transcriptTypes'].to_s.split(',')
+    if dataset_has_column?('paired')
+      @params['paired'] = @dataset[0]['paired']
+    end
+    if dataset_has_column?('strandMode')
+      @params['strandMode'] = @dataset[0]['strandMode']
+    end
+  end
+  def commands
+    run_RApp("EzAppSCOneSample")
+  end
+end
+
+if __FILE__ == $0
+
+end
+
