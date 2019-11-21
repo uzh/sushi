d50e30602ba8eca96e9715a10d3799b6ac3cf5fa
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2019-11-21 22:59:28 +0100

    SGSeq App 1st step main commit

---
+++ master/lib/SGSeqApp.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+Version = '20180209-163825'
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class SGSeqApp < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'SGSeq'
+    @analysis_category = 'Alternative_Splicing'
+ @description =<<-EOS
+    Data preparation for alternative splicing analysis<br/>
+<a href='https://bioconductor.org/packages/devel/bioc/vignettes/SGSeq/inst/doc/SGSeq.html'>manual</a><br/>
+EOS
+    @required_columns = ['Name','BAM','BAI']
+    @required_params = ['refBuild','refFeatureFile']
+    @params['cores'] = '8'
+    @params['ram'] = '20'
+    @params['scratch'] = '10'
+    @params['refBuild'] = ref_selector
+    @params['paired'] = false
+    @params['strandMode'] = ['both', 'sense', 'antisense']
+    @params['refFeatureFile'] = 'genes.gtf'
+    @params['specialOptions'] = ''
+    @params['mail'] = ""
+    @modules = ["Dev/R"]
+    @inherit_tags = ["Factor", "B-Fabric"]
+  end
+  def set_default_parameters
+    @params['refBuild'] = @dataset[0]['refBuild']
+    if dataset_has_column?('refFeatureFile')
+      @params['refFeatureFile'] = @dataset[0]['refFeatureFile']
+    end
+  end
+  def next_dataset
+    {'Name'=>@dataset['Name'],
+     'Count [File]'=>File.join(@result_dir, "#{@dataset['Name']}.txt"),
+     'SgSeqRDataFile [File]'=>File.join(@result_dir, "#{@dataset['Name']}.Rdata"),
+     'refBuild'=>@params['refBuild'],
+     'refFeatureFile'=>@params['refFeatureFile']
+    }.merge(extract_columns(@inherit_tags))
+  end
+  def commands
+    run_RApp("EzAppSGSeq")
+  end
+end
+
+if __FILE__ == $0
+
+end
