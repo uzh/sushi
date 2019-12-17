81001896fe02daf5f74ab0e1fb705407fcc44856
MiqG <miquelangladagirotto@gmail.com>
2019-12-17 16:57:18 +0100

    exceRpt's sushi apps: - exceRptApp: define parameters - exceRptAppReport: first commit

--- master/lib/ExceRptApp.rb
+++ master/lib/ExceRptApp.rb
@@ -11,25 +11,49 @@ class ExceRptApp < SushiFabric::SushiApp
     @params['process_mode'] = 'SAMPLE'
     @analysis_category = 'Count'
     @description =<<-EOS
-Annotation and Profiling of ncRNAs in smallRNA-seq<br/>
-Uses ncPRO-seq for a complete analysis of small-RNA-seq. ncPRO-seq considers performs quality assessment and quantiation.
-It considers various species of small RNA.<br />
-<a href='https://ncpro.curie.fr/index.html'>https://ncpro.curie.fr/index.html/</a>
-ncPRO can only process single-end stranded RNA.
-<strong>IMPORTANT: ncPRO does not run on the virtual nodes!!!!</strong>
+Annotation and Profiling of smallRNA-seq with exceRpt's pipeline<br/>
+ExceRpt performs a complete analysis of small-RNA-seq. This software carries out processing, filtering, and alignment for each smallRNA-seq sample from a run.<br />
+<a href='https://github.com/rkitchen/exceRpt'>https://github.com/rkitchen/exceRpt/</a>
 EOS
-
     @required_columns = ['Name','Read1', 'Adapter1', 'Species']
     @required_params = ['refBuild', 'cores', 'ram', 'scratch']
-
+    ## parameters
+    ### general
     @params['cores'] = '8'
     @params['ram'] = '40'
+    @params['JAVA_RAM'] = '10G'
     @params['scratch'] = '100'
-    @params['refBuild'] = ['Mus_musculus/UCSC/mm10', 'Homo_sapiens/UCSC/hg19', 'Rattus_norvegicus/UCSC/rn5']
-    #@params['paired'] = false
-    #@params['paired', 'description'] = 'whether the reads are paired end; must be false since ncPRO-seq does not support paired-end'
+    # @params['refBuild'] = ['Mus_musculus/UCSC/mm10', 'Homo_sapiens/UCSC/hg19', 'Rattus_norvegicus/UCSC/rn5']
     @params['mail'] = ""
-    @modules = ["Aligner/Bowtie", "QC/Flexbar", "Tools/ncPROseq", "QC/Trimmomatic", "Dev/R"]
+    
+    ### tunable parameters exceRpt_smallRNA
+    @params['ADAPTER_SEQ'] = 'guessKnown'
+    @params['SAMPLE_NAME'] = 'NULL'
+    @params['MAIN_ORGANISM_GENOME_ID'] = ['hg38','mm10']
+    @params['ENDOGENOUS_LIB_PRIORITY'] = 'miRNA,tRNA,piRNA,gencode,circRNA'
+    
+    @params['CALIBRATOR_LIBRARY'] = 'NULL'
+    @params['CALIBRATOR_TRIM5p'] = '0'
+    @params['CALIBRATOR_TRIM3p'] = '0'
+    
+    @params['TRIM_N_BASES_5p'] = '0'
+    @params['TRIM_N_BASES_3p'] = '0'
+    @params['RANDOM_BARCODE_LENGTH'] = '0'
+    @params['RANDOM_BARCODE_LOCATION'] = ['-5p -3p','-5p','-3p']
+    @params['KEEP_RANDOM_BARCODE_STATS'] = ['false','true']
+    @params['DOWNSAMPLE_RNA_READS'] = 'NULL'
+    @params['MAP_EXOGENOUS'] = ['off','miRNA']
+    @params['MIN_READ_LENGTH'] = '18'
+    @params['QFILTER_MIN_QUAL'] = '20'
+    @params['QFILTER_MIN_READ_FRAC'] = '80'
+    @params['STAR_alignEndsType'] = 'Local'
+    @params['STAR_outFilterMatchNmin'] = '18'
+    @params['STAR_outFilterMatchNminOverLread'] = '0.9' 
+    @params['STAR_outFilterMismatchNmax'] = '1'
+    @params['MAX_MISMATCHES_EXOGENOUS'] = '0'
+
+    ## modules
+    @modules = ["Dev/R"]
   end
   def preprocess
     if @params['paired']
@@ -42,12 +66,11 @@ EOS
       'excerpt [File]'=>File.join(@result_dir, "#{@dataset['Name']}"),
       'Species'=>@dataset['Species'],
       'refBuild'=>@params['refBuild'],
-      'strandMode'=>@params['strandMode']
     }
    dataset
   end
   def commands
-    run_RApp("EzAppNcpro")
+    run_RApp("EzAppExceRpt")
   end
 end
 
