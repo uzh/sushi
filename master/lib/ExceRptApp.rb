76378e379341bcf2dc5ba1e767f28bc96cec7eb4
MiqG <miquelangladagirotto@gmail.com>
2019-12-19 09:16:31 +0100

    Improve form descriptions

--- master/lib/ExceRptApp.rb
+++ master/lib/ExceRptApp.rb
@@ -21,18 +21,23 @@ EOS
     ### general
     @params['cores'] = '8'
     @params['ram'] = '40'
-    @params['JAVA_RAM'] = '10G'
+    @params['ram','description'] = 'in Gigabytes'
+    #@params['JAVA_RAM'] = '10G'
     @params['scratch'] = '100'
-    # @params['refBuild'] = ['Mus_musculus/UCSC/mm10', 'Homo_sapiens/UCSC/hg19', 'Rattus_norvegicus/UCSC/rn5']
+    @params['refBuild'] = ['Mus_musculus/UCSC/mm10', 'Homo_sapiens/UCSC/hg38']
     @params['mail'] = ""
     
     ### tunable parameters exceRpt_smallRNA
-    @params['ADAPTER_SEQ'] = 'guessKnown'
+    #@params['ADAPTER_SEQ'] = 'guessKnown'
+    #@params['ADAPTER_SEQ','description'] = 'Write your adapter sequence or "none" in case adapters were trimmed. By default, "guessKnown" will attempt to guess the 3p adapter using known sequences.'
     @params['SAMPLE_NAME'] = 'NULL'
-    @params['MAIN_ORGANISM_GENOME_ID'] = ['hg38','mm10']
+    @params['SAMPLE_NAME','description'] = 'If "NULL", the ExceRptApp will automatically set the name from the fastq(.gz) files.'
+    #@params['MAIN_ORGANISM_GENOME_ID'] = ['hg38','mm10']
     @params['ENDOGENOUS_LIB_PRIORITY'] = 'miRNA,tRNA,piRNA,gencode,circRNA'
+    @params['ENDOGENOUS_LIB_PRIORITY','description'] = '<comma,separated,list,no,spaces>'
     
     @params['CALIBRATOR_LIBRARY'] = 'NULL'
+    @params['CALIBRATOR_LIBRARY','description'] = 'Path to a bowtie2 index of calibrator oligos used for QC or normalisation.'
     @params['CALIBRATOR_TRIM5p'] = '0'
     @params['CALIBRATOR_TRIM3p'] = '0'
     
@@ -42,15 +47,19 @@ EOS
     @params['RANDOM_BARCODE_LOCATION'] = ['-5p -3p','-5p','-3p']
     @params['KEEP_RANDOM_BARCODE_STATS'] = ['false','true']
     @params['DOWNSAMPLE_RNA_READS'] = 'NULL'
-    @params['MAP_EXOGENOUS'] = ['off','miRNA']
+    #@params['MAP_EXOGENOUS'] = ['off','miRNA']
     @params['MIN_READ_LENGTH'] = '18'
     @params['QFILTER_MIN_QUAL'] = '20'
     @params['QFILTER_MIN_READ_FRAC'] = '80'
-    @params['STAR_alignEndsType'] = 'Local'
+    @params['STAR_alignEndsType'] = ['Local','EndToEnd']
+    @params['STAR_alignEndsType','description'] = 'Defines the alignment mode; local alignment is recommended to allow for isomiRs.'
     @params['STAR_outFilterMatchNmin'] = '18'
-    @params['STAR_outFilterMatchNminOverLread'] = '0.9' 
+    @params['STAR_outFilterMatchNmin','description'] = 'Minimum number of bases to include in the alignment (should match the minimum read length defined above).'
+    @params['STAR_outFilterMatchNminOverLread'] = '0.9'
+    @params['STAR_outFilterMatchNminOverLread','description'] = 'Minimum fraction of the read that *must* remain following soft-clipping in a local alignment.'
     @params['STAR_outFilterMismatchNmax'] = '1'
-    @params['MAX_MISMATCHES_EXOGENOUS'] = '0'
+    @params['STAR_outFilterMismatchNmax','description'] = 'Maximum allowed mismatched bases in the aligned portion of the read.'
+    #@params['MAX_MISMATCHES_EXOGENOUS'] = '0'
 
     ## modules
     @modules = ["Dev/R"]
@@ -65,7 +74,7 @@ EOS
       'Name'=>@dataset['Name'],
       'excerpt [File]'=>File.join(@result_dir, "#{@dataset['Name']}"),
       'Species'=>@dataset['Species'],
-      'refBuild'=>@params['refBuild'],
+      'refBuild'=>@params['refBuild']
     }
    dataset
   end
