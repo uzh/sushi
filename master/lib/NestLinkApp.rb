2a9004c02623eebc883f29c5ecfbc05ab2af8440
Lennart Opitz <lopitz@fgcz.ethz.ch>
2020-01-06 13:45:22 +0100

    add NestLinkApp

---
+++ master/lib/NestLinkApp.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class NestLinkApp <  SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'NestLinkApp'
+    @analysis_category = 'Misc'
+    @description =<<-EOS
+NestLink - an R data package to guide through Engineered Peptide Barcodes for In-Depth Analyzes of Binding Protein Ensembles - https://bioconductor.org/packages/release/data/experiment/html/NestLink.html
+    EOS
+    @required_columns = ['Name','Read1', 'FlashLog']
+    @required_params = ['NB_Linker1', 'NB_Linker2', 'ProteaseSite','FC_Linker','knownNBPath']
+    @params['cores'] = '1'
+    @params['ram'] = '40'
+    @params['scratch'] = '30'
+    @params['nReads'] = '0'
+    @params['maxMismatch'] = 1
+    @params['NB_Linker1'] = "GGCCggcggGGCC"
+    @params['NB_Linker2'] = "GCAGGAGGA"
+    @params['ProteaseSite'] = "TTAGTCCCAAGA"
+    @params['FC_Linker'] = "GGCCaaggaggcCGG"
+    @params['knownNBPath'] = '/home/huberlea/knownNB_new_corrected_LO.txt'
+    @params['minRelBestHitFreq'] = '0'
+    @params['minConsensusScore'] = '0.9'
+    @params['minNanobodyLength'] = '321'
+    @params['minFlycodeLength'] = '33'
+    @params['FCminFreq'] = '4'
+    @params['name'] = 'NestLink'
+    @params['specialOptions'] = ''
+    @params['mail'] = ''
+    @modules = ["Dev/R"]
+  end
+  def next_dataset
+    {'Name'=>@dataset['Name'],
+     'NestLink Result [File]'=>File.join(@result_dir, "#{@dataset['Name']}")
+    }
+  end
+  def set_default_parameters
+  end
+  def commands
+    run_RApp("EzAppNestLink")
+  end
+end
+
+if __FILE__ == $0
+
+end
