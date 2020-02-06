52a23fd0ece737e7547290a6c01eef6a4dc70a6b
Daymé González Rodríguez <daymegr@fgcz-net.unizh.ch>
2020-02-06 17:58:13 +0100

    added species variable

--- master/lib/SCMultipleSamplesOneGroupApp.rb
+++ master/lib/SCMultipleSamplesOneGroupApp.rb
@@ -26,6 +26,7 @@ class SCMultipleSamplesOneGroupApp < SushiFabric::SushiApp
     @params['name'] = 'SCReportMerging'
     @params['refBuild'] = ref_selector
     @params['refFeatureFile'] = 'genes.gtf'
+    @params['species'] = ['Human', 'Mouse', "other"]
     @params['tissue'] = []
     @params['tissue','multi_selection'] = true
     @params['tissue','all_selected'] = true
