83feaba3e5fcbfec59d902a5497677159ba3abd7
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2020-02-25 14:49:59 +0100

    Added @params[key, 'hr'] option to put hr tag in parameter view

--- master/lib/STARApp.rb
+++ master/lib/STARApp.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-Version = '20171109-095500'
+Version = '20200225-143601'
 
 require 'sushi_fabric'
 require_relative 'global_variables'
@@ -37,6 +37,7 @@ EOS
     @params['twopassMode', 'description'] = 'Per-sample 2-pass mapping or 1-pass mapping in STAR. 2-pass mapping allows to detect more splices reads mapping to novel junctions.'
     # trimming options
     # general
+    @params['trimAdapter', 'hr'] = true
     @params['trimAdapter'] = true
     # Fastp
     ## trimming
