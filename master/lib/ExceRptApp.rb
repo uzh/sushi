5d4fd664520895cd2bad915158dc5f5c7b17744c
MiqG <miquelangladagirotto@gmail.com>
2020-02-27 10:19:42 +0100

    excerpt cleanup

--- master/lib/ExceRptApp.rb
+++ master/lib/ExceRptApp.rb
@@ -23,30 +23,28 @@ EOS
     ## general
     @params['cores'] = '8'
     @params['ram'] = '16'
-    #@params['JAVA_RAM'] = '10G'
     @params['scratch'] = '100'
     @params['refBuild'] = ['Homo_sapiens/UCSC/hg38', 'Mus_musculus/UCSC/mm10']  ## this param substitutes exceRpt_smallRNA's vaibale: "MAIN_ORGANISM_GENOME_ID"
-    
+
     ## tunable parameters exceRpt_smallRNA
-    #@params['ADAPTER_SEQ'] = 'guessKnown' # taken from dataset
-    @params['SAMPLE_NAME'] = 'NULL'
-    @params['SAMPLE_NAME','description'] = 'If "NULL", the ExceRptApp will automatically set the name from the fastq(.gz) files.'
     @params['ENDOGENOUS_LIB_PRIORITY'] = 'miRNA,tRNA,piRNA,gencode,circRNA'
     @params['ENDOGENOUS_LIB_PRIORITY','description'] = 'Choose the priority of each library during read assignment and quantification, e.g. "comma,separated,list,with,no,spaces".'
+    
     ## preprocessing
     @params['TRIM_N_BASES_5p'] = '0'
     @params['TRIM_N_BASES_3p'] = '0'
     @params['MIN_READ_LENGTH'] = '18'
     @params['QFILTER_MIN_QUAL'] = '20'
     @params['QFILTER_MIN_READ_FRAC'] = '80'
+    
     ## barcodes
     @params['RANDOM_BARCODE_LENGTH'] = '0'
     @params['RANDOM_BARCODE_LENGTH','description'] = 'Identify and remove random barcodes of this number of nucleotides. For a Bioo prep with a 4N random barcode on both the 3p and 5p adapter, this value should be 4.'
     @params['RANDOM_BARCODE_LOCATION'] = ['-5p -3p','-5p','-3p']
     @params['RANDOM_BARCODE_LOCATION','description'] = 'Specify where to look for the random barcode(s).'
     @params['KEEP_RANDOM_BARCODE_STATS'] = ['false','true']
-    @params['KEEP_RANDOM_BARCODE_STATS'] = 'Specify whether or not to calculate overrepresentation statistics using the random barcodes (this may be slow and memory intensive!).'
-    
+    @params['KEEP_RANDOM_BARCODE_STATS','description'] = 'Specify whether or not to calculate overrepresentation statistics using the random barcodes (this may be slow and memory intensive!).'
+
     ## STAR parameters
     @params['STAR_alignEndsType'] = ['Local','EndToEnd']
     @params['STAR_alignEndsType','description'] = 'Define the alignment mode; local alignment is recommended to allow for isomiRs.'
@@ -56,21 +54,13 @@ EOS
     @params['STAR_outFilterMatchNminOverLread','description'] = 'Minimum fraction of the read that *must* remain following soft-clipping in a local alignment.'
     @params['STAR_outFilterMismatchNmax'] = '1'
     @params['STAR_outFilterMismatchNmax','description'] = 'Maximum allowed mismatched bases in the aligned portion of the read.'
-    #@params['MAX_MISMATCHES_EXOGENOUS'] = '0'
     @params['REMOVE_LARGE_INTERMEDIATE_FILES'] = ['true','false']
     @params['REMOVE_LARGE_INTERMEDIATE_FILES','description'] = 'When exceRpt finishes, choose whether to remove the large alignment files that can take a lot of disk space.'
     
-    ## calibration (to be further tested before making it available)
-    #@params['CALIBRATOR_LIBRARY'] = 'NULL'
-    #@params['CALIBRATOR_LIBRARY','description'] = 'Path to a bowtie2 index of calibrator oligos spiked-in for QC or normalisation.'
-    #@params['CALIBRATOR_TRIM5p'] = '0'
-    #@params['CALIBRATOR_TRIM3p'] = '0'
-    
-    #@params['DOWNSAMPLE_RNA_READS'] = 'NULL'
-    #@params['DOWNSAMPLE_RNA_READS','description'] = 'Choose whether to downsample to this number of reads after assigning reads to the various transcriptome libraries (may be useful for normalising very different yields)'
-    #@params['MAP_EXOGENOUS'] = ['off','miRNA'] # we do not use this functionality
-    
+    @params['cmdOptions'] = 'JAVA_RAM=10G'
+
     @params['mail'] = ""
+
     ## modules
     @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
     @modules = ["Dev/R"]
