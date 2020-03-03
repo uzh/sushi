ee823f3c94cec9dcc1030a4fbb357710b0fe2c9e
Ge Tan <gtan@me.com>
2020-03-03 14:06:15 +0100

    new CellRangerATACApp

---
+++ master/lib/CellRangerATACApp.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class CellRangerATACApp <  SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'CellRangerATACCount'
+    @analysis_category = 'SingleCell'
+    @description =<<-EOS
+This wrapper runs <a href='https://support.10xgenomics.com/single-cell-atac/software/pipelines/latest/using/count',>cellranger atac count</a> in Single-library analysis mode.
+    EOS
+    @required_columns = ['Name','RawDataDir','Species']
+    @required_params = ['name', 'refBuild']
+    @params['cores'] = '8'
+    @params['ram'] = '40'
+    @params['scratch'] = '200'
+    @params['name'] = 'CellRangerATACCount'
+    @params['refBuild'] = ref_selector
+    @params['refFeatureFile'] = 'genes.gtf'
+    @params['featureLevel'] = 'gene'
+    @params['cmdOptions'] = ''
+    @params['cmdOptions', 'description'] = 'specify the commandline options for CellRanger ATAC; do not specify any option that is already covered by the dedicated input fields'
+    @params['specialOptions'] = ''
+    @params['mail'] = ""
+    @modules = ["Dev/R"]
+    @inherit_tags = ["B-Fabric"]
+  end
+  def set_default_parameters
+  end
+  def next_dataset
+    report_dir = File.join(@result_dir,"#{@dataset['Name']}")
+    if @params['TenXLibrary'] == "VDJ"
+      dataset = {
+        'Name'=>@dataset['Name'],
+        'Species'=>@dataset['Species'],
+        'refBuild'=>@params['refBuild'],
+        'refFeatureFile'=>@params['refFeatureFile'],
+        'featureLevel'=>@params['featureLevel'],
+        'ResultDir [File]'=>report_dir,
+        'Report [Link]'=>File.join(report_dir, 'web_summary.html')
+      }.merge(extract_columns(@inherit_tags))
+    else
+      dataset = {
+        'Name'=>@dataset['Name'],
+        'Species'=>@dataset['Species'],
+        'refBuild'=>@params['refBuild'],
+        'refFeatureFile'=>@params['refFeatureFile'],
+        'featureLevel'=>@params['featureLevel'],
+        'ResultDir [File]'=>report_dir,
+        'Report [Link]'=>File.join(report_dir, 'web_summary.html'),
+        'CountMatrix [Link]'=>File.join(report_dir, 'filtered_peak_bc_matrix')
+      }.merge(extract_columns(@inherit_tags))
+    end
+    dataset
+  end
+  def commands
+    run_RApp("EzAppCellRangerATAC")
+  end
+end
+
+if __FILE__ == $0
+
+end
