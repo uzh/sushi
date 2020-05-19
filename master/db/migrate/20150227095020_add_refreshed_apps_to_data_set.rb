5084524c62cb2b0986ba069cb793b2d061a8c0b9
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2020-05-19 11:01:28 +0200

    Updated migrates for Rails 5.2

--- master/db/migrate/20150227095020_add_refreshed_apps_to_data_set.rb
+++ master/db/migrate/20150227095020_add_refreshed_apps_to_data_set.rb
@@ -1,4 +1,4 @@
-class AddRefreshedAppsToDataSet < ActiveRecord::Migration
+class AddRefreshedAppsToDataSet < ActiveRecord::Migration[5.2]
   def change
     add_column :data_sets, :refreshed_apps, :boolean
   end
