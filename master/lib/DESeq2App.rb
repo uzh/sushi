f486e076b7230d80950dafd92c5c32ff28b3602d
Ge Tan <gtan@me.com>
2020-05-26 15:11:00 +0200

    Clean and add new Rversion

--- master/lib/DESeq2App.rb
+++ master/lib/DESeq2App.rb
@@ -46,7 +46,7 @@ class DESeq2App < SushiFabric::SushiApp
     @params['specialOptions'] = ''
     @params['expressionName'] = ''
     @params['mail'] = ""
-    @params['Rversion'] = ["Dev/R/3.6.1", "Dev/R/3.6.0", "Dev/R/3.5.1", "Dev/R/3.5.0", "Dev/R/3.4.2", "Dev/R/3.4.0", "Dev/R/3.3.0"]
+    @params['Rversion'] = ["Dev/R/4.0.0", "Dev/R/3.6.1", "Dev/R/3.6.0", "Dev/R/3.5.1", "Dev/R/3.5.0", "Dev/R/3.4.2", "Dev/R/3.4.0", "Dev/R/3.3.0"]
     @modules = ["Tools/samtools", "Tools/GFOLD", "Dev/PhantomJS", "Tools/sambamba"]
   end
   def preprocess
