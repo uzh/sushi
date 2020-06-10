b2dc003213b5890f4460b91e65b43bbd54635264
Hubert Rehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2020-06-10 07:39:36 +0200

    update kallisto to use fastp

--- master/lib/KallistoApp.rb
+++ master/lib/KallistoApp.rb
@@ -39,13 +39,44 @@ EOS
     @params['transcriptFasta', 'description'] = 'give full path of transcript fasta file; in that case the build is ignored; if it comes from trinity assembly the gene-isoform associations will be extracted and used'
     @params['transcriptTypes'] = ['protein_coding', 'rRNA', 'tRNA', 'Mt_rRNA', 'Mt_tRNA', 'long_noncoding', 'short_noncoding', 'pseudogene']
     @params['transcriptTypes', 'multi_selection'] = true
+    # trimming options
+    # general
+    @params['trimAdapter', 'hr'] = true
     @params['trimAdapter'] = true
-    @params['trimLeft'] = 0
-    @params['trimRight'] = 0
-    @params['minTailQuality'] = 0
-    @params['minAvgQuality'] = 20
+    # Fastp
+    ## trimming
+    @params['trim_front1'] = '0'
+    @params['trim_front1','description'] = 'trimming how many bases in front for read1 (and read2), default is 0.'
+    @params['trim_tail1'] = '0'
+    @params['trim_tail1','description'] = 'trimming how many bases in tail for read1 (and read2), default is 0.'
+    @params['cut_front'] = false
+    @params['cut_front','description'] = 'move a sliding window from front (5p) to tail, drop the bases in the window if its mean quality < threshold, stop otherwise.'
+    @params['cut_front_window_size'] = '4'
+    @params['cut_front_mean_quality'] = '20'
+    @params['cut_tail'] = false
+    @params['cut_tail','description'] = 'move a sliding window from tail (3p) to front, drop the bases in the window if its mean quality < threshold, stop otherwise.'
+    @params['cut_tail_window_size'] = '4'
+    @params['cut_tail_mean_quality'] = '20'
+    @params['cut_right'] = false
+    @params['cut_right','description'] = 'move a sliding window from front to tail, if meet one window with mean quality < threshold, drop the bases in the window and the right part, and then stop.'
+    @params['cut_right_window_size'] = '4'
+    @params['cut_right_mean_quality'] = '20'    
+    @params['average_qual'] = '0'
+    @params['average_qual','description'] = 'if one read average quality score <avg_qual>, then this read/pair is discarded. Default 0 means no requirement'
+    @params['max_len1'] = '0'
+    @params['max_len1','description'] = 'if read1 is longer than max_len1, then trim read1 at its tail to make it as long as max_len1. Default 0 means no limitation. If two reads are present, the same will apply to read2.'
+    @params['max_len2'] = '0'
+    @params['max_len2','description'] = 'if read1 is longer than max_len2, then trim read2 at its tail to make it as long as max_len1. Default 0 means no limitation.'
+    @params['poly_x_min_len'] = '10'
+    @params['poly_x_min_len','description'] = 'the minimum length to detect polyX in the read tail. 10 by default.'
+    @params['length_required'] = '18'
+    @params['length_required','description'] = 'reads shorter than length_required will be discarded.'
+    @params['cmdOptionsFastp'] = ''
+    ## additional commands
+    @params['markDuplicates'] = true
+    @params['markDuplicates', 'description'] = 'should duplicates be marked with sambamba. It is recommended for ChIP-seq and ATAC-seq data.'
     @params['mail'] = ""
-    @modules = ["Tools/samtools", "Aligner/kallisto", "QC/Flexbar", "QC/Trimmomatic", "Dev/R", "Tools/sambamba"]
+    @modules = ["Tools/samtools", "Aligner/kallisto", "QC/fastp", "Dev/R", "Tools/sambamba"]
     @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
   end
   def preprocess
