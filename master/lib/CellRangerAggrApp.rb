938e9fe17774960c9511673b9bc1ba797a48ab50
Daymé González Rodríguez <daymegr@fgcz-net.unizh.ch>
2020-07-31 14:26:34 +0200

    missing comma

--- master/lib/CellRangerAggrApp.rb
+++ master/lib/CellRangerAggrApp.rb
@@ -39,7 +39,7 @@ EOS
   end
   def next_dataset
     report_file = File.join(@result_dir, @params['name'])
-    {'Name'=>@params['name'],
+    {'Name'=>@dataset['name'],
      'Condition'=>@dataset['Condition'],
      'Species'=>@dataset['Species'],
      'refBuild'=>@params['refBuild'],
@@ -47,7 +47,7 @@ EOS
      'featureLevel'=>@params['featureLevel'],
      'transcriptTypes'=>@params['transcriptTypes'],
      'ResultDir [File]'=>report_file,
-     'Report [Link]'=>File.join(report_file, 'web_summary.html')
+     'Report [Link]'=>File.join(report_file, 'web_summary.html'),
      'CountMatrix [Link]'=>File.join(report_dir, 'filtered_feature_bc_matrix')
     }
   end
