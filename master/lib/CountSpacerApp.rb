2639480d951c495630e1c5c0ca40abe12d4adaf7
Lennart Opitz <lopitz@fgcz.ethz.ch>
2020-08-26 15:15:35 +0200

    switch CountSpacerApp to fastp

--- master/lib/CountSpacerApp.rb
+++ master/lib/CountSpacerApp.rb
@@ -22,16 +22,7 @@ EOS
     @params['cores'] = '8'
     @params['ram'] = '40'
     @params['scratch'] = '100'
-    @params['trimAdapter'] = true
-    @params['trimAdapter', 'description'] = 'if adapters should be trimmed'
-    @params['trimLeft'] = 0
-    @params['trimLeft', 'description'] = 'fixed trimming at the "left" i.e. 5-prime end of the read'
-    @params['trimRight'] = 0
-    @params['trimRight', 'description'] = 'fixed trimming at the "right" i.e. 3-prime end of the read'
-    @params['minTailQuality'] = 0
-    @params['minTailQuality', 'description'] = 'if above zero, then reads are trimmed as soon as 4 consecutive bases have lower mean quality'
-    @params['minAvgQuality'] = 10
-    @params['minReadLength'] = 19
+
     @params['name'] = 'CountSpacer_Result'
     @params['dictPath'] = ''
     @params['dictPath'] = {'select'=>''}
@@ -45,8 +36,43 @@ EOS
 
     @params['specialOptions'] = ''
     @params['specialOptions', 'description'] = 'special unsupported options that the R wrapper may support, format: <key>=<value>'
+    
+    # trimming options
+    # general
+    @params['trimAdapter', 'hr'] = true
+    @params['trimAdapter'] = true
+    # Fastp
+    ## trimming
+    @params['trim_front1'] = '0'
+    @params['trim_front1','description'] = 'trimming how many bases in front for read1 (and read2), default is 0.'
+    @params['trim_tail1'] = '0'
+    @params['trim_tail1','description'] = 'trimming how many bases in tail for read1 (and read2), default is 0.'
+    @params['cut_front'] = false
+    @params['cut_front','description'] = 'move a sliding window from front (5p) to tail, drop the bases in the window if its mean quality < threshold, stop otherwise.'
+    @params['cut_front_window_size'] = '4'
+    @params['cut_front_mean_quality'] = '20'
+    @params['cut_tail'] = false
+    @params['cut_tail','description'] = 'move a sliding window from tail (3p) to front, drop the bases in the window if its mean quality < threshold, stop otherwise.'
+    @params['cut_tail_window_size'] = '4'
+    @params['cut_tail_mean_quality'] = '20'
+    @params['cut_right'] = false
+    @params['cut_right','description'] = 'move a sliding window from front to tail, if meet one window with mean quality < threshold, drop the bases in the window and the right part, and then stop.'
+    @params['cut_right_window_size'] = '4'
+    @params['cut_right_mean_quality'] = '20'
+    @params['average_qual'] = '10'
+    @params['average_qual','description'] = 'if one read average quality score <avg_qual>, then this read/pair is discarded. Default 0 means no requirement'
+    @params['max_len1'] = '0'
+    @params['max_len1','description'] = 'if read1 is longer than max_len1, then trim read1 at its tail to make it as long as max_len1. Default 0 means no limitation. If two reads are present, the same will apply to read2.'
+    @params['max_len2'] = '0'
+    @params['max_len2','description'] = 'if read1 is longer than max_len2, then trim read2 at its tail to make it as long as max_len1. Default 0 means no limitation.'
+    @params['poly_x_min_len'] = '10'
+    @params['poly_x_min_len','description'] = 'the minimum length to detect polyX in the read tail. 10 by default.'
+    @params['length_required'] = '19'
+    @params['length_required','description'] = 'reads shorter than length_required will be discarded.'
+    @params['cmdOptionsFastp'] = ''
+    ## additional commands
     @params['mail'] = ""
-    @modules = ["QC/Flexbar", "QC/Trimmomatic", "QC/fastp", "Dev/R", "Aligner/Bowtie"]
+    @modules = ["QC/fastp", "Dev/R", "Aligner/Bowtie"]
     @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
   end
   def preprocess
