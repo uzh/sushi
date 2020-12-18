3cb6d2352ee9e71bfb775e17f1a1460c8fcba9ff
Hubert Rehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2020-12-18 09:18:19 +0100

    added samtools

--- master/lib/MACS2App.rb
+++ master/lib/MACS2App.rb
@@ -33,7 +33,7 @@ Capturing the influence of genome complexity to evaluate the significance of enr
     @params['markDuplicates'] = false
     @params['specialOptions'] = ''
     @params['mail'] = ''
-    @modules = ["Tools/UCSC", "Tools/BEDTools", "Dev/R", "Tools/Picard"]
+    @modules = ["Tools/samtools", "Tools/UCSC", "Tools/BEDTools", "Dev/R", "Tools/Picard"]
     # MACS2 is in Python2 bin
     @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
   end
