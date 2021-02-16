c6cf6b29ab5455acc96d6cd9dafc1b4820f82ed4
Daymé González Rodríguez <daymegr@fgcz-net.unizh.ch>
2021-02-16 16:04:18 +0100

    retire no longer used apps

---
+++ master/lib/otherApps/SCMultipleSamplesAndGroupsApp.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+
+class SCMultipleSamplesAndGroupsApp < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'SCMultipleSamplesAndGroups'
+    @params['process_mode'] = 'DATASET'
+    @analysis_category = 'SingleCell'
+    @description =<<-EOS
+    The report of merged single cell samples/plates<br/>
+    EOS
+    @required_columns = ['Name', 'Species', 'refBuild', 'refFeatureFile', 'Static Report']
+    @required_params = []
+    # optional params
+    @params['cores'] = '4'
+    @params['ram'] = '50'
+    @params['scratch'] = '50'
+    @params['node'] = ''
+    @params['process_mode'] = 'DATASET'
+    @params['name'] = 'SCReportMerging'
+    @params['refBuild'] = ref_selector
+    @params['refFeatureFile'] = 'genes.gtf'
+    @params['tissue'] = []
+    @params['tissue','multi_selection'] = true
+    @params['tissue','all_selected'] = true
+    @params['tissue', 'multi_selection_size'] = 10
+    tissue = {}
+    CSV.foreach("/srv/GT/databases/scGeneSets/all_cell_markers.txt", headers: true, col_sep: "\t") do |e|
+      tissue[e["tissueType"]] = true
+    end
+    @params['tissue'] = tissue.keys.sort
+    @params['tissue', 'description'] = 'Tissue the cells come from. Used in cell types identification for Human and Mouse organisms.'
+    @params['npcs'] = '30'
+    @params['npcs', 'description'] = 'Number of principal components to use for dimensionality reduction.'
+    @params['resolution'] = '0.6'
+    @params['resolution', 'description'] = 'Value between 0 and 1. A higher value will lead to larger communities.'
+    @params['batchCorrection'] = 'true'
+    @params['batchCorrection', 'description'] = "Perform batch correction?"
+    @params['SCT.regress'] = ['none', 'CellCycle']
+    @params['SCT.regress', 'description'] = "Variable to regress when processing the counts with the SCTransform method."
+    @params['DE.method'] = ['wilcox', 'LR']
+    @params['DE.method', 'description'] = "Method to be used when calculating gene cluster markers and differentially expressed genes between conditions."
+    @params['DE.regress'] = ['Batch', 'CellCycle']
+    @params['DE.regress','multi_selection'] = true
+    @params['DE.regress', 'description'] = "Variables to regress when calculating gene cluster markers and differentially expressed genes. Only used with the LR method."
+    @params['maxSamplesSupported'] = '5'
+    @params['specialOptions'] = ''
+    @params['mail'] = ""
+    @params['Rversion'] = ["Dev/R/4.0.3", "Dev/R/4.0.1"]
+  end
+  def next_dataset
+    report_file = File.join(@result_dir, @params['name'])
+    report_link = File.join(report_file, '00index.html')
+    {'Name'=>@params['name'],
+     'Species'=>(dataset = @dataset.first and dataset['Species']),
+     'refBuild'=>@params['refBuild'],
+     'refFeatureFile'=>@params['refFeatureFile'],
+     'Static Report [Link]'=>report_link,
+     'Report [File]'=>report_file,
+    }
+  end
+  def set_default_parameters
+    @params['refBuild'] = @dataset[0]['refBuild']
+    if dataset_has_column?('refFeatureFile')
+      @params['refFeatureFile'] = @dataset[0]['refFeatureFile']
+    end
+  end
+  def commands
+    command = "module load #{@params["Rversion"]}\n"
+    command << run_RApp("EzAppSCMultipleSamplesAndGroups")
+  end
+end
+
+if __FILE__ == $0
+
+end
---
+++ master/lib/otherApps/SCMultipleSamplesOneGroupApp.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+
+class SCMultipleSamplesOneGroupApp < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'SCMultipleSamplesOneGroup'
+    @params['process_mode'] = 'DATASET'
+    @analysis_category = 'SingleCell'
+    @description =<<-EOS
+    The report of merged single cell samples/plates<br/>
+    EOS
+    @required_columns = ['Name', 'Species', 'refBuild', 'refFeatureFile', 'Static Report']
+    @required_params = []
+    # optional params
+    @params['cores'] = '4'
+    @params['ram'] = '50'
+    @params['scratch'] = '50'
+    @params['node'] = ''
+    @params['process_mode'] = 'DATASET'
+    @params['name'] = 'SCReportMerging'
+    @params['refBuild'] = ref_selector
+    @params['refFeatureFile'] = 'genes.gtf'
+    @params['tissue'] = []
+    @params['tissue','multi_selection'] = true
+    @params['tissue','all_selected'] = true
+    @params['tissue', 'multi_selection_size'] = 10
+    tissue = {}
+    CSV.foreach("/srv/GT/databases/scGeneSets/all_cell_markers.txt", headers: true, col_sep: "\t") do |e|
+      tissue[e["tissueType"]] = true
+    end
+    @params['tissue'] = tissue.keys.sort
+    @params['npcs'] = '30'
+    @params['resolution'] = '0.6'
+    @params['batchCorrection'] = 'true'
+    @params['SCT.regress'] = ['none', 'CellCycle']
+    @params['DE.method'] = ['wilcox', 'LR']
+    @params['DE.regress'] = ['Plate', 'CellCycle']
+    @params['DE.regress','multi_selection'] = true
+    @params['chosenClusters'] = ''
+    @params['all2allMarkers'] = 'false'
+    @params['maxSamplesSupported'] = '5'
+    @params['specialOptions'] = ''
+    @params['mail'] = ""
+    @params['Rversion'] = ["Dev/R/4.0.3", "Dev/R/4.0.1"]
+  end
+  def next_dataset
+    report_file = File.join(@result_dir, @params['name'])
+    report_link = File.join(report_file, '00index.html')
+    {'Name'=>@params['name'],
+     'Species'=>(dataset = @dataset.first and dataset['Species']),
+     'refBuild'=>@params['refBuild'],
+     'refFeatureFile'=>@params['refFeatureFile'],
+     'Static Report [Link]'=>report_link,
+     'Report [File]'=>report_file,
+    }
+  end
+  def set_default_parameters
+    @params['refBuild'] = @dataset[0]['refBuild']
+    if dataset_has_column?('refFeatureFile')
+      @params['refFeatureFile'] = @dataset[0]['refFeatureFile']
+    end
+  end
+  def commands
+    command = "module load #{@params["Rversion"]}\n"
+    command << run_RApp("EzAppSCMultipleSamplesOneGroup")
+  end
+end
+
+if __FILE__ == $0
+
+end
