f8071b05fa928d98522f1f4f32094b1405076cd8
Lennart Opitz <lennart.opitz@fgcz.ethz.ch>
2021-06-30 11:27:55 +0200

    add control seqs to STARApp

--- master/lib/STARApp.rb
+++ master/lib/STARApp.rb
@@ -30,6 +30,8 @@ EOS
     @params['paired'] = false
     @params['strandMode'] = ['both', 'sense', 'antisense']
     @params['refFeatureFile'] = 'genes.gtf'
+    @params['controlSeqs'] = ''
+    @params['controlSeqs', 'description'] = 'The extra control sequences (such as spikein sequences) available in https://fgcz-gstore.uzh.ch/reference/controlSeqs.fa'
     @params['cmdOptions'] = '--sjdbOverhang 150 --outFilterType BySJout --outFilterMatchNmin 30 --outFilterMismatchNmax 10 --outFilterMismatchNoverLmax 0.05 --outMultimapperOrder Random --alignSJDBoverhangMin 1 --alignSJoverhangMin 8 --alignIntronMax 100000 --alignMatesGapMax 100000  --outFilterMultimapNmax 50 --chimSegmentMin 15 --chimJunctionOverhangMin 15 --chimScoreMin 15 --chimScoreSeparation 10 --outSAMstrandField intronMotif --alignEndsProtrude 3 ConcordantPair'
     @params['getJunctions'] = false
     #@params['getChimericJunctions'] = false
