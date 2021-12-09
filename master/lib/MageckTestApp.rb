29d1cf2f3c59f4dfeb8dfe0ba78fa7662f85777c
falkonoe <falnoe@ethz.ch>
2021-12-09 16:29:15 +0100

    Added MageckTestApp

--- master/lib/MageckTestApp.rb
+++ master/lib/MageckTestApp.rb
@@ -6,10 +6,10 @@ require_relative 'global_variables'
 include GlobalVariables
 
 
-class CountQCApp < SushiFabric::SushiApp
+class MageckTestApp < SushiFabric::SushiApp
   def initialize
     super
-    @name = 'CountQC'
+    @name = 'MageckTest'
     @analysis_category = 'QC'
     @description =<<-EOS
     Run test module in the tool Model-based Analysis of Genome-wide CRISPR-Cas9 Knockout (<a href='https://sourceforge.net/p/mageck/wiki/Home/'>MAGeCK</a>)
@@ -23,6 +23,11 @@ class CountQCApp < SushiFabric::SushiApp
     @params['scratch'] = ['10']
     @params['name'] = 'MAGeCK_Test'
     @params['specialOptions'] = ''
+    @params['grouping'] = ''
+    @params['sampleGroup'] = ''
+    @params['sampleGroup', 'description'] = 'sampleGroup should be different from refGroup'
+    @params['refGroup'] = ''
+    @params['refGroup', 'description'] = 'refGroup should be different from sampleGroup'
     @params['mail'] = ""
     @modules = ["Dev/R"]
     @inherit_tags = ["B-Fabric"]
@@ -30,25 +35,18 @@ class CountQCApp < SushiFabric::SushiApp
   def preprocess
   end
   def next_dataset
-    report_file = File.join(@result_dir, @params['name'])
-    report_link = File.join(report_file, '00index.html')
+    @comparison = "#{@params['sampleGroup']}--over--#{@params['refGroup']}"
+    @params['comparison'] = @comparison
+    @params['name'] = @comparison
+    report_folder = File.join(@result_dir, "#{@params['comparison']}")
     {'Name'=>@params['name'],
-     'Species'=>(dataset = @dataset.first and dataset['Species']),
-     'refBuild'=>@params['refBuild'],
-     'Static Report [Link]'=>report_link,
-     'Live Report [Link]'=>"#{SHINY_EXPLORE_COUNTS}?data=#{report_file}/counts-#{@random_string}-EzResult.RData",
-     'Report [File]'=>report_file,
+     'Report [File]'=>report_folder,
     }.merge(extract_columns(@inherit_tags))
   end
   def set_default_parameters
-    @params['refBuild'] = @dataset[0]['refBuild']
-    if dataset_has_column?('refFeatureFile')
-      @params['refFeatureFile'] = @dataset[0]['refFeatureFile']
-    end
-    @params['transcriptTypes'] = @dataset[0]['transcriptTypes'].to_s.split(',')
   end
   def commands
-    run_RApp("EzAppCountQC")
+    run_RApp("EzAppMageckTest")
   end
 end
 
