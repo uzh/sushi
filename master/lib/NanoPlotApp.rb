a3a763249206da8262f533b699f1b537b7e30a78
Weihong Qi <weihong.qi@fgcz.ethz.ch>
2022-05-12 11:49:25 +0200

    change NanoPlot output dir

--- master/lib/NanoPlotApp.rb
+++ master/lib/NanoPlotApp.rb
@@ -17,23 +17,18 @@ class NanoPlotApp <  SushiFabric::SushiApp
     <a href='https://github.com/wdecoster/NanoPlot'/>Github web-site</a> 
 EOS
     @required_columns = ['Name','Read1']
-    #@required_columns = ['Name','Read1', 'Species']
-    @required_params = ['name']
+    @required_params = ['cores']
     @params['cores'] = '8'
     @params['ram'] = '30'
     @params['scratch'] = '100'
-    @params['name'] = 'NanoPlot_Result'
     @params['cmdOptions'] = ""
     @params['mail'] = ""
     @modules = ["Dev/R"]
   end
   def next_dataset
-    report_file = File.join(@result_dir, @params['name'])    
-    report_link = File.join(@result_dir, @params['name'], "#{@dataset['Name']}.NanoPlot-report.html")
     {'Name'=>@dataset['Name'],
-     #'Species'=>@dataset['Species'],
-     'Report [File]'=>report_file,
-     'Report [Link]'=>report_link
+     'NanoPlot Result [File]'=>File.join(@result_dir, "#{@dataset['Name']}"),
+     'Report [Link]'=>File.join(@result_dir, "#{@dataset['Name']}", "#{@dataset['Name']}.NanoPlot-report.html")
     }
   end
   def commands
