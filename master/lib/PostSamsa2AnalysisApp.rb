d907b156d8f135b35b56b684815da339f4e9d46e
Natalia Zajac <natalia.zajac@fgcz.ethz.ch>
2022-09-19 15:11:01 +0200

    postsamsa2report

---
+++ master/lib/PostSamsa2AnalysisApp.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+Version = '20171109-095604'
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class PostSamsa2AnalysisApp < SushiFabric::SushiApp
+def initialize
+super
+@name = 'PostSamsa2Analysis'
+@analysis_category = 'Metagenomics'
+@description =<<-EOS
+Step_6 of Samsa2, data analysis and report. 
+  EOS
+@params['process_mode'] = 'DATASET'
+@required_columns = ['Name', 'annotationORGFileRefSeq', 'annotationFUNCFileRefSeq']
+@required_params = ['grouping', 'sampleGroup', 'refGroup'  ]
+@params['cores'] = '1'
+@params['ram'] = '7'
+@params['scratch'] = '10'
+@params['grouping'] = '' 
+@params['sampleGroup'] = '' 
+@params['sampleGroup', 'description'] = 'sampleGroup should be different from refGroup'
+@params['refGroup'] = '' 
+@params['refGroup', 'description'] = 'refGroup should be different from sampleGroup'
+@params['mail'] = ""
+@inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
+@modules = ["Dev/R"]
+end
+
+def next_dataset
+@params['name'] = "PostSamsa2Analysis"
+    report_file = File.join(@result_dir, @params['name'])
+    report_link = File.join(report_file, '00index.html')
+{'Name'=>@params['name'],
+  'Report [File]'=>report_file,
+  'Static Report [Link]'=>report_link,
+}
+end
+def commands
+run_RApp("EzAppPostSamsa2Analysis")
+end
+end
+
+if __FILE__ == $0
+end
+
