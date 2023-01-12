c4b073ba088bcc78bd3e37cefbe9661f9263fbfa
Masaomi Hatakeyama <masaomi.hatakeyama@uzh.ch>
2023-01-12 10:07:06 +0100

    Added min/maxDP for filteration in GATKv4JointGenoTypesApp.rb

--- master/lib/GATKv4JointGenoTypesApp.rb
+++ master/lib/GATKv4JointGenoTypesApp.rb
@@ -25,7 +25,8 @@ genotype,merge and annotate gvcf-Files<br/>
     @params['QD'] = '2.0'
     @params['MQ'] = '30.0'
     @params['GQ'] = '20'
-    @params['DP'] = '0'
+    @params['minDP'] = '2'
+    @params['maxDP'] = '10000'
     @params['MQRankSum'] = '-15.0'
     @params['specialOptions'] = ''
     @modules = ["Variants/GATK/4.2.0.0", "Tools/Picard/2.18.0"]
@@ -47,7 +48,7 @@ genotype,merge and annotate gvcf-Files<br/>
     ref = refs.find{|fa| File.exist?(fa)}
     combined_g_vcf = @params['name'] + ".g.vcf"
     combined_raw_vcf = @params['name'] + ".raw.vcf"
-    combined_raw_snp_vcf = @params['name'] + ".raw.snp.vcf"
+    combined_filter_marked_vcf = @params['name'] + ".filter_marked.vcf"
     combined_filtered_vcf = @params['name'] + ".filtered.vcf"
     gvcfs = []
     command = ""
@@ -62,15 +63,13 @@ genotype,merge and annotate gvcf-Files<br/>
     gvcfs_option = gvcfs.map{|gvcf| "-V #{gvcf}"}.join(" ")
     command << "gatk CombineGVCFs -R #{ref} #{gvcfs_option} -O #{combined_g_vcf}\n"
     command << "gatk GenotypeGVCFs -R #{ref} -V #{combined_g_vcf} -O #{combined_raw_vcf}\n"
-    raw_vcf = combined_raw_vcf
-    if @params['only_SNP']
-      command << "gatk SelectVariants -R #{ref} -V #{combined_raw_vcf} -O #{combined_raw_snp_vcf} -select-type SNP\n"
-      raw_vcf = combined_raw_snp_vcf
-    end
+    #raw_vcf = combined_raw_vcf
     #command << "gatk VariantFiltration -R #{ref} -V #{raw_vcf} --filter-expression \"! vc.hasAttribute('QD') || QD < #{@params['QD']}\" --filter-name \"QD\" --filter-expression \"vc.isSNP() && (MQ < #{@params['MQ']} || (vc.hasAttribute('MQRankSum') && MQRankSum < #{@params['MQRankSum']}))\" --filter-name \"MQ\" --genotype-filter-expression \"GQ < #{@params['GQ']} || DP == #{@params['DP']}\" --genotype-filter-name \"GQ\" -O #{combined_filtered_vcf}\n"
-    command << "gatk VariantFiltration -R #{ref} -V #{raw_vcf} --filter-name \"QD\" --filter-expression \"vc.hasAttribute('QD') && QD < #{@params['QD']}\" --filter-name \"MQ\" --filter-expression \"vc.isSNP() && vc.hasAttribute('MQ') && MQ < #{@params['MQ']}\" --filter-name \"MQRankSum\" --filter-expression \"vc.hasAttribute('MQRankSum') && MQRankSum < #{@params['MQRankSum']}\" --genotype-filter-name \"GQ\" --genotype-filter-expression \"GQ < #{@params['GQ']}\" --filter-name \"DP\" --filter-expression \"vc.hasAttribute('DP') && DP < #{@params['DP']}\" -O #{combined_filtered_vcf}\n"
+    command << "gatk VariantFiltration -R #{ref} -V #{combined_raw_vcf} --filter-name \"QD\" --filter-expression \"vc.hasAttribute('QD') && QD < #{@params['QD']}\" --filter-name \"MQ\" --filter-expression \"vc.isSNP() && vc.hasAttribute('MQ') && MQ < #{@params['MQ']}\" --filter-name \"MQRankSum\" --filter-expression \"vc.hasAttribute('MQRankSum') && MQRankSum < #{@params['MQRankSum']}\" --genotype-filter-name \"GQ\" --genotype-filter-expression \"GQ < #{@params['GQ']}\" --filter-name \"DP\" --filter-expression \"vc.hasAttribute('DP') && (DP < #{@params['minDP']} || DP >= #{@params['maxDP']})\" -O #{combined_filter_marked_vcf}\n"
     if @params['only_SNP']
-      command << "mv #{combined_raw_snp_vcf} #{combined_raw_vcf}\n"
+      command << "gatk SelectVariants -R #{ref} -V #{combined_filter_marked_vcf} -O #{combined_filtered_vcf} -select-type SNP --exclude-filtered\n"
+    else
+      command << "gatk SelectVariants -R #{ref} -V #{combined_filter_marked_vcf} -O #{combined_filtered_vcf} --exclude-filtered\n"
     end
     command << "gzip #{combined_raw_vcf}\n"
     command << "gzip #{combined_filtered_vcf}\n"
