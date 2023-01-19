cc7ce04d6ec25c64655fff9325c16d372beae346
Lennart Opitz <lennart.opitz@fgcz.ethz.ch>
2023-01-19 11:03:16 +0100

    update cellranger version

--- master/lib/CellRangerApp.rb
+++ master/lib/CellRangerApp.rb
@@ -45,7 +45,7 @@ This wrapper runs <a href='https://support.10xgenomics.com/single-cell-gene-expr
     @params['specialOptions'] = ''
     @params['mail'] = ""
     @modules = ["Tools/seqtk", "Dev/R", "Dev/Python/3.8.3", "Tools/samtools"]
-    @params['CellRangerVersion'] = ["Aligner/CellRanger/7.0.0", "Aligner/CellRanger/6.1.2"]
+    @params['CellRangerVersion'] = ["Aligner/CellRanger/7.1.0", "Aligner/CellRanger/7.0.0", "Aligner/CellRanger/6.1.2"]
     @inherit_tags = ["Factor", "B-Fabric"]
   end
   def set_default_parameters
