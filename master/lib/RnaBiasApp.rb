c702775f7792799fade3b47e88e983f9a538d5cf
Lennart Opitz <lennart.opitz@fgcz.ethz.ch>
2023-03-03 15:21:56 +0100

    add RnaBiasApp

---
+++ master/lib/RnaBiasApp.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class RnaBiasApp <  SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'RnaBiasApp'
+    @params['process_mode'] = 'DATASET'
+    @analysis_category = 'QC'
+    @description =<<-EOS 
+EOS
+    @required_columns = ['Name','Read1','Species']
+    @required_params = ['name']
+
+@params['cores'] = '8'
+@params['ram'] = '20'
+@params['scratch'] = '100'
+@params['node'] = ''
+@params['name'] = 'RNA_Bias_Result'
+@params['paired'] = false
+@params['strandMode'] = 'both'
+@params['refFeatureFile'] = 'genes.gtf'
+@params['bootstrap-samples'] = '10'
+@params['seed'] = '42'
+@params['fragment-length'] = '150'
+@params['sd'] = '70'
+@params['bias'] = true
+@params['pseudobam'] = true
+@params['transcriptFasta'] = ''
+@params['transcriptTypes'] = 'protein_coding'
+@params['trimAdapter'] = true
+@params['cut_front'] = false
+@params['trim_front1'] = '0'
+@params['trim_tail1'] = '0'
+@params['cut_front'] = false
+@params['cut_front_window_size'] =	'4'
+@params['cut_front_mean_quality'] =	'20'
+@params['cut_tail']	= true
+@params['cut_tail_window_size'] =	'4'
+@params['cut_tail_mean_quality'] =	'15'
+@params['cut_right'] =	false
+@params['cut_right_window_size'] =	'4'
+@params['cut_right_mean_quality'] =	'20'
+@params['average_qual'] =	'20'
+@params['max_len1'] = 	'0'
+@params['max_len2'] =	'0'
+@params['poly_x_min_len'] =	'10'
+@params['length_required'] =	'20'
+@params['refBuild'] = ref_selector
+@params['backgroundExpression'] = 5 ## low numbers needed for the iSeq runs with
+@params['sigThresh'] = 5
+@params['normMethod'] = 'logMean'
+@params['expressionName'] = 'est_counts'
+@params['nReads']= '10000'
+@params['mail'] = ''
+@params['cmdOptions'] = ''
+  
+@modules = ["Dev/R", "Aligner/Kallisto", "QC/Fastq"]
+  end
+ def set_default_parameters
+    @params['paired'] = dataset_has_column?('Read2')
+  end
+  def preprocess
+    if @params['paired']
+      @required_columns<<  'Read2'
+    end
+  end
+  def next_dataset
+    report_file = File.join(@result_dir,'report-RNAseq-bias.pdf')    
+    {'Name'=>@params['name'],
+     'Report [File]'=>report_file,
+     'Report [Link]'=>report_file
+    }
+  end
+  def commands
+    run_RApp("EzAppRnaComputeBias")
+  end
+end
+
+if __FILE__ == $0
+  
+end
