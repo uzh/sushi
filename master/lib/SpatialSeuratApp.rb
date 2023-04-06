daef805da5f6a07adc3521613465f8e37c6ea160
Lennart Opitz <lennart.opitz@fgcz.ethz.ch>
2023-04-06 12:51:53 +0200

    inherit tags in SpatialSeurat

--- master/lib/SpatialSeuratApp.rb
+++ master/lib/SpatialSeuratApp.rb
@@ -50,7 +50,8 @@ Single cell report<br/>
     @params['nUMIs', 'description'] = 'A gene will be kept if it has at least nUMIs in the fraction of spots specified before'
     @params['specialOptions'] = ''
     @params['mail'] = ""
-    @params['Rversion'] = ["Dev/R/4.2.2", "Dev/R/4.2.0", "Dev/R/4.1.2", "Dev/R/4.1.0", "Dev/R/4.0.4", "Dev/R/4.0.3"]
+    @params['Rversion'] = ["Dev/R/4.2.2", "Dev/R/4.2.0"]
+    @inherit_tags = ["Factor", "B-Fabric"]
   end
   def preprocess
     @random_string = (1..12).map{[*('a'..'z')].sample}.join
@@ -64,7 +65,7 @@ Single cell report<br/>
      'refFeatureFile'=>@params['refFeatureFile'],
      'Static Report [Link]'=>report_link,
      'Report [File]'=>report_file,
-    }
+    }.merge(extract_columns(@inherit_tags))
   end
   def set_default_parameters
     @params['refBuild'] = @dataset[0]['refBuild']
