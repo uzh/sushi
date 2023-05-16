c087b1170bc9832379cce28b5cecbf9775e2e25b
Masaomi Hatakeyama <masaomi.hatakeyama@uzh.ch>
2023-05-16 15:02:00 +0200

    Updated db/schema.rb

--- master/db/schema.rb
+++ master/db/schema.rb
@@ -2,17 +2,17 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_130324) do
+ActiveRecord::Schema.define(version: 2023_05_16_090858) do
 
-  create_table "data_sets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "data_sets", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.integer "project_id"
     t.integer "parent_id"
     t.string "name"
@@ -31,9 +31,12 @@ ActiveRecord::Schema.define(version: 2020_10_08_130324) do
     t.string "run_name_order_id"
     t.integer "workunit_id"
     t.text "order_ids"
+    t.text "job_parameters"
+    t.integer "order_id"
+    t.index ["order_id"], name: "index_data_sets_on_order_id"
   end
 
-  create_table "jobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "jobs", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.integer "submit_job_id"
     t.integer "input_dataset_id"
     t.integer "next_dataset_id"
@@ -42,21 +45,21 @@ ActiveRecord::Schema.define(version: 2020_10_08_130324) do
     t.string "script_path"
   end
 
-  create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "projects", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "data_set_tree", limit: 16777215
+    t.text "data_set_tree", size: :medium
   end
 
-  create_table "samples", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "samples", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.text "key_value"
     t.integer "data_set_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "sushi_applications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "sushi_applications", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "class_name"
     t.string "analysis_category"
     t.text "required_columns"
@@ -64,9 +67,10 @@ ActiveRecord::Schema.define(version: 2020_10_08_130324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.boolean "employee"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
