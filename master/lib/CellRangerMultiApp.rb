44e699bddd5c60ca83e36a527868b12e442d3aa2
CodingKaiser <falkonoe@gmail.com>
2023-07-26 22:40:40 +0200

    Update CellRangerMultiApp.rb documentation

--- master/lib/CellRangerMultiApp.rb
+++ master/lib/CellRangerMultiApp.rb
@@ -14,11 +14,11 @@ class CellRangerMultiApp <  SushiFabric::SushiApp
 This wrapper runs <a href='https://support.10xgenomics.com/single-cell-vdj/software/pipelines/latest/using/multi',>cellranger multi</a> in Single-library analysis mode.<br><br>
 
 Note: that running this app usually requires manual curation of the input dataset. Column names for the corresponding 10X library are as follows.<br>
-<table style="width: 25%;">
+<table style="width: 35%;">
 <tbody>
-<tr><td>VDJ-T </td><td>VdjTDataDir [File]</td></tr>
+<tr><td>VDJ-T</td><td>VdjTDataDir [File]</td></tr>
 <tr><td>VDJ-B</td><td>VdjBDataDir [File]</td></tr>
-<tr><td>Multiplexing </td><td>MultiDataDir [File]</td></tr>
+<tr><td>Multiplexing</td><td>MultiDataDir [File]</td></tr>
 </tbody>
 </table>
     EOS
