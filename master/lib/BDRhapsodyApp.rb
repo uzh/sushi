054c849331a35df06310f0317fb1cd816288f0da
CodingKaiser <falkonoe@gmail.com>
2023-07-28 12:11:50 +0200

    Added rest of parameters

--- master/lib/BDRhapsodyApp.rb
+++ master/lib/BDRhapsodyApp.rb
@@ -13,7 +13,7 @@ class BDRhapsodySequenceAnalysis <  SushiFabric::SushiApp
     @description =<<-EOS
 This wrapper runs a <a href='https://scomix.bd.com/hc/en-us/categories/360000838932-Resource-Library',>CWL workflow</a> for the analysis of BD Single-Cell Multiomics.
     EOS
-    @required_columns = ['Name','Read1','Read2','Species']
+    @required_columns = ['Name', 'Read1', 'Read2', 'Species']
     @required_params = ['name', 'refBuild']
     @params['cores'] = ['8', '12', '16']
     @params['ram'] = ['60', '40', '80']
@@ -31,6 +31,13 @@ This wrapper runs a <a href='https://scomix.bd.com/hc/en-us/categories/360000838
     @params['exactCellCount', 'description'] = 'Set a specific number (>=1) of cells as putative, based on those with the highest error-corrected read count.'
     @params['expectedCellCount'] = ''
     @params['expectedCellCount', 'description'] = 'Guide the basic putative cell calling algorithm by providing an estimate of the number of cells expected. Usually this can be the number of cells loaded into the Rhapsody cartridge.'
+    @params['generateBamOutput'] = false
+    @params['excludeIntronicReads'] = false
+    @params['putativeCellCalling'] = ['mRNA', 'AbSeq']
+    @params['putativeCellCalling', 'description'] = 'Specify the data to be used for putative cell calling'
+    @params['targetedReference'] = ''
+    @params['targetedReference', 'file_upload'] = true
+    @params['targetedReference', 'description'] = 'This is an mRNA reference file in fasta format. This is a pre-designed, supplemental, or custom panel.'
     @params['transcriptTypes'] = ['protein_coding', 'rRNA', 'tRNA', 'Mt_rRNA', 'Mt_tRNA', 'long_noncoding', 'short_noncoding', 'pseudogene']
     @params['transcriptTypes', 'multi_selection'] = true
     @params['transcriptTypes', 'selected'] = ['protein_coding', 'rRNA', 'tRNA', 'Mt_rRNA', 'Mt_tRNA']
