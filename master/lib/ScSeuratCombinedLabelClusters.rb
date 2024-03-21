43e9d5da9eeb498bb64ebe873d15998a4a543f0c
Falko <falkonoe@gmail.com>
2024-03-21 07:07:27 +0100

    Create app

---
+++ master/lib/ScSeuratCombinedLabelClusters.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+
+class ScSeuratCombinedLabelClusters < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'ScSeuratCombinedLabelClusters'
+    @params['process_mode'] = 'DATASET'
+    @analysis_category = 'SingleCell'
+    @description =<<-EOS
+    The report of merged single cell samples/plates<br/>
+    EOS
+    @required_columns = ['Name', 'Species', 'Static Report', 'Report']
+    @required_params = ['ClusterAnnotationFile']
+    # optional params
+    @params['cores'] = '8'
+    @params['ram'] = '60'
+    @params['scratch'] = '100'
+    @params['node'] = ''
+    @params['process_mode'] = 'DATASET'
+    @params['name'] = 'SCReportMultipleSamplesSeurat'
+    @params['ClusterAnnotationFile'] = ''
+    @params['ClusterAnnotationFile', 'file_upload'] = true
+    @params['ClusterAnnotationFile', 'description'] = "A 3-column mapping of old cluster to new cluster labels in .xlsx format. Use the 'clusterInfos.xlsx' file as a template. The first column indicates the sample name. The second column are the old cluster labels. The third column are the new cluster labels. The first row should be a header indicating the column names. We recommend using the names provided in 'clusterInfos.xlsx', namely 'Sample', 'Cluster', and 'ClusterLabel'."
+    @params['tissue'] = []
+    @params['tissue','multi_selection'] = true
+    @params['tissue','all_selected'] = true
+    @params['tissue', 'multi_selection_size'] = 10
+    tissue = {}
+    CSV.foreach("/srv/GT/databases/scGeneSets/all_cell_markers.txt", headers: true, col_sep: "\t") do |e|
+      tissue[e["tissueType"]] = true
+    end
+    @params['tissue'] = tissue.keys.sort
+    @params['tissue', 'description'] = 'Tissue the cells come from. Used in cell types identification for Human and Mouse organisms.'
+    @params['enrichrDatabase'] = ['Tabula_Muris', 'Tabula_Sapiens', 'Azimuth_Cell_Types_2021', 'PanglaoDB_Augmented_2021', 'CellMarker_Augmented_2021', 'Allen_Brain_Atlas_10x_scRNA_2021', 'Human_Gene_Atlas', 'Mouse_Gene_Atlas', ]
+    @params['enrichrDatabase','multi_selection'] = true
+    @params['enrichrDatabase','all_selected'] = true
+    @params['SingleR'] = ['none', 'BlueprintEncodeData', 'DatabaseImmuneCellExpressionData', 'HumanPrimaryCellAtlasData', 
+                          'ImmGenData', 'MonacoImmuneData', 'MouseRNAseqData', 'NovershternHematopoieticData']
+    @params['DE.method'] = ['wilcox', 'LR']
+    @params['DE.method', 'description'] = "Method to be used when calculating gene cluster markers and differentially expressed genes between conditions."
+    @params['DE.regress'] = ['Batch', 'CellCycle']
+    @params['DE.regress','multi_selection'] = true
+    @params['DE.regress', 'description'] = "Variables to regress when calculating gene cluster markers and differentially expressed genes. Only used with the LR method."
+    @params['min.pct'] = 0.1
+    @params['min.pct', 'description'] = 'Used in calculating cluster markers: The minimum fraction of cells in either of the two tested populations.'
+    @params['logfc.threshold'] = 0.25
+    @params['logfc.threshold', 'description'] = 'Used in calculating cluster markers: Limit testing to genes which show, on average, at least X-fold difference (log-scale) between the two groups of cells.'
+    @params['computePathwayTFActivity'] = false
+    @params['computePathwayTFActivity', 'description'] = 'Whether to calculate the TF and pathway activities (Note: Only for human and mouse)'
+    @params['specialOptions'] = ''
+    @params['mail'] = ""
+    @modules = ["Dev/R"]
+    @inherit_tags = ["Factor", "B-Fabric"]
+  end
+  def next_dataset
+    report_file = File.join(@result_dir, @params['name'])
+    report_link = File.join(report_file, '00index.html')
+    {'Name'=>@params['name'],
+     'Species'=>(dataset = @dataset.first and dataset['Species']),
+     'Static Report [Link]'=>report_link,
+     'Report [File]'=>report_file,
+    }
+  end
+  def commands
+    run_RApp("EzAppScSeuratCombine")
+  end
+end
+
+if __FILE__ == $0
+
+end
