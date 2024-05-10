3e0f9746e337f098b2b3e71a0e80ea82005be178
Lennart Opitz <lennart.opitz@fgcz.ethz.ch>
2024-05-10 15:24:40 +0200

    use older R version for GATK due to conflict

--- master/lib/GatkJoinGenoTypesApp.rb
+++ master/lib/GatkJoinGenoTypesApp.rb
@@ -37,7 +37,7 @@ genotype,merge and annotate gvcf-Files<br/>
     @params['dbNSFP_fields'] = 'ExAC_AF,ExAC_AC,1000Gp1_EUR_AF,Uniprot_acc,Interpro_domain,phastCons100way_vertebrate,CADD_phred,Polyphen2_HDIV_pred,Polyphen2_HVAR_pred,SIFT_score,SIFT_pred'
     @params['dbNSFP_fields','description'] = "details about available fields are available under <a href='https://sites.google.com/site/jpopgen/dbNSFP',>dbNSFP</a>"
     @params['mail'] = ""
-    @modules = ["Dev/jdk", "Variants/GATK", "Tools/Picard", "Variants/SnpEff", "Dev/R"]
+    @modules = ["Dev/jdk", "Variants/GATK", "Tools/Picard", "Variants/SnpEff", "Dev/R/4.3.0"]
   end
   def next_dataset
     report_dir = File.join(@result_dir, @params['name'])
