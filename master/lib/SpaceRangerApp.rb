b1572e78b32561855edd3369243f442132604623
Lennart Opitz <lennart.opitz@fgcz.ethz.ch>
2024-06-11 08:42:26 +0200

    update SR

--- master/lib/SpaceRangerApp.rb
+++ master/lib/SpaceRangerApp.rb
@@ -48,7 +48,7 @@ This wrapper runs <a href='https://support.10xgenomics.com/spatial-gene-expressi
     @params['cmdOptions', 'description'] = 'specify the commandline options for SpaceRanger; do not specify any option that is already covered by the dedicated input fields'
     @params['specialOptions'] = ''
     @params['mail'] = ""
-    @params['SpaceRangerVersion'] = ["Aligner/SpaceRanger/2.1.1", "Aligner/SpaceRanger/2.1.0", "Aligner/SpaceRanger/3.0.0"]
+    @params['SpaceRangerVersion'] = ["Aligner/SpaceRanger/3.0.1", "Aligner/SpaceRanger/2.1.1"]
     @modules = ["Dev/R", "Aligner/CellRanger"]
     @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
   end
