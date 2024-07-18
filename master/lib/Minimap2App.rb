9ebe008d5929f94bb06408581c3ac3089874fbc8
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2024-07-18 13:06:20 +0200

    add minimap2

---
+++ master/lib/Minimap2App.rb
@@ -0,0 +1,87 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class BWAApp < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'Minimap2'
+    @analysis_category = 'Map'
+    @description =<<-EOS
+    https://lh3.github.io/minimap2/minimap2.html
+    <br/>
+    EOS
+    @required_columns = ['Name','Read1','Species']
+    @required_params = ['refBuild','paired']
+    # optional params
+    @params['cores'] = '8'
+    @params['ram'] = '30'
+    @params['scratch'] = '100'
+    @params['refBuild'] = ref_selector
+    @params['refBuild', 'description'] = 'the genome refBuild and annotation to use as reference. If human variant calling is the main goal, please use hg_19_karyotypic.'
+    @params['cmdOptions'] = ''
+
+    # trimming options
+    # general
+    @params['trimAdapter'] = false
+    # Fastp
+    ## trimming
+    @params['trim_front1'] = '0'
+    @params['trim_front1','description'] = 'trimming how many bases in front for read1 (and read2), default is 0.'
+    @params['trim_tail1'] = '0'
+    @params['trim_tail1','description'] = 'trimming how many bases in tail for read1 (and read2), default is 0.'
+    @params['cut_front'] = false
+    @params['cut_front','description'] = 'move a sliding window from front (5p) to tail, drop the bases in the window if its mean quality < threshold, stop otherwise.'
+    @params['cut_front_window_size'] = '4'
+    @params['cut_front_mean_quality'] = '20'
+    @params['cut_tail'] = false
+    @params['cut_tail','description'] = 'move a sliding window from tail (3p) to front, drop the bases in the window if its mean quality < threshold, stop otherwise.'
+    @params['cut_tail_window_size'] = '4'
+    @params['cut_tail_mean_quality'] = '20'
+    @params['cut_right'] = false
+    @params['cut_right','description'] = 'move a sliding window from front to tail, if meet one window with mean quality < threshold, drop the bases in the window and the right part, and then stop.'
+    @params['cut_right_window_size'] = '4'
+    @params['cut_right_mean_quality'] = '20'    
+    @params['average_qual'] = '0'
+    @params['average_qual','description'] = 'if one read average quality score <avg_qual>, then this read/pair is discarded. Default 0 means no requirement'
+    @params['max_len1'] = '0'
+    @params['max_len1','description'] = 'if read1 is longer than max_len1, then trim read1 at its tail to make it as long as max_len1. Default 0 means no limitation. If two reads are present, the same will apply to read2.'
+    @params['max_len2'] = '0'
+    @params['max_len2','description'] = 'if read1 is longer than max_len2, then trim read2 at its tail to make it as long as max_len1. Default 0 means no limitation.'
+    @params['poly_x_min_len'] = '10'
+    @params['poly_x_min_len','description'] = 'the minimum length to detect polyX in the read tail. 10 by default.'
+    @params['length_required'] = '18'
+    @params['length_required','description'] = 'reads shorter than length_required will be discarded.'
+    ## additional commands
+    @params['cmdOptionsFastp'] = ''
+    
+    @params['mail'] = ""
+    @modules = ["Tools/samtools", "Aligner/minimap2", "QC/fastp", "Dev/R", "Tools/Picard"]
+    @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
+  end
+  def next_dataset
+    {'Name'=>@dataset['Name'],
+     'BAM [File]'=>File.join(@result_dir, "#{@dataset['Name']}.bam"),
+     'BAI [File]'=>File.join(@result_dir, "#{@dataset['Name']}.bam.bai"),
+     'IGV [Link]'=>File.join(@result_dir, "#{@dataset['Name']}-igv.html"),
+     'Species'=>@dataset['Species'],
+     'refBuild'=>@params['refBuild'],
+     'paired'=>false,
+     'refFeatureFile'=>@params['refFeatureFile'],
+     'strandMode'=>@params['strandMode'],
+     'Read Count'=>@dataset['Read Count'],
+     'IGV [File]'=>File.join(@result_dir, "#{@dataset['Name']}-igv.html"),
+     'PreprocessingLog [File]'=>File.join(@result_dir, "#{@dataset['Name']}_preprocessing.log")
+    }.merge(extract_columns(@inherit_tags))
+  end
+  def commands
+    run_RApp("EzAppMinimap2")
+  end
+end
+
+if __FILE__ == $0
+
+end
