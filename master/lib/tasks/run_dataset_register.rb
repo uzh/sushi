865e232e47a1e5504f3ee59a9ac74e6d780c79c6
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2024-07-19 16:00:25 +0200

    Updated run_dataset_register.rb

--- master/lib/tasks/run_dataset_register.rb
+++ master/lib/tasks/run_dataset_register.rb
@@ -1,19 +1,25 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-# Version = '20240712-101921'
-
-#!/usr/bin/env ruby
+# Version = '20240718-110347'
 
 require 'pathname'
+
+RAILS_ROOT='/srv/sushi/production/master'
+SLEEP_TIME = 60*60 # [s] = 1h, valid only for daemon-mode
 
 help =-> () do
   puts <<-eos
   usage:
-   #{File.basename(__FILE__)} [task name]
+   #{File.basename(__FILE__)} [task name] (--run, --year [YEAR], --rails-root [RAILS_ROOT], --daemon-mode)
+
+  RAILS_ROOT (default): #{RAILS_ROOT}
 
   e.g.:
-   #{File.basename(__FILE__)} ds:register_datasets[2024,run]
+   #{File.basename(__FILE__)} --year 2024
+   #{File.basename(__FILE__)} --run
+   #{File.basename(__FILE__)} --year 2024 --run
    #{File.basename(__FILE__)} ds:register_datasets[2024,]
+   #{File.basename(__FILE__)} ds:register_datasets[2024,run]
   eos
   exit
 end
@@ -22,65 +28,42 @@ if i=ARGV.index("-h")
   help.()
 end
 
+run = ARGV.index("--run")
+year = if i=ARGV.index("--year")
+         ARGV[i+1]
+       else
+         Time.now.strftime("%Y")
+       end
 
-# Railsアプリケーションのディレクトリを指定（絶対パスに置き換えてください）
-rails_root = Pathname.new('/srv/sushi/masa_test_sushi_20240711/master')
+rails_root = if i=ARGV.index("--rails-root")
+               ARGV[i+1]
+             else
+               Pathname.new(RAILS_ROOT)
+             end
 
-# 環境変数の設定
+daemon_mode = ARGV.index("--daemon-mode")
+
 ENV['RAILS_ENV'] = 'production'
 ENV['DISABLE_DATABASE_ENVIRONMENT_CHECK'] = '1'
 
-# 実行するRakeタスクとその引数を取得
-task_name_with_args = ARGV[0] || 'ds:register_datasets[2024,]'
+task_name_with_args = if ARGV[0] =~ /^ds\:register_datasets/
+                        ARGV[0]
+                      elsif run
+                        "ds:register_datasets[#{year},run]"
+                      else
+                        "ds:register_datasets[#{year},]"
+                      end
 
-# bundle exec rakeコマンドを実行
 command = "bundle exec rake #{task_name_with_args}"
 Dir.chdir(rails_root) do
-  system(command)
+  if daemon_mode
+    loop do
+      system(command)
+      sleep SLEEP_TIME
+    end
+  else
+    system(command)
+  end
 end
 
 
-# #!/usr/bin/env ruby
-# 
-# require 'pathname'
-# require 'rake'
-# 
-# # Railsアプリケーションのディレクトリを指定（絶対パスに置き換えてください）
-# rails_root = Pathname.new('/srv/sushi/masa_test_sushi_20240704/master')
-# 
-# # 環境変数の設定
-# ENV['RAILS_ENV'] = 'production'
-# ENV['DISABLE_DATABASE_ENVIRONMENT_CHECK'] = '1'
-# 
-# # Rails環境をロード
-# require rails_root.join('config', 'environment')
-# 
-# ## Rakeアプリケーションを初期化
-# #rake_app = Rake.application
-# #rake_app.init
-# #rake_app.load_rakefile
-# 
-# # 実行したいRakeタスクを指定
-# # 引数からタスク名とその引数を取得
-# # ds:register_datasets[2024,run]
-# task_name_with_args = ARGV[0] || 'ds:register_datasets[2024,]'
-# 
-# 
-# #task_name, args = task_name_with_args.split('[')
-# #args = args ? args.chomp(']').split(',') : []
-# 
-# # bundle exec rakeコマンドを実行
-# command = "bundle exec rake #{task_name_with_args}"
-# Dir.chdir(rails_root) do
-#   system(command)
-# end
-# 
-# # # タスクを実行
-# # rake_app[task_name].invoke(*args)
-# # 
-# # # タスクを実行
-# # rake_app[task_name].invoke
-# 
-# 
-# 
-# 
