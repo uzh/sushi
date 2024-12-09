f85a6ee0a6134777894fc12d05b7b00ff2451137
Lennart Opitz <lopitz@fgcz.ethz.ch>
2024-12-09 15:53:54 +0100

    Update DmrseqApp.rb

--- master/lib/DmrseqApp.rb
+++ master/lib/DmrseqApp.rb
@@ -16,31 +16,39 @@ class DmrseqApp <  SushiFabric::SushiApp
     It requires cov file from Bismark. <br/>
 EOS
     @required_columns = ['Name','COV']
-    @required_params = ['grouping', 'sampleGroup', 'refGroup']
+    @required_params = ['grouping', 'sampleGroup', 'refGroup', 'refBuild']
     @params['cores'] = '4'
     @params['ram'] = '80'
     @params['scratch'] = '100'
     @params['paired'] = true
+    @params['refBuild'] = ref_selector
+    @params['refFeatureFile'] = 'genes.gtf'
     @params['grouping'] = ''
     @params['sampleGroup'] = ''
     @params['sampleGroup', 'description'] = 'sampleGroup should be different from refGroup'
     @params['refGroup'] = ''
     @params['refGroup', 'description'] = 'refGroup should be different from sampleGroup'
-    @params['qval'] = '0.05'
+    @params['qval'] = '0.1'
     @params['qval', 'description'] = 'FDR cutoff'
     @params['cmdOptions'] = ''
     @params['mail'] = ""
     @modules = ["Dev/R"]
     @inherit_columns = ["Order Id"]
   end
+  def set_default_parameters
+    @params['refBuild'] = @dataset[0]['refBuild']
+  end
+  
   def next_dataset
     @comparison = "#{@params['sampleGroup']}--over--#{@params['refGroup']}"
     @params['comparison'] = @comparison
     @params['name'] = @comparison
     {'Name'=>@params['name'],
+      'Report [File]'=>File.join(@result_dir,'00index.html'),
       'CovObject [File]'=>File.join(@result_dir,'bismarkBSseq_filtered.rds'),
       'RegionFile [File]'=>File.join(@result_dir,'dmrseq_results.rds'),
-      'BlockFile [File]'=>File.join(@result_dir,'large_blocks.rds')
+      'BlockFile [File]'=>File.join(@result_dir,'large_blocks.rds'),
+      'Params [File]'=>File.join(@result_dir,'param.rds')
     }.merge(extract_columns(colnames: @inherit_columns))
   end
   def commands
