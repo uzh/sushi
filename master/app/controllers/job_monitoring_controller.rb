252a77e37eb722e5c938111ba8691ad4370ea3e3
Masaomi Hatakeyama <masaomi.hatakeyama@uzh.ch>
2024-12-13 14:25:15 +0100

    Updated resubmit function without calling workflow manager

--- master/app/controllers/job_monitoring_controller.rb
+++ master/app/controllers/job_monitoring_controller.rb
@@ -87,47 +87,24 @@ class JobMonitoringController < ApplicationController
     end
   end
   def resubmit_job
-    if @job_id = params[:id]
-      gstore_script_dir = if job = Job.find_by_submit_job_id(@job_id)
-                            @data_set_id = if data_set = job.data_set
-                                             data_set.id
-                                           end
-                            File.dirname(job.script_path)
-                          end
-      prev_params = {}
-      if parameters_tsv = File.join(File.dirname(gstore_script_dir), "parameters.tsv") and File.exist?(parameters_tsv)
-        File.readlines(parameters_tsv).each do |line|
-          name, value = line.chomp.split
-          prev_params[name] = value.to_s.delete('"')
-        end
-        script_content = @@workflow_manager.get_script(@job_id)
-        script_path = @@workflow_manager.get_script_path(@job_id)
-        project_number = session[:project]
-        gsub_options = []
-        gsub_options << "-c #{prev_params['cores']}" unless prev_params['cores'].to_s.empty?
-        gsub_options << "-n #{prev_params['node']}" unless prev_params['node'].to_s.empty?
-        gsub_options << "-p #{prev_params['partition']}" unless prev_params['partition'].to_s.empty?
-        gsub_options << "-r #{prev_params['ram']}" unless prev_params['ram'].to_s.empty?
-        gsub_options << "-s #{prev_params['scratch']}" unless prev_params['scratch'].to_s.empty?
-        gsub_options << "-i #{prev_params['nice']}" unless prev_params['nice'].to_s.empty?
-      end
+    if @job_id = params[:id] and job = Job.find_by_id(@job_id)
+      @data_set_id = if data_set = job.data_set
+                       data_set.id
+                     end
 
-      if script_path and current_user and script_content and project_number and gstore_script_dir and @data_set_id and File.exist?(parameters_tsv)
-        new_job_id = @@workflow_manager.start_monitoring3(script_path, script_content, current_user.login, project_number, gsub_options.join(' '), gstore_script_dir, job.next_dataset_id, SushiFabric::RAILS_HOST)
-        puts "job_id: #{new_job_id}"
-        new_job = Job.new
-        new_job.submit_job_id = new_job_id.to_i
-        new_job.script_path = script_path
-        new_job.next_dataset_id = job.next_dataset_id
-        new_job.save
-        new_job.data_set.jobs << new_job
-        new_job.data_set.save
+      file_base_name = File.basename(job.script_path)
+      submit_job_script_dir = SushiFabric::Application.config.submit_job_script_dir
+      new_stdout_path = File.join(submit_job_script_dir, file_base_name + "_o.log")
+      new_stderr_path = File.join(submit_job_script_dir, file_base_name + "_e.log")
+      job.stdout_path = new_stdout_path
+      job.stderr_path = new_stderr_path
+      job.status = "CREATED"
+      job.save
 
-        puts "RESUBMITTED"
-      else
-        #raise "SOMETHING WRONG"
-        puts "FAILED resubmission"
-      end
+      puts "RESUBMITTED"
+    else
+      #raise "SOMETHING WRONG"
+      puts "FAILED resubmission"
     end
   end
   def change_status
