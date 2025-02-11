d4bc7e42d3e27224dffc5c5d06e5000e249e6def
Lennart Opitz <lopitz@fgcz.ethz.ch>
2025-02-11 20:49:03 +0100

    Update MemeApp.rb

--- master/lib/otherApps/MemeApp.rb
+++ master/lib/otherApps/MemeApp.rb
@@ -20,13 +20,13 @@ Perform motif discovery on DNA, RNA or protein datasets<br/>
     @params['ram'] = '10'
     @params['scratch'] = '20'
     @params['name'] = 'MotifCheck_MEME'
-    @params['motifDB'] = '-db /srv/GT/databases/meme_db/motif_databases/JASPAR/JASPAR2018_CORE_vertebrates_redundant.meme -db /srv/GT/databases/meme_db/motif_databases/MOUSE/uniprobe_mouse.meme -db /srv/GT/databases/meme_db/motif_databases/HUMAN/HOCOMOCOv9.meme'
+    @params['motifDB'] = '-db /srv/GT/databases/MEME/motif_databases/JASPAR/JASPAR2022_CORE_vertebrates_redundant_v2.meme -db /srv/GT/databases/MEME/motif_databases/MOUSE/uniprobe_mouse.meme -db /srv/GT/databases/MEME/motif_databases/HUMAN/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme'
     @params['cmdOptions'] = '-meme-mod zoops -minw 6 -maxw 30 -meme-nmotifs 5 -centrimo-score 5.0 -centrimo-ethresh 10.0'
     @params['filterPeaks'] = 'true'
     @params['specialOptions'] = 'distanceToTSS=2000 minFold=4'
     @params['refBuild'] = ref_selector
     @params['mail'] = ''
-    @modules = ['Dev/OpenMPI','Dev/Python','Tools/MEME', "Dev/R", "Tools/BEDTools"]
+    @modules = ['Dev/Perl','Dev/Python','Tools/MEME', "Dev/R", "Tools/BEDTools"]
   end
   def next_dataset
     meme_link = File.join(@result_dir, "#{@dataset['Name']}/#{@dataset['Name']}_meme-chip.html")
