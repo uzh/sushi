d1b20cbe7e15db63cead13096fcc5062317a58e2
Masaomi Hatakeyama <masaomi.hatakeyama@uzh.ch>
2025-03-21 13:12:18 +0100

    Updated config/environments/production.rb removing workflow manager

--- master/config/environments/production.rb
+++ master/config/environments/production.rb
@@ -93,7 +93,7 @@ SushiFabric::Application.configure do
     #config.rails_host = "https://fgcz-sushi-demo.uzh.ch"    # demo
     #config.rails_host = "https://fgcz-course1.bfabric.org"  # course1
     #config.rails_host = "https://fgcz-course2.bfabric.org"  # course2
-    config.rails_host = "http://fgcz-h-037.fgcz-net.unizh.ch:4000"  # test
+    #config.rails_host = "http://fgcz-h-037.fgcz-net.unizh.ch:4000"  # test
     config.copy_command = "g-req copy"
     config.sushi_server_class = "SushiFabric::TestSushi"
   end
