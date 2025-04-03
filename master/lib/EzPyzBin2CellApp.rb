1ee46241bd1fae0abad001db9baa5364243f3cbc
Raphael De Gottardi <raphael.degottardi@students.unibe.ch>
2025-04-03 15:54:20 +0200

    Created Bin2Cell App (ezpyz)

---
+++ master/lib/EzPyzBin2CellApp.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class EzPyzBin2CellApp <  SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'EzPyzBin2CellApp'
+    @params['process_mode'] = 'DATASET'
+    @analysis_category = 'Spatial'
+    @description =<<-EOS
+    A Bin2Cell app for VisiumHD data<br/>
+    EOS
+    @required_columns = ['Name','BinnedOutputs2um [File]','SourceImage [File]','SpaceRanger [File]']
+    @required_params = ['name']
+    @params['cores'] = '1'
+    @params['ram'] = '30'
+    @params['scratch'] = '100'
+    @params['name'] = 'EzPyzBin2CellApp'
+    @params['sizeFactors'] = '1,3,5,10,20,30'
+    @params['mail'] = ""
+    @modules = []
+    @inherit_tags = ["Factor", "B-Fabric"]
+  end
+  def next_dataset
+    report_dir = File.join(@result_dir, @params['name'])
+    {'Name'=>@params['name'],
+     'Figures [File]'=>File.join(report_dir, 'Figures'),
+     'Report [File]'=>File.join(report_dir, 'stardist')
+    }
+  end
+  def commands
+    run_PyApp("Bin2cell",conda_env: 'tmp_bin2cell') #change to App specific env soon
+  end
+end
