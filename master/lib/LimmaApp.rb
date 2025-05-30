16ad87cc5fb3670b27ebe1dd1d318734e64364b9
Lennart Opitz <lopitz@fgcz.ethz.ch>
2025-05-30 16:17:15 +0200

    Update LimmaApp.rb

--- master/lib/LimmaApp.rb
+++ master/lib/LimmaApp.rb
@@ -47,7 +47,7 @@ class LimmaApp < SushiFabric::SushiApp
     @params['specialOptions'] = ''
     @params['expressionName'] = ''
     @params['mail'] = ""
-    @params['Rversion'] = ["Dev/R/4.4.2"]
+    @params['Rversion'] = ["Dev/R/4.5.0","Dev/R/4.4.2"]
     @inherit_columns = ["Order Id"]
   end
    def preprocess
