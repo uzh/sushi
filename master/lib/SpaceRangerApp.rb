2068b42e359419f2497ba8e831139be5800b62ee
Paul Gueguen <34238952+p-gueguen@users.noreply.github.com>
2025-06-13 14:58:30 +0200

    Add SpaceRanger v4.0.1

--- master/lib/SpaceRangerApp.rb
+++ master/lib/SpaceRangerApp.rb
@@ -48,7 +48,7 @@ This wrapper runs <a href='https://support.10xgenomics.com/spatial-gene-expressi
     @params['cmdOptions', 'description'] = 'specify the commandline options for SpaceRanger; do not specify any option that is already covered by the dedicated input fields'
     @params['specialOptions'] = ''
     @params['mail'] = ""
-    @params['SpaceRangerVersion'] = ["Aligner/SpaceRanger/3.1.3","Aligner/SpaceRanger/3.1.2","Aligner/SpaceRanger/3.0.1"]
+    @params['SpaceRangerVersion'] = ["Aligner/SpaceRanger/4.0.1","Aligner/SpaceRanger/3.1.3","Aligner/SpaceRanger/3.1.2","Aligner/SpaceRanger/3.0.1"]
     @modules = ["Dev/R", "Aligner/CellRanger", "Tools/samtools"]
     @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
   end
