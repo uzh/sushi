211462743895cf357e5cff591bb3c901a5f26c66
Marco Notaro <marco.notaro@fgcz.uzh.ch>
2025-08-22 09:48:19 +0200

    Create NfCoreCutAndRunApp

---
+++ master/lib/NfCoreCutAndRunApp.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class NfCoreAtacSeqApp <  SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'NfCoreCutAndRun'
+    @params['process_mode'] = 'DATASET'
+    @analysis_category = 'CutAndRun'
+    @description =<<-EOS
+    A CUT&RUN processing pipeline from NF-Core. <br/>
+   <a href='https://nf-co.re/cutandrun'>NF-Core CUT&RUN</a>
+EOS
+    @required_columns = ['Name','Read1','Read2','Species']
+    @required_params = ['refBuild', 'peakStyle', 'grouping']
+    @params['cores'] = '8'
+    @params['ram'] = '100'
+    @params['scratch'] = '200'
+    @params['paired'] = true
+    @params['refBuild'] = ref_selector
+    @params['refFeatureFile'] = 'genes.gtf'
+    @params['peakStyle'] = ['broad', 'narrow']
+    @params['peakCaller'] = ['macs2', 'seacr']
+    @params['spikeinGenome'] = ['K12-MG1655', 'R64-1-1', 'BDGP6']
+    @params['spikeinGenome', 'description'] = 'name of the iGenome reference for the spike-in genome. Defaulting to E. coli K12, for yeast set to R64-1-1, for fruit fly BDGP6'
+    @params['normalization'] = ['Spikein', 'RPKM', 'CPM', 'BPM', 'None']
+    @params['grouping'] = ''
+    @params['grouping', 'description'] = 'grouping information needs to be filled in the input dataset'
+    @params['control'] = ''
+    @params['control', 'description'] = 'control information needs to be filled in the input dataset'
+    builds = ['GRCm39', 'GRCm38', 'GRCh38', 'GRCh37']
+    build_name = @params['refBuild']
+    black_path = '/scratch/mnotaro/test-nfcore/.nextflow/assets/nf-core/cutandrun/assets/blacklists/' # move to a global path
+    @params['blacklist'] = if builds.include?(build_name)
+      "#{black_path}#{build_name}-blacklist.bed"
+    else
+      ""
+    end
+    @params['name'] = ['NfCoreCutAndRun']
+    @params['cmdOptions'] = ""
+    @params['mail'] = ""
+    @modules = ["Dev/jdk"]
+  end
+ def set_default_parameters
+    if @params['paired']
+      @required_columns<<  'Read2'
+    end
+  end
+  def next_dataset
+     report_file = File.join(@result_dir, "#{@params['name']}_results")
+     report_link = File.join(@result_dir, "#{@params['name']}_results", "multiqc", "#{@params['peakStyle']}_peak", "multiqc_report.html")
+     {'Name'=>@params['name'],
+     'Species'=>(dataset = @dataset.first and dataset['Species']),
+     'refBuild'=>@params['refBuild'],
+     'grouping'=>@params['grouping'],
+     'Result [File]'=>report_file,
+     'Report [Link]'=>report_link
+    }
+  end
+  def commands
+    run_RApp('EzAppNfCoreCutAndRun')
+  end
+end
