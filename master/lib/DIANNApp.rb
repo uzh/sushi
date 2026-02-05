0d8fb91ab5f875cb6aac730c11c560787d3b6670
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2026-02-05 09:51:23 +0100

    add diann

---
+++ master/lib/DIANNApp.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+
+class DIANNApp < SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'DIANN'
+    @analysis_category = 'QC'
+    @description =<<-EOS
+Quality control after counting reads<br/>
+    EOS
+    @params['process_mode'] = 'DATASET'
+    @required_columns = ['Name','DDA RAW', 'Resource', 'Grouping Var']
+    @required_params = []
+    # optional params
+    @params['cores'] = ['64']
+    @params['cores', "context"] = "slurm"
+    @params['ram'] = ['32']
+    @params['ram', "context"] = "slurm"
+    @params['scratch'] = ['20']
+    @params['scratch', "context"] = "slurm"
+    @params['name'] = 'DIANN_v23_DDA'
+    @params['03_fasta_database_path'] = 'NONE'
+    @params['03_fasta_use_custom'] = 'false'
+    @params['03b_additional_fasta_database_path'] = '/misc/fasta/p34486_Proteobench_TripleProteome_with_headers_20251008.fasta'
+    @params['05_diann_is_dda'] = 'true'
+    @params['06a_diann_mods_variable'] = '--var-mods 1 --var-mod UniMod:35,15.994915,M'
+    @params['06b_diann_mods_no_peptidoforms'] = 'false'
+    @params['06c_diann_mods_unimod4'] = 'true'
+    @params['06d_diann_mods_met_excision'] = 'true'
+    @params['07_diann_peptide_fragment_mz_max'] = '1800'
+    @params['07_diann_peptide_fragment_mz_min'] = '200'
+    @params['07_diann_peptide_max_length'] = '30'
+    @params['07_diann_peptide_min_length'] = '6'
+    @params['07_diann_peptide_precursor_charge_max'] = '3'
+    @params['07_diann_peptide_precursor_charge_min'] = '2'
+    @params['07_diann_peptide_precursor_mz_max'] = '1500'
+    @params['07_diann_peptide_precursor_mz_min'] = '400'
+    @params['08_diann_digestion_cut'] = 'K*,R*'
+    @params['08_diann_digestion_missed_cleavages'] = '1'
+    @params['09_diann_mass_acc_ms1'] = '15'
+    @params['09_diann_mass_acc_ms2'] = '20'
+    @params['10_diann_scoring_qvalue'] = '0.01'
+    @params['11a_diann_protein_pg_level'] = 'protein_names_1'
+    @params['11b_diann_protein_relaxed_prot_inf'] = 'true'
+    @params['12a_diann_quantification_reanalyse'] = 'true'
+    @params['12b_diann_quantification_no_norm'] = 'false'
+    @params['13_diann_freestyle'] = 'None'
+    @params['97_raw_converter'] = 'thermoraw'
+    @params['98_diann_binary'] = 'diann-docker'
+    @params['99_other_verbose'] = '1'
+    @params['application_version'] = '2.3'    
+    @params['specialOptions'] = ''
+    @params['mail'] = ""
+    @modules = ["Dev/R"]
+    @inherit_columns = []
+  end
+  def next_dataset
+    report_file = File.join(@result_dir, @params['name'])
+    #report_link = File.join(report_file, '00index.html')
+    {'Name'=>@params['name'],
+     'Result [File]'=>"Result.zip",
+     'DIANN Result [File]'=>"DIANN_v23_Result.zip",
+    }.merge(extract_columns(colnames: @inherit_columns))
+  end
+  def commands
+    run_RApp("EzAppDIANN")
+  end
+end
+
+if __FILE__ == $0
+
+end
+
