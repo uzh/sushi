3d1e89668f7cc2b9802227ec6ffd47030639e43c
Lennart Opitz <lopitz@fgcz.ethz.ch>
2026-09-14 18:12:32 +0200

    update ExploreMageckCount

--- master/lib/ExploreMageckCountsApp.rb
+++ master/lib/ExploreMageckCountsApp.rb
@@ -25,48 +25,29 @@ Quality control after counting sgRNAs  with Mageck<br/>
     @params['scratch'] = ['10', '20']
     @params['scratch', "context"] = "slurm"
     @params['name'] = 'ExploreMageckCounts'
-    #@params['refBuild'] = ref_selector
-    @params['refBuild', "context"] = "referfence genome assembly"
-    #@params['refFeatureFile'] = 'genes.gtf'
-    #@params['featureLevel'] = ['gene', 'isoform']
-    @params['normMethod'] = 'logMean'
-    @params['normMethod', "context"] = "ExploreMageckCounts"
-    #@params['runGO'] = ['true', 'false']
+    @params['normMethod'] = ['deseq2', 'tmm', 'cpm', 'logMean']
+    @params['normMethod', "description"] = "count normalisation: deseq2 (size factors, as MAGeCK), edgeR tmm/cpm, or logMean"
+    @params['refGroup'] = ''
+    @params['refGroup', "description"] = "reference/plasmid/T0 condition used as baseline for essential-gene depletion + ROC; leave empty to skip that analysis"
     @params['backgroundExpression'] = 5
-    @params['backgroundExpression', "description"] = "counts to be added to shrink estimated log2 ratios"
-    @params['backgroundExpression', "context"] = "ExploreMageckCounts"
-    #@params['transcriptTypes'] = ''
-    #@params['transcriptTypes', 'multi_selection'] = true
-    #@params['transcriptTypes', 'selected'] = 0
-    @params['specialOptions'] = ''
-    @params['specialOptions', "context"] = "ExploreMageckCounts"
-    @params['expressionName'] = 'sample1'
-    @params['expressionName', "context"] = "ExploreMageckCounts"
+    @params['backgroundExpression', "description"] = "pseudo-count added before the log2 transform"
+    @params['topGeneSize'] = 100
+    @params['topGeneSize', "description"] = "number of most-variable sgRNAs shown in the top-feature heatmap"
+    @params['nSampleClusters'] = 6
+    @params['nSampleClusters', "description"] = "number of sample clusters annotated in the high-variance heatmap"
     @params['mail'] = ""
     @modules = ["Dev/R"]
     @inherit_columns = ["Order Id"]
-  end
-  def preprocess
-    @random_string = (1..12).map{[*('a'..'z')].sample}.join
   end
   def next_dataset
     report_file = File.join(@result_dir, @params['name'])
     report_link = File.join(report_file, '00index.html')
     {'Name'=>@params['name'],
      'Species'=>(dataset = @dataset.first and dataset['Species']),
-     #'refBuild'=>@params['refBuild'],
      'libName'=>(dataset = @dataset.first and dataset['libName']),
      'Static Report [Link]'=>report_link,
-     'Live Report [Link]'=>"http://fgcz-shiny.uzh.ch/exploreDE?data=#{report_file}/counts-#{@random_string}-EzResult.RData",
      'Report [File]'=>report_file,
     }.merge(extract_columns(colnames: @inherit_columns))
-  end
-  def set_default_parameters
-    #@params['refBuild'] = @dataset[0]['refBuild']
-    #if dataset_has_column?('refFeatureFile')
-    #  @params['refFeatureFile'] = @dataset[0]['refFeatureFile']
-    #end
-    #@params['transcriptTypes'] = @dataset[0]['transcriptTypes'].to_s.split(',')
   end
   def commands
     run_RApp("EzAppExploreMageckCounts")
